3af5acaaf590a4425f3f853e5c660b324b2328da
matyhtf <mikan.tenny@gmail.com>
2013-10-31 10:42:23 +0800

    Ignore test & wiki dir

--- examples/async_mysql2.php
+++ examples/async_mysql2.php
@@ -5,6 +5,7 @@ $db->query("show tables", MYSQLI_ASYNC);
 swoole_event_add(swoole_get_mysqli_sock($db), function($db_sock) {
     global $db;
     $res = $db->reap_async_query();
-    var_dump($res->fetch_all(MYSQLI_ASSOC), true);
+    var_dump($res->fetch_all(MYSQLI_ASSOC));
     swoole_event_exit();
 });
+echo "Finish\n";
