5f269de25ca5d3eb2109cfc6702a5805efe6f55b
matyhtf <mikan.tenny@gmail.com>
2014-04-24 09:55:33 +0800

    Added eof_check examples

---
+++ examples/server/eof_server.php
@@ -0,0 +1,19 @@
+<?php
+$serv = new swoole_server("127.0.0.1", 9501);
+$serv->set(array(
+	'package_eof' => "\r\n\r\n", 
+	'open_eof_check' => true,
+	'package_max_length' => 1024*1024*2, //2M
+));
+$serv->on('connect', function ($serv, $fd){
+    echo "[#".posix_getpid()."]\tClient:Connect.\n";
+});
+$serv->on('receive', function ($serv, $fd, $from_id, $data) {
+	$req = unserialize(trim($data));
+	echo $req['name']."\n";
+	echo "content_length: ".strlen($req['content'])."\n";
+});
+$serv->on('close', function ($serv, $fd) {
+    echo "[#".posix_getpid()."]\tClient: Close.\n";
+});
+$serv->start();
