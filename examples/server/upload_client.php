84f8d259b5314ea598010d72d48d34786b72d3c1
matyhtf <mikan.tenny@gmail.com>
2014-05-08 14:38:22 +0800

    Added upload file examples

---
+++ examples/server/upload_client.php
@@ -0,0 +1,66 @@
+<?php
+$client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_SYNC);
+$args = getopt("p:h:f:t");
+if (empty($args['p']) or empty($args['h']) or empty($args['f'])) {
+    echo "Usage: php {$argv[0]} -h server_ip -p server_port -f file\n";
+    exit;
+}
+
+if (empty($args['t'])) {
+    $args['t'] = 30;
+}
+
+$file = $args['f'];
+$size = filesize($file);
+
+if (!is_file($file)) {
+    die("Error: file '{$args['f']}' not found\n");
+}
+
+if (!$client->connect($args['h'], $args['p'], $args['t'], 0)) {
+    echo "Error: connect to server failed. " . swoole_strerror($client->errCode);
+    die("\n");
+}
+
+$data = array(
+    'name' => $file,
+    'size' => $size,
+);
+if (!$client->send(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) . "\r\n\r\n")) {
+    die("Error: send header failed.\n");
+}
+getResponse($client);
+
+echo "Start transport. file={$file}, size={$size}\n";
+
+$fp = fopen($file, 'r');
+if (!$fp) {
+    die("Error: open $file failed.\n");
+}
+while(!feof($fp))
+{
+    $read = fread($fp, 8192);
+    if (!$client->send($read)) {
+        echo "send failed. ErrCode=".$client->errCode."\n";
+        break;
+    }
+}
+getResponse($client);
+echo "Success. send_size = $size\n";
+
+function getResponse(swoole_client $client)
+{
+    $recv = $client->recv();
+    if (!$recv) {
+        die("Error: recv header failed.\n");
+    }
+    $respCode = json_decode($recv, true);
+    if (!$respCode) {
+        die("Error: header json_decode failed.\n");
+    }
+    if ($respCode['code'] != 0) {
+        die("Server: message={$respCode['msg']}.\n");
+    } else
+    echo "[FromServer]\t{$respCode['msg']}\n";
+    return true;
+}
---
+++ examples/server/upload_server.php
@@ -0,0 +1,104 @@
+<?php
+$svr = new SwooleUploadServer;
+$svr->run();
+
+class SwooleUploadServer
+{
+    /**
+     * @var swoole_server
+     */
+    protected $serv;
+    protected $buffer;
+    protected $files;
+
+    protected $root_path = '/tmp/';
+    protected $override = false;
+
+    static $max_file_size = 100000000; //100M
+
+    function onConnect($serv, $fd, $from_id)
+    {
+        echo "new upload client[$fd] connected.\n";
+    }
+
+    function message($fd, $code, $msg)
+    {
+        $this->serv->send($fd, json_encode(array('code' => $code, 'msg' => $msg)));
+        echo "[-->$fd]\t$code\t$msg\n";
+        if ($code != 0) {
+            $this->serv->close($fd);
+        }
+        return true;
+    }
+
+    function onReceive(swoole_server $serv, $fd, $from_id, $data)
+    {
+        //传输尚未开始
+        if (empty($this->files[$fd])) {
+            $req = json_decode($data, true);
+            if ($req === false) {
+                return $this->message($fd, 400, 'Error Request');
+            }
+            if (empty($req['size']) or empty($req['name'])) {
+                return $this->message($fd, 500, 'require file name and size.');
+            } elseif ($req['size'] > self::$max_file_size) {
+                return $this->message($fd, 501, 'over the max_file_size. ' . self::$max_file_size);
+            }
+            $file = $this->root_path . '/' . $req['name'];
+            $dir = realpath(dirname($file));
+            if (!$dir or strncmp($dir, $this->root_path, strlen($this->root_path)) != 0) {
+                return $this->message($fd, 502, "file path[$dir] error. Access deny.");
+            } elseif ($this->override and is_file($file)) {
+                return $this->message($fd, 503, 'file exists. Server not allowed override');
+            }
+            $fp = fopen($file, 'w');
+            if (!$fp) {
+                return $this->message($fd, 5034, 'can open file.');
+            } else {
+                $this->message($fd, 0, 'transmission start');
+                $this->files[$fd] = array('fp' => $fp, 'name' => $file, 'size' => $req['size'], 'recv' => 0);
+            }
+        } //传输已建立
+        else {
+            $info = & $this->files[$fd];
+            $fp = $info['fp'];
+            $file = $info['name'];
+
+            if (!fwrite($fp, $data)) {
+                $this->message($fd, 600, "fwrite failed. transmission stop.");
+                unlink($file);
+            } else {
+                $info['recv'] += strlen($data);
+                if ($info['recv'] >= $info['size']) {
+                    $this->message($fd, 0, "Success, transmission finish. Close connection.");
+                }
+            }
+        }
+    }
+
+    function onclose($serv, $fd, $from_id)
+    {
+        unset($this->files[$fd]);
+        echo "upload client[$fd] closed.\n";
+    }
+
+    function run()
+    {
+        $serv = new swoole_server("0.0.0.0", 9507);
+        $serv->set(array(
+            'worker_num' => 4,
+        ));
+        $serv->on('Start', function ($serv) {
+            echo "Swoole Upload Server running\n";
+        });
+        $this->root_path = rtrim($this->root_path, ' /');
+        $serv->on('connect', array($this, 'onConnect'));
+        $serv->on('receive', array($this, 'onreceive'));
+        $serv->on('close', array($this, 'onclose'));
+        $this->serv = $serv;
+        $serv->start();
+    }
+}
+
+
+
