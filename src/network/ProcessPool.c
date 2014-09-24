swoole/swoole-src

commit c07d2b76bbfce078e9fcd573e0a404e0fbddea98

Fixed task_ipc_mode config not works as expected

src/network/ProcessPool.c
@@ -25,39 +25,44 @@ static void swProcessPool_free(swProcessPool *pool);
 int swProcessPool_create(swProcessPool *pool, int worker_num, int max_request, key_t msgqueue_key)
 {
 	bzero(pool, sizeof(swProcessPool));
+
+	pool->worker_num = worker_num;
+	pool->max_request = max_request;
+
+	if (msgqueue_key > 0) {
+	    pool->use_msgqueue = 1;
+	    pool->msgqueue_key = msgqueue_key;
+	}
+
 	pool->workers = sw_calloc(worker_num, sizeof(swWorker));
-	if (pool->workers == NULL)
+    if (pool->workers == NULL)
     {
         swWarn("malloc[1] failed.");
         return SW_ERR;
     }
 
-	pool->worker_num = worker_num;
-	pool->max_request = max_request;
-
-	pool->pipes = sw_calloc(worker_num, sizeof(swPipe));
-	if (pool->pipes == NULL)
-	{
-		swWarn("malloc[2] failed.");
-		sw_free(pool->workers);
-		return SW_ERR;
-	}
-
 	pool->map = swHashMap_new(SW_HASHMAP_INIT_BUCKET_N, free);
-	if (pool->map == NULL)
-	{
-	    return SW_ERR;
-	}
+    if (pool->map == NULL)
+    {
+        sw_free(pool->workers);
+        return SW_ERR;
+    }
 
 	int i;
-	if (msgqueue_key > 0)
+	if (pool->use_msgqueue)
 	{
-		if (swQueueMsg_create(&pool->queue, 1, msgqueue_key, 1) < 0)
+	    pool->pipes = sw_calloc(worker_num, sizeof(swPipe));
+        if (pool->pipes == NULL)
+        {
+            swWarn("malloc[2] failed.");
+            sw_free(pool->workers);
+            return SW_ERR;
+        }
+
+		if (swQueueMsg_create(&pool->queue, 1, pool->msgqueue_key, 1) < 0)
 		{
 			return SW_ERR;
 		}
-		pool->use_msgqueue = 1;
-		pool->msgqueue_key = msgqueue_key;
 	}
 	else
 	{
@@ -246,7 +251,7 @@ static int swProcessPool_worker_start(swProcessPool *pool, swWorker *worker)
 	 */
 	out.buf.info.from_fd = worker->id;
 
-	if (SwooleG.task_ipc_mode > 1)
+	if (SwooleG.task_ipc_mode == SW_IPC_MSGQUEUE)
 	{
 		out.mtype = worker->id + 1;
 	}

src/factory/FactoryProcess.c
@@ -284,7 +284,7 @@ static int swFactoryProcess_manager_start(swFactory *factory)
 	if (SwooleG.task_worker_num > 0)
 	{
 		key_t msgqueue_key = 0;
-		if (SwooleG.task_ipc_mode > 0)
+		if (SwooleG.task_ipc_mode == SW_IPC_MSGQUEUE)
 		{
 			msgqueue_key =  serv->message_queue_key + 2;
 		}

commit 2f5dd5d9942a148424ab590d6133743435086c6b

Fixed segfault caused by commit c07d2b76bbfce078e9

src/network/ProcessPool.c
@@ -51,6 +51,13 @@ int swProcessPool_create(swProcessPool *pool, int worker_num, int max_request, k
     int i;
     if (pool->use_msgqueue)
     {
+        if (swQueueMsg_create(&pool->queue, 1, pool->msgqueue_key, 1) < 0)
+        {
+            return SW_ERR;
+        }
+    }
+    else
+    {
         pool->pipes = sw_calloc(worker_num, sizeof(swPipe));
         if (pool->pipes == NULL)
         {
@@ -59,13 +66,6 @@ int swProcessPool_create(swProcessPool *pool, int worker_num, int max_request, k
             return SW_ERR;
         }
 
-        if (swQueueMsg_create(&pool->queue, 1, pool->msgqueue_key, 1) < 0)
-        {
-            return SW_ERR;
-        }
-    }
-    else
-    {
         swPipe *pipe;
         for (i = 0; i < worker_num; i++)
         {