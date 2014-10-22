ad2666e4c44563af2e8eca9c9e94ca031fb84dfc
matyhtf <mikan.tenny@gmail.com>
2014-10-22 10:33:55 +0800

    Update http_server examples

--- examples/server/http_server.php
+++ examples/server/http_server.php
@@ -1,18 +1,26 @@
 <?php
-$serv = new swoole_server("127.0.0.1", 9501);
-$serv->set(array(
-		'open_http_protocol' => true,
-		'worker_num' => 1,
-		'dispatch_mode' => 2,
-		'package_max_length' => 1024 * 1024 * 2,
-));
+$http = new swoole_http_server("127.0.0.1", 9501);
 
-$serv->on('receive', function (swoole_server $serv, $fd, $from_id, $data) {
-	echo "recv: ".strlen($data)."\n";
-	$body = "<h1>hello world!</h1>";
-	$header  = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: Keep-Alive\r\n";
-	$header .= "Content-Length: ".strlen($body)."\r\n\r\n";
-	$serv->send($fd, $header.$body);
+$http->on('request', function ($request, $response) {
+	var_dump($request->header);
+	var_dump($request->get);
+	var_dump($request->post);
+	
+	$response->header("Location: http://www.baidu.com/");
+	$response->write("<h1>Hello Swoole</h1>");
+	/**
+	 * close connection
+	 */
+	$response->end();
 });
 
-$serv->start();
+$http->on('message', function ($request, $response) {
+	var_dump($request->message);
+	/**
+	 * WebSocket->send
+	 */
+	$response->write("some data\n");
+	
+});
+
+$http->start();
