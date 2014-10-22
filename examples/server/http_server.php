de691c9a2813ebb8cee2897564ae65f58a162563
Tianfeng <mikan.tenny@gmail.com>
2014-10-22 23:53:59 +0800

    Update http_server.php

--- examples/server/http_server.php
+++ examples/server/http_server.php
@@ -2,23 +2,14 @@
 $http = new swoole_http_server("127.0.0.1", 9501);
 
 $http->on('request', function ($request, $response) {
-	var_dump($request->header);
-	var_dump($request->get);
-	var_dump($request->post);
-	
+	var_dump($request->header, $request->get, $request->post, $request->cookie, $request->info);
 	$response->header("Location: http://www.baidu.com/");
 	$response->write("<h1>Hello Swoole</h1>");
-	/**
-	 * close connection
-	 */
-	$response->end();
+	$response->end(); //close connection
 });
 
 $http->on('message', function ($request, $response) {
 	var_dump($request->message);
-	/**
-	 * WebSocket->send
-	 */
 	$response->write("some data\n");
 	
 });
