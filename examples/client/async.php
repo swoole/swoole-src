51ba75fe0ba29d1467a2f601e250bbc3b546c065
matyhtf <mikan.tenny@gmail.com>
2014-12-01 12:18:04 +0800

    Update async client

--- examples/client/async.php
+++ examples/client/async.php
@@ -1,10 +1,10 @@
 <?php
 $client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC); //异步非阻塞
-
+$client->finish = false;
 $client->on("connect", function(swoole_client $cli) {
     $cli->send("GET / HTTP/1.1\r\n\r\n");
     //$cli->sendfile(__DIR__.'/test.txt');
-    $cli->_count = 0;
+    //$cli->_count = 0;
 });
 
 $client->on("receive", function(swoole_client $cli, $data){
@@ -15,8 +15,8 @@ $client->on("receive", function(swoole_client $cli, $data){
         $cli->close();
         return;
     }
-    //usleep(500000);
     $cli->send(str_repeat('A', 100)."\n");
+    $cli->finish = true;
 });
 
 $client->on("error", function(swoole_client $cli){
@@ -27,7 +27,15 @@ $client->on("close", function(swoole_client $cli){
     echo "Connection close\n";
 });
 
-$client->connect('127.0.0.1', 9501, 0.5);
+$client->connect('www.baidu.com', 80);
+swoole_timer_after(1000, function () use ($client) {
+    if ($client->finish) {
+        return;
+    } else {
+        echo "socket timeout\n";
+        $client->close();
+    }
+});
 
 echo "connect to 127.0.0.1:9501\n";
 //for PHP5.3-
