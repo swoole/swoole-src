20586d6075bb3cbfa5864073e1d374e042d4ee13
shenzhe <shenzhe163@gmail.com>
2014-12-06 09:31:24 +0800

    web socket example

--- examples/server/websocket_server.php
+++ examples/server/websocket_server.php
@@ -1,31 +1,18 @@
 <?php
 $http = new swoole_http_server("127.0.0.1", 9501);
+<<<<<<< Updated upstream
 //$http->set(['worker_num' => 4, 'task_worker_num' => 4]);
+=======
+$http->set(['worker_num' => 4]);
+>>>>>>> Stashed changes
 $http->on('message', function($data, $response){
     //var_dump($data);
     $response->message("server send:".$data);
 });
 $http->on('request', function ($request, $response) {
-//	var_dump($request->cookie);
-	//$response->status(301);
-    //$response->header("Location", "http://www.baidu.com/");
-	//$response->cookie("hello", "world", time() + 3600);
-    //$response->header("Content-Type", "text/html; charset=utf-8");
-    //var_dump($request->rawContent());
-    
-    //var_dump($request->post);
 	$response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
-    //global $http;
-    //$http->task("hello world");
 });
 
-$http->on('finish', function(){
-    echo "task finish";
-});
-
-$http->on('task', function(){
-    echo "async task\n";
-});
 
 $http->on('close', function(){
     echo "on close\n";
