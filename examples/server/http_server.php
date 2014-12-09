57c5e6ce6557b9f0f241f1c018afbbd8f249aff9
shenzhe <shenzhe163@gmail.com>
2014-12-09 09:48:39 +0800

    debug http_server examples

--- examples/server/http_server.php
+++ examples/server/http_server.php
@@ -1,5 +1,5 @@
 <?php
-$http = new swoole_http_server("127.0.0.1", 9501, SWOOLE_BASE);
+$http = new swoole_http_server("127.0.0.1", 9501);
 //$http->set(['worker_num' => 4, 'task_worker_num' => 4]);
 $http->on('request', function ($request, $response) {
 //	var_dump($request->cookie);
