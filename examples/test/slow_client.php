a8722d8de1bb45f738788cfb8f3decd3cc2764fe
matyhtf <mikan.tenny@gmail.com>
2014-12-24 18:51:38 +0800

    Update test file.

---
+++ examples/test/slow_client.php
@@ -0,0 +1,18 @@
+<?php
+$client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_SYNC); //同步阻塞
+if(!$client->connect('127.0.0.1', 9501))
+{
+    exit("connect failed\n");
+}
+
+$data = $client->recv();
+echo "recv ".strlen($data)." bytes\n";
+sleep(1);
+
+for($i=0; $i<500; $i++)
+{
+    $data .= $client->recv();
+}
+echo "recv ".strlen($data)." bytes\n";
+
+sleep(1000);
