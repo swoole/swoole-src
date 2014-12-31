5b48219ce1877c7547644afb1a244fb3c887d1d2
matyhtf <mikan.tenny@gmail.com>
2014-12-31 18:51:35 +0800

    remove buffer overflow log.

--- src/network/ReactorThread.c
+++ src/network/ReactorThread.c
@@ -316,11 +316,9 @@ int swReactorThread_send2worker(void *data, int len, uint16_t target_worker_id)
 
             if (buffer->length > SwooleG.socket_buffer_size)
             {
-                swWarn("pipe buffer overflow, reactor will block.");
                 swYield();
                 swSocket_wait(pipe_fd, SW_SOCKET_OVERFLOW_WAIT, SW_EVENT_WRITE);
             }
-
             if (swBuffer_append(buffer, data, len) < 0)
             {
                 swWarn("append to pipe_buffer failed.");
