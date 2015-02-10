828806b1a442d5daec7490bc65bc6bfee96e9510
wenxuanguan <choose_home@126.com>
2015-02-10 09:53:38 +0800

    to pull request

--- examples/server/websocket_server.php
+++ examples/server/websocket_server.php
@@ -1,78 +1,27 @@
-<?php
-$http = new swoole_http_server("0.0.0.0", 9501);
-$http->set(['worker_num' => 4, 'user'=>'www', 'group'=>'www', 'daemonize'=>0]);
+<?
 
-$http->on('open', function($response) {  //handshake成功之后回调, 和js的onopen对应
-    echo "handshake success\n";
-    var_dump($response);
+$ser= new swoole_websocket_server("0.0.0.0", 9501);
+
+$ser-> set(array( 
+	"work_num" => 1
+));
+
+$ser-> on( 'open', function($ser, $fd)
+{
+	echo "server:shakehand success with fd{$fd}\r\n";
 });
 
-$http->on('message', function(swoole_websocket_frame $frame) use ($http) {
-    //var_dump($frame);
-    echo "fd:".$frame->fd . ", fin:".$frame->fin . ", opcode:".$frame->opcode."\n";
-    $frame->message("server send:".$frame->data);
-    $http->push($frame->fd, "hello, i am swoole http server.");
+$ser-> on( 'message', function( $ser, $fd, $data, $opcode, $fin)
+{
+	echo "receive from {$fd}:{$data},opcode:{$opcode},fin:{$fin}\r\n";
+	$ser -> push( $fd, "this is server", WEBSOCKET_OPCODE_TEXT, 1 );
 });
 
-$http->on('request', function ($request, swoole_http_response $response) {
-    $response->header('Content-Type', 'text/html; charset=utf-8');
-	$response->end(<<<HTML
-<!doctype html>
-<html>
-	<head>
-	    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-		<title>swoole websocket demo ...</title>
-	</head>
-	<body>
-		<script type="text/javascript">
-			var ws  = new WebSocket("ws://127.0.0.1:9501");
-			ws.onopen = function(){
-                console.log("连接服务器成功");
-                ws.send("i client");
-            };
-			ws.onmessage = function(e) {
-                console.log("From Server: "+e.data);
-            };
-			ws.onclose = function(){
-                console.log("服务器已关闭");
-            };
-		</script>
-
-	</body>
-</html>
-HTML
-);
+$ser-> on( 'close', function()
+{
+	echo "client closed\r\n";
 });
 
-$http->on('close', function(){
-    echo "on close\n";
-});
+$ser-> start();
 
-/*
-$http->on('handshake', function($request, $response) {  //自定定握手规则，没有设置则用系统内置的（只支持version:13的）
-            if (!isset($request->header['sec-websocket-key'])) {
-                    //'Bad protocol implementation: it is not RFC6455.'
-                    $response->end();
-                    return false;
-    }
-    if (0 === preg_match('#^[+/0-9A-Za-z]{21}[AQgw]==$#', $request->header['sec-websocket-key']) || 16 !== strlen(base64_decode($request->header['sec-websocket-key']))) {
-                    //Header Sec-WebSocket-Key is illegal;
-                    $response->end();
-                    return false;
-    }
-    $headers =  array(
-            'Upgrade' => 'websocket',
-            'Connection' => 'Upgrade',
-            'Sec-WebSocket-Accept' => ''. base64_encode(sha1($request->header['sec-websocket-key'] . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11', true)),
-            'Sec-WebSocket-Version' => '13',
-            'KeepAlive' => 'off',
-        );
-    foreach($headers as $key => $val) {
-                    $response->header($key, $val);
-                }
-    $response->status(101);
-    $response->end();
-});
-*/
-
-$http->start();
+?>
