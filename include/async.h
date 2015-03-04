3ccb9a560e5af0352413098d3d7152300ad720d6
matyhtf <mikan.tenny@gmail.com>
2015-03-04 16:58:03 +0800

    update license comment.

--- include/async.h
+++ include/async.h
@@ -1,8 +1,17 @@
 /*
- * async.h
- *
- *  Created on: 2014-3-21
- *      Author: htf
+ +----------------------------------------------------------------------+
+ | Swoole                                                               |
+ +----------------------------------------------------------------------+
+ | This source file is subject to version 2.0 of the Apache license,    |
+ | that is bundled with this package in the file LICENSE, and is        |
+ | available through the world-wide-web at the following url:           |
+ | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ | If you did not receive a copy of the Apache2.0 license and are unable|
+ | to obtain it through the world-wide-web, please send a note to       |
+ | license@swoole.com so we can mail you a copy immediately.            |
+ +----------------------------------------------------------------------+
+ | Author: Tianfeng Han  <mikan.tenny@gmail.com>                        |
+ +----------------------------------------------------------------------+
  */
 
 #ifndef _SW_ASYNC_H_
