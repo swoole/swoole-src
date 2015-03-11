b4f001d7340a60922a5e78f93ebe4a098b8ba15d
matyhtf <mikan.tenny@gmail.com>
2015-03-11 19:24:39 +0800

    added waitall example.

--- examples/recv_file.php
+++ examples/recv_file.php
@@ -8,6 +8,7 @@ else
 	$server_ip = $argv[1];
 }
 $cli = new swoole_client(SWOOLE_TCP);
+$start_ms = microtime(true);
 $cli->connect($server_ip, 9501, 5);
 $filesize = intval($cli->recv());
 if ($filesize == 0)
@@ -17,15 +18,28 @@ if ($filesize == 0)
 echo "file_size = $filesize\n";
 $content = '';
 $cli->send("get file");
-while(1)
+
+$use_waitall = false;
+
+if ($use_waitall)
 {
-	$content .= $cli->recv();
-    echo strlen($content)."\n";
-	if(strlen($content) == $filesize)
+	//waitall，需要一次性分配内存，适合小一点的文件
+	$content = $cli->recv($filesize, true);
+}
+else
+{
+	//循环接收，适合大型文件
+	while(1)
 	{
-        $i = time();
-		file_put_contents(__DIR__."/recv_file_{$i}.jpg", $content);
-		break;
+		//超大文件接收，这里需要改成分段写磁盘
+		$content .= $cli->recv();
+		if (strlen($content) == $filesize)
+		{
+			break;
+		}
 	}
 }
+file_put_contents(__DIR__."/recv_file_".time().".jpg", $content);
+echo "recv ".strlen($content)." byte data\n";
+echo "used ".((microtime(true) - $start_ms)*1000)."ms\n";
 $cli->close();
