3825c5ffe14fc463bcd52de321018f31552e7349
matyhtf <mikan.tenny@gmail.com>
2015-03-11 19:51:54 +0800

    update package.xml

--- examples/recv_file.php
+++ examples/recv_file.php
@@ -1,11 +1,11 @@
 <?php
 if (empty($argv[1]))
 {
-	$server_ip = '127.0.0.1';
+    $server_ip = '127.0.0.1';
 }
 else
 {
-	$server_ip = $argv[1];
+    $server_ip = $argv[1];
 }
 $cli = new swoole_client(SWOOLE_TCP);
 $start_ms = microtime(true);
@@ -13,7 +13,7 @@ $cli->connect($server_ip, 9501, 5);
 $filesize = intval($cli->recv());
 if ($filesize == 0)
 {
-	die("get file size failed.\n");
+    die("get file size failed.\n");
 }
 echo "file_size = $filesize\n";
 $content = '';
@@ -23,21 +23,21 @@ $use_waitall = false;
 
 if ($use_waitall)
 {
-	//waitall，需要一次性分配内存，适合小一点的文件
-	$content = $cli->recv($filesize, true);
+    //waitall，需要一次性分配内存，适合小一点的文件
+    $content = $cli->recv($filesize, true);
 }
 else
 {
-	//循环接收，适合大型文件
-	while(1)
-	{
-		//超大文件接收，这里需要改成分段写磁盘
-		$content .= $cli->recv();
-		if (strlen($content) == $filesize)
-		{
-			break;
-		}
-	}
+    //循环接收，适合大型文件
+    while(1)
+    {
+        //超大文件接收，这里需要改成分段写磁盘
+        $content .= $cli->recv();
+        if (strlen($content) == $filesize)
+        {
+            break;
+        }
+    }
 }
 file_put_contents(__DIR__."/recv_file_".time().".jpg", $content);
 echo "recv ".strlen($content)." byte data\n";
