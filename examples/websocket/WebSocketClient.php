191e2040649ea3abe38617de235bd741eabfc7bd
matyhtf <mikan.tenny@gmail.com>
2015-03-16 14:28:24 +0800

    update websocket examples.

--- examples/websocket/WebSocketClient.php
+++ examples/websocket/WebSocketClient.php
@@ -1,4 +1,5 @@
 <?php
+
 class WebSocketClient
 {
     const VERSION = '0.1.4';
@@ -16,6 +17,9 @@ class WebSocketClient
     private $host;
     private $port;
     private $path;
+    /**
+     * @var swoole_client
+     */
     private $socket;
     private $buffer = '';
     private $origin = null;
@@ -23,12 +27,13 @@ class WebSocketClient
      * @var bool
      */
     private $connected = false;
+
     /**
      * @param string $host
-     * @param int $port
+     * @param int    $port
      * @param string $path
      */
-    function __construct($host = '127.0.0.1', $port = 8080, $path = '/', $origin=null)
+    function __construct($host = '127.0.0.1', $port = 8080, $path = '/', $origin = null)
     {
         $this->host = $host;
         $this->port = $port;
@@ -36,6 +41,7 @@ class WebSocketClient
         $this->origin = $origin;
         $this->key = $this->generateToken(self::TOKEN_LENGHT);
     }
+
     /**
      * Disconnect on destruct
      */
@@ -43,25 +49,28 @@ class WebSocketClient
     {
         $this->disconnect();
     }
+
     /**
      * Connect client to server
      *
-     * @throws ConnectionException
      * @return $this
      */
     public function connect()
     {
         $this->socket = new \swoole_client(SWOOLE_SOCK_TCP);
-        if (!$this->socket->connect($this->host, $this->port)) {
+        if (!$this->socket->connect($this->host, $this->port))
+        {
             return false;
         }
         $this->socket->send($this->createHeader());
         return $this->recv();
     }
+
     public function getSocket()
     {
         return $this->socket;
     }
+
     /**
      * Disconnect from server
      */
@@ -70,31 +79,38 @@ class WebSocketClient
         $this->connected = false;
         $this->socket->close();
     }
+
     public function recv()
     {
         $data = $this->socket->recv();
-        if ($data === false) {
+        if ($data === false)
+        {
             echo "Error: {$this->socket->errMsg}";
             return false;
         }
         $this->buffer .= $data;
         $recv_data = $this->parseData($this->buffer);
-        if ($recv_data) {
+        if ($recv_data)
+        {
             $this->buffer = '';
             return $recv_data;
-        } else {
+        }
+        else
+        {
             return false;
         }
     }
+
     /**
-     * @param $data
+     * @param        $data
      * @param string $type
-     * @param bool $masked
+     * @param bool   $masked
      */
     public function send($data, $type = 'text', $masked = true)
     {
         return $this->socket->send($this->hybi10Encode($data, $type, $masked));
     }
+
     /**
      * Parse received data
      *
@@ -102,15 +118,22 @@ class WebSocketClient
      */
     private function parseData($response)
     {
-        if (!$this->connected && isset($response['Sec-Websocket-Accept'])) {
-            if (base64_encode(pack('H*', sha1($this->key . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'))) === $response['Sec-Websocket-Accept']) {
+        if (!$this->connected && isset($response['Sec-Websocket-Accept']))
+        {
+            if (base64_encode(pack('H*', sha1($this->key . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')))
+                === $response['Sec-Websocket-Accept']
+            )
+            {
                 $this->connected = true;
-            } else {
+            }
+            else
+            {
                 throw new \Exception("error response key.");
             }
         }
         return $this->hybi10Decode($response);
     }
+
     /**
      * Create header for websocket client
      *
@@ -119,7 +142,8 @@ class WebSocketClient
     private function createHeader()
     {
         $host = $this->host;
-        if ($host === '127.0.0.1' || $host === '0.0.0.0') {
+        if ($host === '127.0.0.1' || $host === '0.0.0.0')
+        {
             $host = 'localhost';
         }
         return "GET {$this->path} HTTP/1.1" . "\r\n" .
@@ -132,10 +156,12 @@ class WebSocketClient
         "Sec-WebSocket-Protocol: wamp" . "\r\n" .
         "Sec-WebSocket-Version: 13" . "\r\n" . "\r\n";
     }
+
     /**
      * Parse raw incoming data
      *
      * @param $header
+     *
      * @return array
      */
     private function parseIncomingRaw($header)
@@ -143,29 +169,46 @@ class WebSocketClient
         $retval = array();
         $content = "";
         $fields = explode("\r\n", preg_replace('/\x0D\x0A[\x09\x20]+/', ' ', $header));
-        foreach ($fields as $field) {
-            if (preg_match('/([^:]+): (.+)/m', $field, $match)) {
-                $match[1] = preg_replace_callback('/(?<=^|[\x09\x20\x2D])./', function ($matches) {
-                    return strtoupper($matches[0]);
-                }, strtolower(trim($match[1])));
-                if (isset($retval[$match[1]])) {
+        foreach ($fields as $field)
+        {
+            if (preg_match('/([^:]+): (.+)/m', $field, $match))
+            {
+                $match[1] = preg_replace_callback('/(?<=^|[\x09\x20\x2D])./',
+                    function ($matches)
+                    {
+                        return strtoupper($matches[0]);
+                    },
+                    strtolower(trim($match[1])));
+                if (isset($retval[$match[1]]))
+                {
                     $retval[$match[1]] = array($retval[$match[1]], $match[2]);
-                } else {
+                }
+                else
+                {
                     $retval[$match[1]] = trim($match[2]);
                 }
-            } else if (preg_match('!HTTP/1\.\d (\d)* .!', $field)) {
-                $retval["status"] = $field;
-            } else {
-                $content .= $field . "\r\n";
+            }
+            else
+            {
+                if (preg_match('!HTTP/1\.\d (\d)* .!', $field))
+                {
+                    $retval["status"] = $field;
+                }
+                else
+                {
+                    $content .= $field . "\r\n";
+                }
             }
         }
         $retval['content'] = $content;
         return $retval;
     }
+
     /**
      * Generate token
      *
      * @param int $length
+     *
      * @return string
      */
     private function generateToken($length)
@@ -173,7 +216,8 @@ class WebSocketClient
         $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"§$%&/()=[]{}';
         $useChars = array();
         // select some random chars:
-        for ($i = 0; $i < $length; $i++) {
+        for ($i = 0; $i < $length; $i++)
+        {
             $useChars[] = $characters[mt_rand(0, strlen($characters) - 1)];
         }
         // Add numbers
@@ -183,10 +227,12 @@ class WebSocketClient
         $randomString = substr($randomString, 0, self::TOKEN_LENGHT);
         return base64_encode($randomString);
     }
+
     /**
      * Generate token
      *
      * @param int $length
+     *
      * @return string
      */
     public function generateAlphaNumToken($length)
@@ -194,16 +240,19 @@ class WebSocketClient
         $characters = str_split('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789');
         srand((float)microtime() * 1000000);
         $token = '';
-        do {
+        do
+        {
             shuffle($characters);
             $token .= $characters[mt_rand(0, (count($characters) - 1))];
         } while (strlen($token) < $length);
         return $token;
     }
+
     /**
-     * @param $payload
+     * @param        $payload
      * @param string $type
-     * @param bool $masked
+     * @param bool   $masked
+     *
      * @return bool|string
      */
     private function hybi10Encode($payload, $type = 'text', $masked = true)
@@ -211,7 +260,8 @@ class WebSocketClient
         $frameHead = array();
         $frame = '';
         $payloadLength = strlen($payload);
-        switch ($type) {
+        switch ($type)
+        {
             case 'text':
                 // first byte indicates FIN, Text-Frame (10000001):
                 $frameHead[0] = 129;
@@ -230,51 +280,65 @@ class WebSocketClient
                 break;
         }
         // set mask and payload length (using 1, 3 or 9 bytes)
-        if ($payloadLength > 65535) {
+        if ($payloadLength > 65535)
+        {
             $payloadLengthBin = str_split(sprintf('%064b', $payloadLength), 8);
             $frameHead[1] = ($masked === true) ? 255 : 127;
-            for ($i = 0; $i < 8; $i++) {
+            for ($i = 0; $i < 8; $i++)
+            {
                 $frameHead[$i + 2] = bindec($payloadLengthBin[$i]);
             }
             // most significant bit MUST be 0 (close connection if frame too big)
-            if ($frameHead[2] > 127) {
+            if ($frameHead[2] > 127)
+            {
                 $this->close(1004);
                 return false;
             }
-        } elseif ($payloadLength > 125) {
+        }
+        elseif ($payloadLength > 125)
+        {
             $payloadLengthBin = str_split(sprintf('%016b', $payloadLength), 8);
             $frameHead[1] = ($masked === true) ? 254 : 126;
             $frameHead[2] = bindec($payloadLengthBin[0]);
             $frameHead[3] = bindec($payloadLengthBin[1]);
-        } else {
+        }
+        else
+        {
             $frameHead[1] = ($masked === true) ? $payloadLength + 128 : $payloadLength;
         }
         // convert frame-head to string:
-        foreach (array_keys($frameHead) as $i) {
+        foreach (array_keys($frameHead) as $i)
+        {
             $frameHead[$i] = chr($frameHead[$i]);
         }
-        if ($masked === true) {
+        if ($masked === true)
+        {
             // generate a random mask:
             $mask = array();
-            for ($i = 0; $i < 4; $i++) {
+            for ($i = 0; $i < 4; $i++)
+            {
                 $mask[$i] = chr(rand(0, 255));
             }
             $frameHead = array_merge($frameHead, $mask);
         }
         $frame = implode('', $frameHead);
         // append payload to frame:
-        for ($i = 0; $i < $payloadLength; $i++) {
+        for ($i = 0; $i < $payloadLength; $i++)
+        {
             $frame .= ($masked === true) ? $payload[$i] ^ $mask[$i % 4] : $payload[$i];
         }
         return $frame;
     }
+
     /**
      * @param $data
+     *
      * @return null|string
      */
     private function hybi10Decode($data)
     {
-        if (empty($data)) {
+        if (empty($data))
+        {
             return null;
         }
         $bytes = $data;
@@ -285,26 +349,40 @@ class WebSocketClient
         $secondByte = sprintf('%08b', ord($bytes[1]));
         $masked = ($secondByte[0] == '1') ? true : false;
         $dataLength = ($masked === true) ? ord($bytes[1]) & 127 : ord($bytes[1]);
-        if ($masked === true) {
-            if ($dataLength === 126) {
+        if ($masked === true)
+        {
+            if ($dataLength === 126)
+            {
                 $mask = substr($bytes, 4, 4);
                 $coded_data = substr($bytes, 8);
-            } elseif ($dataLength === 127) {
+            }
+            elseif ($dataLength === 127)
+            {
                 $mask = substr($bytes, 10, 4);
                 $coded_data = substr($bytes, 14);
-            } else {
+            }
+            else
+            {
                 $mask = substr($bytes, 2, 4);
                 $coded_data = substr($bytes, 6);
             }
-            for ($i = 0; $i < strlen($coded_data); $i++) {
+            for ($i = 0; $i < strlen($coded_data); $i++)
+            {
                 $decodedData .= $coded_data[$i] ^ $mask[$i % 4];
             }
-        } else {
-            if ($dataLength === 126) {
+        }
+        else
+        {
+            if ($dataLength === 126)
+            {
                 $decodedData = substr($bytes, 4);
-            } elseif ($dataLength === 127) {
+            }
+            elseif ($dataLength === 127)
+            {
                 $decodedData = substr($bytes, 10);
-            } else {
+            }
+            else
+            {
                 $decodedData = substr($bytes, 2);
             }
         }
