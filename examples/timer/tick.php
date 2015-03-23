b7f5959b21f083428a2dd8eb4e87d81e73af6cef
matyhtf <mikan.tenny@gmail.com>
2015-03-23 15:07:52 +0800

    update tick timer example.

--- examples/timer/tick.php
+++ examples/timer/tick.php
@@ -1,10 +1,13 @@
 <?php
-swoole_timer_tick(2000, function() {
+swoole_timer_tick(2000, function($id) {
 	echo "tick-1 2000ms\n";
+    var_dump($id);
 });
 
 usleep(500000);
 
-swoole_timer_tick(2000, function() {
-        echo "tick-2 2000ms\n";
-});
+swoole_timer_tick(2000, function($id, $params) {
+    echo "tick-2 2000ms\n";
+    var_dump($id, $params);
+    swoole_timer_clear($id);
+}, 2);
