86353417a52de8f74a80b1dc007491881324003f
matyhtf <mikan.tenny@gmail.com>
2015-05-13 16:29:14 +0800

    disable curl 100-continue.

--- examples/http/curl.php
+++ examples/http/curl.php
@@ -5,8 +5,9 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "http://127.0.0.1:9501");
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_POST, 1);//设置为POST方式
-curl_setopt($ch, CURLOPT_POSTFIELDS, array('test' => str_repeat('a', 100)));//POST数据
-//$cfile = curl_file_create(__DIR__ . '/client.php');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
+curl_setopt($ch, CURLOPT_POSTFIELDS, array('test' => str_repeat('a', 800000)));//POST数据
+//$cfile = curl_file_create(__DIR__ . '/test.ppt');
 //curl_setopt($ch, CURLOPT_POSTFIELDS, array('file' => $cfile));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 // 抓取URL并把它传递给浏览器
