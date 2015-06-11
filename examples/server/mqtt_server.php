1fed50228110365f286a5a200c1afd311847a538
haixingdev <cloverphp@foxmail.com>
2015-06-11 16:14:39 +0800

    Update mqtt_server.php
    修改conect ack的返回

--- examples/server/mqtt_server.php
+++ examples/server/mqtt_server.php
@@ -64,7 +64,7 @@ $serv->on('receive', function ($serv, $fd, $from_id, $data) {
 
         if ($header['type'] == 1)
         {
-            $resp = ord(32) . ord(2) . ord(0) . ord(0);
+            $resp = chr(32) . chr(2) . chr(0) . chr(0);//返回时候使用chr转换成二进制
             event_connect($header, substr($data, 2));
             $serv->send($fd, $resp);
         }
