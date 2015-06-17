f6efef8f66afe88e56a1ffe5768bec32d65fba8c
Lynnworld <lynnworld7@gmail.com>
2015-06-17 16:15:34 +0800

    fix memory leak in response->header

--- swoole_http.c
+++ swoole_http.c
@@ -1652,6 +1652,7 @@ static void http_build_header(swoole_http_client *client, zval *object, swString
         {
             swString_append_ptr(response, ZEND_STRL("Content-Type: text/html\r\n"));
         }
+        sw_zval_ptr_dtor(&header);
     }
     else
     {
