4b929568a9dae700cc3a5bfc92333c61d23f8ae3
matyhtf <mikan.tenny@gmail.com>
2016-01-05 19:37:44 +0800

    update example

--- examples/redis/client.php
+++ examples/redis/client.php
@@ -1,7 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: htf
- * Date: 16-1-5
- * Time: 下午5:18
- */
+$client = new swoole_redis;
+$client->connect('127.0.0.1', 6379, function (swoole_redis $client, $result) {
+    $client->execute('SET key swoole', function (swoole_redis $client, $result) {
+        var_dump($client, $result);
+        $client->execute('get key', function (swoole_redis $client, $result) {
+            var_dump($client, $result);
+            $client->close();
+        });
+    });
+});
