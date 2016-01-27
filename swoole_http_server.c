c3bbd57f9520a7d395dc0ac1cc04f7611b6cf248
viile <dakgzhu@foxmail.com>
2016-01-27 17:25:36 +0800

    format
    format

--- swoole_http_server.c
+++ swoole_http_server.c
@@ -780,25 +780,25 @@ static int multipart_body_on_data_end(multipart_parser* p)
 
 		if ((name[len-1] == ']') && (name[len-2] == '['))
 		{
-				zval *array_value;
+			zval *array_value;
 
-				if (sw_zend_hash_find(Z_ARRVAL_P(zpost), name, len + 1, (void **) &array_value) == FAILURE)
-				{
-						SW_MAKE_STD_ZVAL(array_value);
-						array_init(array_value);
-
-						add_assoc_zval(zpost, name, array_value);
-						add_next_index_string(array_value, swoole_http_form_data_buffer->str);
-				}
-				else
-				{
-						add_next_index_string(array_value, swoole_http_form_data_buffer->str);
-				}
+			if (sw_zend_hash_find(Z_ARRVAL_P(zpost), name, len + 1, (void **) &array_value) == FAILURE)
+			{
+				SW_MAKE_STD_ZVAL(array_value);
+				array_init(array_value);
+				
+				add_assoc_zval(zpost, name, array_value);
+				add_next_index_string(array_value, swoole_http_form_data_buffer->str);
+			}
+			else
+			{
+				add_next_index_string(array_value, swoole_http_form_data_buffer->str);
+			}
 		}
 		else
 		{
-				sw_add_assoc_stringl_ex(zpost, client->current_form_data_name, client->current_form_data_name_len + 1,
-						swoole_http_form_data_buffer->str, swoole_http_form_data_buffer->length, 1);
+			sw_add_assoc_stringl_ex(zpost, client->current_form_data_name, client->current_form_data_name_len + 1,
+				swoole_http_form_data_buffer->str, swoole_http_form_data_buffer->length, 1);
 		}
 		
         efree(client->current_form_data_name);
