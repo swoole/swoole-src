0901a4669a48177e9bd7e6b68c2669ba8cb84e12
matyhtf <mikan.tenny@gmail.com>
2016-01-27 14:55:02 +0800

    update example files.

--- examples/redis/client.php
+++ examples/redis/client.php
@@ -2,12 +2,14 @@
 $client = new swoole_redis;
 $client->connect('127.0.0.1', 6379, function (swoole_redis $client, $result) {
     echo "connect\n";
-    var_dump($result);
     $client->set('key', 'swoole', function (swoole_redis $client, $result) {
-        var_dump($result);
+        echo "set key OK\n";
         $client->get('key', function (swoole_redis $client, $result) {
             var_dump($result);
-            $client->close();
+            $client->keys('*', function (swoole_redis $client, $result) {
+                var_dump($result);
+                $client->close();
+            });
         });
     });
 });
