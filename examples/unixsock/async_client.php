4b2e3bbda8c04238e356fb4dce4e42eed897b345
matyhtf <mikan.tenny@gmail.com>
2016-04-11 15:59:03 +0800

    update unixsock examples.

--- examples/unixsock/async_client.php
+++ examples/unixsock/async_client.php
@@ -1,7 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: htf
- * Date: 16-4-11
- * Time: 上午10:14
- */
+$client = new swoole_client(SWOOLE_SOCK_UNIX_STREAM, SWOOLE_SOCK_ASYNC);
+
+$client->on("connect", function (swoole_client $cli)
+{
+    $cli->send("GET / HTTP/1.1\r\n\r\n");
+});
+
+$client->on("receive", function (swoole_client $cli, $data)
+{
+    echo "Receive: $data";
+    $cli->send(str_repeat('A', 100) . "\n");
+});
+
+$client->on("error", function (swoole_client $cli)
+{
+    echo "error: [" . $cli->errCode . "] " . socket_strerror($cli->errCode) . "\n";
+});
+
+$client->on("close", function (swoole_client $cli)
+{
+    echo "Connection close\n";
+});
+
+$client->connect(__DIR__ . '/svr.sock', 0, -1);
+
+swoole_event_wait();
+echo "exit\n";
