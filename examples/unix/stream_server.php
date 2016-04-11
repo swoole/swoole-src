34e96ce6276bb8525f6fa021b04f191a2b8942d3
chunfang <xuchunfang@qufenqi.com>
2016-04-11 11:10:25 +0800

    fixed wrong constant name

--- examples/unix/stream_server.php
+++ examples/unix/stream_server.php
@@ -1,5 +1,5 @@
 <?php
-$serv = new swoole_server(__DIR__."/svr.sock", 9501, SWOOLE_BA, SWOOLE_SOCK_UNIX_STREAM);
+$serv = new swoole_server(__DIR__."/svr.sock", 9501, SWOOLE_BASE, SWOOLE_SOCK_UNIX_STREAM);
 $serv->set(array(
     //'tcp_defer_accept' => 5,
     'worker_num' => 1,
