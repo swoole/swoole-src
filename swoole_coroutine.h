0d88853f926e378c59dcbec80a218fe4c8380001
xyzhu <xyzhu1120@gmail.com>
2016-04-19 02:27:11 -0700

    refactor coroutine code and try to release the stack at the last

---
+++ swoole_coroutine.h
@@ -0,0 +1,46 @@
+/*
+ +----------------------------------------------------------------------+
+ | Swoole                                                               |
+ +----------------------------------------------------------------------+
+ | Copyright (c) 2012-2015 The Swoole Group                             |
+ +----------------------------------------------------------------------+
+ | This source file is subject to version 2.0 of the Apache license,    |
+ | that is bundled with this package in the file LICENSE, and is        |
+ | available through the world-wide-web at the following url:           |
+ | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ | If you did not receive a copy of the Apache2.0 license and are unable|
+ | to obtain it through the world-wide-web, please send a note to       |
+ | license@swoole.com so we can mail you a copy immediately.            |
+ +----------------------------------------------------------------------+
+ | Author: Tianfeng Han  <mikan.tenny@gmail.com>                        |
+ +----------------------------------------------------------------------+
+ */
+
+#ifndef SWOOLE_COROUTINE_H_
+#define SWOOLE_COROUTINE_H_
+
+#define php_context _php_context
+
+typedef struct
+{
+    zval **current_return_value_ptr_ptr;
+    zval *current_return_value_ptr;
+    zend_execute_data *current_execute_data;
+    zend_op **current_opline_ptr;
+    zend_op *current_opline;
+    zend_execute_data *prev_execute_data;
+    zend_op_array *current_active_op_array;
+    HashTable *current_active_symbol_table;
+    zval *current_this;
+    zend_class_entry *current_scope;
+    zend_class_entry *current_called_scope;
+    zend_vm_stack current_vm_stack;
+} _php_context;
+
+zend_execute_data *coro_create(zend_op_array *op_array, zval **argv, int argc);
+
+void coro_close();
+php_context *coro_save(zval *return_value, zval **return_value_ptr);
+void coro_resume(php_context *sw_current_context, zval *retval);
+
+#endif
