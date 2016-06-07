f2608c7bacf970995485dba2e50f4d854a385507
matyhtf <mikan.tenny@gmail.com>
2016-06-07 15:48:59 +0800

    update swoole_mysql example.

--- examples/mysql/real_async.php
+++ examples/mysql/real_async.php
@@ -1,32 +1,36 @@
 <?php
 $db = new swoole_mysql;
-
-$r = $db->connect(array(
-    'host' => '192.168.56.102',
-    'user' => 'test',
-    'password' => 'test',
-    'database' => 'test',
-));
-
-if ($r === false) {
-    var_dump($db->errno, $db->error);
-    die;
-}
-$sql = 'show tables';
-//$sql = "INSERT INTO `test`.`userinfo` (`id`, `name`, `passwd`, `regtime`, `lastlogin_ip`) VALUES (NULL, 'jack', 'xuyou', CURRENT_TIMESTAMP, '');";
-$db->query($sql, function(swoole_mysql $db, $r) {
-    global $s;
-    if ($r === false)
+$server = array(
+    'host' => '127.0.0.1',
+    'user' => 'chelun_appuser',
+    'password' => 'hGbxB9PrmVLJzuxE',
+    'database' => 'chelun',
+);
+$r = $db->connect($server, function ($db, $result)
+{
+    if ($result === false)
     {
-        var_dump($db->error, $db->errno);
+        var_dump($db->connect_errno, $db->connect_error);
+        die;
     }
-    elseif ($r === true )
+    echo "connect to mysql server sucess\n";
+    $sql = 'show tables';
+    //$sql = "INSERT INTO `test`.`userinfo` (`id`, `name`, `passwd`, `regtime`, `lastlogin_ip`) VALUES (NULL, 'jack', 'xuyou', CURRENT_TIMESTAMP, '');";
+    $db->query($sql, function (swoole_mysql $db, $r)
     {
-        var_dump($db->affected_rows, $db->insert_id);
-    }
-    echo "count=".count($r).", time=".(microtime(true) - $s), "\n";
-    var_dump($r);
-    $db->close();
+        global $s;
+        if ($r === false)
+        {
+            var_dump($db->error, $db->errno);
+        }
+        elseif ($r === true)
+        {
+            var_dump($db->affected_rows, $db->insert_id);
+        }
+        echo "count=" . count($r) . ", time=" . (microtime(true) - $s), "\n";
+        //var_dump($r);
+        $db->close();
+    });
 });
 
 //$db->connect('10.10.2.205', 'root', '', 'msg_push', 3500);
