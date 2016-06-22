527a4be8151af5628c09e7055e3b7e17590d6d30
matyhtf <mikan.tenny@gmail.com>
2016-06-22 13:53:32 +0800

    revert

--- swoole_server.c
+++ swoole_server.c
@@ -2242,6 +2242,7 @@ PHP_METHOD(swoole_server, taskwait)
 
     if (swProcessPool_dispatch_blocking(&SwooleGS->task_workers, &buf, (int*) &dst_worker_id) >= 0)
     {
+        sw_atomic_fetch_add(&SwooleStats->tasking_num, 1);
         task_notify_pipe->timeout = timeout;
         int ret = task_notify_pipe->read(task_notify_pipe, &notify, sizeof(notify));
         if (ret > 0)
