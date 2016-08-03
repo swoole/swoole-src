c7f27a8ead384fb9af78d55b96a4cea36ed69a39
matyhtf <mikan.tenny@gmail.com>
2016-08-03 18:45:09 +0800

    fix swoole_http_request bugs.

--- swoole_http_server.c
+++ swoole_http_server.c
@@ -2190,7 +2190,7 @@ static PHP_METHOD(swoole_http_response, header)
     }
     if (vlen > SW_HTTP_HEADER_VALUE_SIZE)
     {
-        swoole_php_error(E_WARNING, "header key is too long.");
+        swoole_php_error(E_WARNING, "value key is too long.");
         RETURN_FALSE;
     }
     char key_buf[SW_HTTP_HEADER_KEY_SIZE];
