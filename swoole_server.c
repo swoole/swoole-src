e516af2afadc7c3b6577529695d392fde85a26f9
roketyyang@tencent.com <roketyyang@localhost.localdomain>
2016-09-07 19:36:21 +0800

    fix reuse port

--- swoole_server.c
+++ swoole_server.c
@@ -1608,7 +1608,7 @@ PHP_METHOD(swoole_server, set)
         serv->enable_unsafe_event = Z_BVAL_P(v);
     }
     //port reuse
-    if (php_swoole_array_get_value(vht, "enable_port_reuse", v))
+    if (php_swoole_array_get_value(vht, "enable_reuse_port", v))
     {
         convert_to_boolean(v);
         SwooleG.reuse_port = Z_BVAL_P(v);
