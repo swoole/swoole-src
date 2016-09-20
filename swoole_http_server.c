77fd3c0c34b9e53b8bdeadef01b97d39bf39a99c
matyhtf <mikan.tenny@gmail.com>
2016-09-20 15:30:10 +0800

    http_response_compress use emalloc.

--- swoole_http_server.c
+++ swoole_http_server.c
@@ -1729,6 +1729,16 @@ static void http_build_header(http_context *ctx, zval *object, swString *respons
 }
 
 #ifdef SW_HAVE_ZLIB
+static voidpf php_zlib_alloc(voidpf opaque, uInt items, uInt size)
+{
+    return (voidpf)safe_emalloc(items, size, 0);
+}
+
+static void php_zlib_free(voidpf opaque, voidpf address)
+{
+    efree((void*)address);
+}
+
 static int http_response_compress(swString *body, int level)
 {
     assert(level > 0 || level < 10);
@@ -1751,6 +1761,9 @@ static int http_response_compress(swString *body, int level)
 #endif
 
     int status;
+    zstream.zalloc = php_zlib_alloc;
+    zstream.zfree = php_zlib_free;
+
     if (Z_OK == deflateInit2(&zstream, level, Z_DEFLATED, encoding, MAX_MEM_LEVEL, Z_DEFAULT_STRATEGY))
     {
         zstream.next_in = (Bytef *) body->str;
