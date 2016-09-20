863e0b995c2dd02b5b1fdf289ab61ed8638c4858
matyhtf <mikan.tenny@gmail.com>
2016-09-20 16:16:02 +0800

    fix task_ipc_mode bug

--- swoole_server.c
+++ swoole_server.c
@@ -2248,7 +2248,7 @@ PHP_METHOD(swoole_server, stats)
     sw_add_assoc_long_ex(return_value, ZEND_STRS("request_count"), SwooleStats->request_count);
     sw_add_assoc_long_ex(return_value, ZEND_STRS("worker_request_count"), SwooleWG.request_count);
 
-    if (SwooleG.task_ipc_mode > SW_IPC_UNSOCK && SwooleGS->task_workers.queue)
+    if (SwooleG.task_ipc_mode > SW_TASK_IPC_UNIXSOCK && SwooleGS->task_workers.queue)
     {
         int queue_num = -1;
         int queue_bytes = -1;
