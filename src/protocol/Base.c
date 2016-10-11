80b7e4f860134cd82ebfb2f5241064d64941e790
matyhtf <mikan.tenny@gmail.com>
2016-10-11 11:30:25 +0800

    code optimization.

--- src/protocol/Base.c
+++ src/protocol/Base.c
@@ -135,8 +135,8 @@ int swProtocol_recv_check_length(swProtocol *protocol, swConnection *conn, swStr
         recv_size = protocol->package_length_offset + protocol->package_length_size;
     }
 
-    int ret = swConnection_recv(conn, buffer->str + buffer->length, recv_size, 0);
-    if (ret < 0)
+    int n = swConnection_recv(conn, buffer->str + buffer->length, recv_size, 0);
+    if (n < 0)
     {
         switch (swConnection_error(errno))
         {
@@ -150,21 +150,20 @@ int swProtocol_recv_check_length(swProtocol *protocol, swConnection *conn, swStr
             return SW_OK;
         }
     }
-    else if (ret == 0)
+    else if (n == 0)
     {
         return SW_ERR;
     }
     else
     {
-        buffer->length += ret;
+        buffer->length += n;
 
         if (conn->recv_wait)
         {
             if (buffer->length >= buffer->offset)
             {
                 do_dispatch:
-                ret = protocol->onPackage(conn, buffer->str, buffer->offset);
-                if (ret < 0)
+                if (protocol->onPackage(conn, buffer->str, buffer->offset) < 0)
                 {
                     return SW_ERR;
                 }
@@ -248,7 +247,6 @@ int swProtocol_recv_check_eof(swProtocol *protocol, swConnection *conn, swString
 {
     int recv_again = SW_FALSE;
     int buf_size;
-    int ret;
 
     recv_data: buf_size = buffer->size - buffer->length;
     char *buf_ptr = buffer->str + buffer->length;
@@ -300,8 +298,7 @@ int swProtocol_recv_check_eof(swProtocol *protocol, swConnection *conn, swString
         }
         else if (memcmp(buffer->str + buffer->length - protocol->package_eof_len, protocol->package_eof, protocol->package_eof_len) == 0)
         {
-            ret = protocol->onPackage(conn, buffer->str, buffer->length);
-            if (ret < 0)
+            if (protocol->onPackage(conn, buffer->str, buffer->length) < 0)
             {
                 return SW_ERR;
             }
