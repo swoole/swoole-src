cbbf744b27b26dfef55d52cd21ba268f943e1f1e
yangxikun <yangrokety@gmail.com>
2016-11-06 00:14:13 +0800

    upgrade redis client

--- swoole_redis_coro.c
+++ swoole_redis_coro.c
@@ -45,28 +45,6 @@ inline_zend_get_parameters_array(int ht, int param_count, zval *argument_array T
     efree(zv);
     return ret;
 }
-
-#define ZEND_HASH_FOREACH_KEY_VAL(ht, _h, _key, _val) do { \
-    HashPosition _hpos; \
-    for (zend_hash_internal_pointer_reset_ex(ht, &_hpos); \
-         (_val = zend_hash_get_current_data_ex(ht, &_hpos)) != NULL; \
-         zend_hash_move_forward_ex(ht, &_hpos) \
-    ) { \
-        zend_string _zstr = {0}; \
-        char *_str_index; uint _str_length; ulong _num_index; \
-        switch (zend_hash_get_current_key_ex(ht, &_str_index, &_str_length, &_num_index, 0, &_hpos)) { \
-            case HASH_KEY_IS_STRING: \
-                _zstr.len = _str_length; \
-                _zstr.val = _str_index; \
-                _key = &_zstr; \
-                break; \
-            case HASH_KEY_IS_LONG: \
-                _key = NULL; \
-                _h = _num_index; \
-                break; \
-            default: \
-                continue; \
-        }
 #endif
 
 #define SW_REDIS_COMMAND_BUFFER_SIZE   64
