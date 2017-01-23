cbcde3ae52ec536a5e3a3685c437d77ae3d22577
matyhtf <mikan.tenny@gmail.com>
2017-01-23 17:11:42 +0800

    revert some commits.

--- include/Client.h
+++ include/Client.h
@@ -102,7 +102,7 @@ typedef struct _swClient
     void (*onBufferFull)(struct _swClient *cli);
     void (*onBufferEmpty)(struct _swClient *cli);
 
-    int (*connect)(struct _swClient *cli, char *hosts, int port, double _timeout, int sock_flag);
+    int (*connect)(struct _swClient *cli, char *host, int port, double _timeout, int sock_flag);
     int (*send)(struct _swClient *cli, char *data, int length, int flags);
     int (*sendfile)(struct _swClient *cli, char *filename, off_t offset);
     int (*recv)(struct _swClient *cli, char *data, int len, int flags);
