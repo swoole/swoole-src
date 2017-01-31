c4e60c4479527782c2c681a473bc79828dcde095
Artem Naluzhnyy <Artem.Naluzhnyy@gmail.com>
2017-01-31 13:33:41 +0200

    Fix: socket fd leak
    Reference: Coverity CID #1397912

--- src/network/ReactorProcess.c
+++ src/network/ReactorProcess.c
@@ -548,6 +548,7 @@ static int swReactorProcess_reuse_port(swListenPort *ls)
     //bind address and port
     if (swSocket_bind(sock, ls->type, ls->host, &ls->port) < 0)
     {
+        close(sock);
         return SW_ERR;
     }
     //stream socket, set nonblock
