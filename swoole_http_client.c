d6498aaf593af6659e8b5cf943946dd38667496f
matyhtf <mikan.tenny@gmail.com>
2017-02-05 10:52:01 +0800

    fix memory leak in http client  after the separation of array.

--- swoole_http_client.c
+++ swoole_http_client.c
@@ -1113,6 +1113,7 @@ static PHP_METHOD(swoole_http_client, set)
     }
     php_swoole_array_separate(zset);
     zend_update_property(swoole_http_client_class_entry_ptr, getThis(), ZEND_STRL("setting"), zset TSRMLS_CC);
+    sw_zval_ptr_dtor(&zset);
     RETURN_TRUE;
 }
 
