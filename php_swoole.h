11d4c5300d9beb7d04198d90fa4af4394a159274
matyhtf <mikan.tenny@gmail.com>
2017-02-13 20:02:42 +0800

    Serialize can only be used on PHP7.

--- php_swoole.h
+++ php_swoole.h
@@ -422,8 +422,10 @@ void php_swoole_onBufferFull(swServer *, swDataHead *);
 void php_swoole_onBufferEmpty(swServer *, swDataHead *);
 int php_swoole_length_func(swProtocol *protocol, swConnection *conn, char *data, uint32_t length);
 
+#if PHP_MAJOR_VERSION >= 7
 PHPAPI zend_string* php_swoole_serialize(zval *zvalue);
 PHPAPI int php_swoole_unserialize(void *buffer, size_t len, zval *return_value, zval *object_args);
+#endif
 
 static sw_inline zval* php_swoole_server_get_callback(swServer *serv, int server_fd, int event_type)
 {
