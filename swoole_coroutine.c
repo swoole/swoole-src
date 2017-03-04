7c988f0b9bf110709e984af6c08c94061beebc85
xyzhu <xyzhu1120@gmail.com>
2017-03-03 20:41:24 -0800

    fix bug of checking closure when creating coro

--- swoole_coroutine.c
+++ swoole_coroutine.c
@@ -229,7 +229,7 @@ int sw_coro_create(zend_fcall_info_cache *fci_cache, zval **argv, int argc, zval
     object = (func->common.fn_flags & ZEND_ACC_STATIC) ? NULL : fci_cache->object;
     call = zend_vm_stack_push_call_frame(ZEND_CALL_TOP_FUNCTION | ZEND_CALL_ALLOCATED, fci_cache->function_handler, argc, fci_cache->called_scope, object);
 
-    if (unlikely(op_array->fn_flags | ZEND_ACC_CLOSURE)) {
+    if (unlikely(op_array->fn_flags & ZEND_ACC_CLOSURE)) {
         GC_REFCOUNT((zend_object*)op_array->prototype)++;
         ZEND_ADD_CALL_FLAG(call, ZEND_CALL_CLOSURE);
     }
