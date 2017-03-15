4c15a5a0d487f230febb19fd1193c352d4a9e8ab
matyhtf <mikan.tenny@gmail.com>
2017-03-15 18:13:02 +0800

    bzero object.

--- src/pipe/PipeUnsock.c
+++ src/pipe/PipeUnsock.c
@@ -93,6 +93,7 @@ int swPipeUnsock_create(swPipe *p, int blocking, int protocol)
         swWarn("malloc() failed.");
         return SW_ERR;
     }
+    bzero(object, sizeof(swPipeUnsock));
     p->blocking = blocking;
     ret = socketpair(AF_UNIX, protocol, 0, object->socks);
     if (ret < 0)
