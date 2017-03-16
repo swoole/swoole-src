282df3f14e2ed07462d11a10c3e92e2866f0b84c
Gxhua <393323503@qq.com>
2017-03-16 17:45:19 +0800

    fix compile error for cpp_moulde (enum mysql_field_types)

--- swoole_mysql.h
+++ swoole_mysql.h
@@ -501,7 +501,7 @@ static sw_inline int mysql_decode_field(char *buf, int len, mysql_field *col)
     i += 4;
 
     /* type */
-    col->type = (mysql_field_types) buf[i];
+    col->type = (enum mysql_field_types) buf[i];
     i += 1;
 
     /* flags */
