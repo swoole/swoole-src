9580b256a7739ee4ef862cb39fcde9534cd2b288
guoxinhua <guoxinhua@10.10.12.142>
2017-05-23 13:10:51 +0800

    add test

--- swoole_serialize.c
+++ swoole_serialize.c
@@ -578,6 +578,11 @@ static void* swoole_unserialize_arr(void *buffer, zval *zvalue, uint32_t nNumOfE
     {
         return NULL;
     }
+    if (!buffer)
+    {
+        php_error_docref(NULL TSRMLS_CC, E_NOTICE, "illegal unserialize data");
+        return NULL;
+    }
     ZVAL_NEW_ARR(zvalue);
 
     //Initialize buckets
