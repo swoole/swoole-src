a8b4f70f3e852c5a77cfa03708e0b57b0ae50237
fress <gary@100tal.com>
2017-08-14 17:32:37 +0800

    增加自动创建task_tmpdir和upload_tmp_dir目录功能

--- swoole_server.c
+++ swoole_server.c
@@ -61,6 +61,56 @@ static void php_swoole_onManagerStop(swServer *serv);
 
 static zval* php_swoole_server_add_port(swListenPort *port TSRMLS_DC);
 
+int php_swoole_create_dir(const char* path)
+{
+    if(access(path, F_OK) != 0)
+    {
+        int     startpach;
+        int     endpath;
+        int     i            = 0;
+        int     pathlen      = strlen(path);
+        char    curpath[128] = {0};
+        if ('/' != path[0])
+        {
+            getcwd(curpath, sizeof(curpath));
+            strcat(curpath, "/");
+            startpach = strlen(curpath);
+            strcat(curpath, path);
+            if(path[pathlen] != '/')
+            {
+                strcat(curpath, "/");
+            }
+            endpath = strlen(curpath);
+        }
+        else
+        {
+            strcpy(curpath, path);
+            if(path[pathlen] != '/')
+            {
+                strcat(curpath, "/");
+            }
+            startpach    = 1;
+            endpath      = strlen(curpath);
+        }
+        for(i = startpach; i < endpath ; i++ )
+        {  
+            if('/' == curpath[i])
+            {
+                curpath[i] = '\0';
+                if(access(curpath, F_OK) != 0)
+                {
+                    if(mkdir(curpath, 0755) == -1)
+                    {  
+                        return -1;
+                    }
+                }
+                curpath[i] = '/';
+            }
+        }
+    }
+    return 0;
+}
+
 int php_swoole_task_pack(swEventData *task, zval *data TSRMLS_DC)
 {
     smart_str serialized_data = { 0 };
@@ -1716,6 +1766,8 @@ PHP_METHOD(swoole_server, set)
     if (php_swoole_array_get_value(vht, "task_tmpdir", v))
     {
         convert_to_string(v);
+        php_swoole_create_dir(Z_STRVAL_P(v));
+
         if (Z_STRLEN_P(v) > SW_TASK_TMPDIR_SIZE - 30)
         {
             swoole_php_fatal_error(E_ERROR, "task_tmpdir is too long, the max size is %d.", SW_TASK_TMPDIR_SIZE - 1);
@@ -1817,6 +1869,8 @@ PHP_METHOD(swoole_server, set)
     if (php_swoole_array_get_value(vht, "upload_tmp_dir", v))
     {
         convert_to_string(v);
+        php_swoole_create_dir(Z_STRVAL_P(v));
+
         if (Z_STRLEN_P(v) >= SW_HTTP_UPLOAD_TMPDIR_SIZE - 22)
         {
             swoole_php_fatal_error(E_ERROR, "option upload_tmp_dir [%s] is too long.", Z_STRVAL_P(v));
