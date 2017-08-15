f1038815d5d27bc81a349805f427297227af802a
matyhtf <mikan.tenny@gmail.com>
2017-08-15 14:57:48 +0800

    warning free.

--- src/network/TaskWorker.c
+++ src/network/TaskWorker.c
@@ -267,7 +267,7 @@ int swTaskWorker_finish(swServer *serv, char *data, int data_len, int flags)
         {
             ret = task_notify_pipe->write(task_notify_pipe, &flag, sizeof(flag));
 #ifdef HAVE_KQUEUE
-            if (ret < 0 && errno == EAGAIN || errno == ENOBUFS)
+            if (ret < 0 && (errno == EAGAIN || errno == ENOBUFS))
 #else
             if (ret < 0 && errno == EAGAIN)
 #endif
