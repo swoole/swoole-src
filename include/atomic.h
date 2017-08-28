f6ef1f9be6285b3a251f6cf28e21c491c6fd6317
matyhtf <mikan.tenny@gmail.com>
2017-08-28 16:24:25 +0800

    code optimization.

--- include/atomic.h
+++ include/atomic.h
@@ -1,23 +1,23 @@
 #ifndef SW_ATOMIC_H_
 #define SW_ATOMIC_H_
 
-typedef volatile int32_t              sw_atomic_int32_t;
-typedef volatile uint32_t             sw_atomic_uint32_t;
+typedef volatile int32_t                  sw_atomic_int32_t;
+typedef volatile uint32_t                 sw_atomic_uint32_t;
 
 #ifdef __x86_64__
-typedef volatile int64_t              sw_atomic_int64_t;
-typedef volatile uint64_t             sw_atomic_uint64_t;
+typedef volatile int64_t                  sw_atomic_int64_t;
+typedef volatile uint64_t                 sw_atomic_uint64_t;
 #endif
 
 #ifdef __x86_64__
-typedef sw_atomic_int64_t             sw_atomic_long_t;
-typedef sw_atomic_uint64_t            sw_atomic_ulong_t;
+typedef sw_atomic_int64_t                 sw_atomic_long_t;
+typedef sw_atomic_uint64_t                sw_atomic_ulong_t;
 #else
-typedef atomic_int32_t                sw_atomic_long_t;
-typedef atomic_uint32_t               sw_atomic_ulong_t;
+typedef atomic_int32_t                    sw_atomic_long_t;
+typedef atomic_uint32_t                   sw_atomic_ulong_t;
 #endif
 
-typedef sw_atomic_uint32_t             sw_atomic_t;
+typedef sw_atomic_uint32_t                sw_atomic_t;
 
 #define sw_atomic_cmp_set(lock, old, set) __sync_bool_compare_and_swap(lock, old, set)
 #define sw_atomic_fetch_add(value, add)   __sync_fetch_and_add(value, add)
@@ -34,6 +34,10 @@ typedef sw_atomic_uint32_t             sw_atomic_t;
 #define sw_atomic_cpu_pause()
 #endif
 
+#if 0
 #define sw_spinlock_release(lock)         __sync_lock_release(lock)
+#else
+#define sw_spinlock_release(lock)         *(lock) = 0
+#endif
 
 #endif
