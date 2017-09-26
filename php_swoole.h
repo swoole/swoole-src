8d5511f43d6622182266098b9f1ad1b336b56e53
matyhtf <mikan.tenny@gmail.com>
2017-09-26 17:28:38 +0800

    update version code.

--- php_swoole.h
+++ php_swoole.h
@@ -50,7 +50,7 @@
 #include "Client.h"
 #include "async.h"
 
-#define PHP_SWOOLE_VERSION  "2.0.9"
+#define PHP_SWOOLE_VERSION  "2.0.10"
 #define PHP_SWOOLE_CHECK_CALLBACK
 #define PHP_SWOOLE_ENABLE_FASTCALL
 
