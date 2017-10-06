63cdb24b049699cab906e26d820be5fef540ab9a
Paul <yellowrna@gmail.com>
2017-10-06 17:31:38 +0800

    test 222

---
+++ hgc_test.php
@@ -0,0 +1,9 @@
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+echo 111;
+exit;
+
