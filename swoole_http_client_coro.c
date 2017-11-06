38f7dd97efae52f13fef43d2542e4dabff0a8080
matyhtf <mikan.tenny@gmail.com>
2017-11-06 10:15:15 +0800

    fix http_client_coro double free.

--- swoole_http_client_coro.c
+++ swoole_http_client_coro.c
@@ -242,7 +242,6 @@ static void http_client_coro_onTimeout(swTimer *timer, swTimer_node *tnode)
 
     http_client *http = swoole_get_object(zobject);
     http->timer = NULL;
-    http_client_free(zobject TSRMLS_CC);
 
     //define time out RETURN ERROR  110
     zend_update_property_long(swoole_http_client_coro_class_entry_ptr, zobject, ZEND_STRL("errCode"), ETIMEDOUT TSRMLS_CC);
