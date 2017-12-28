ec915cd2baf982ff618625ebb1106d20cbaca660
matyhtf <mikan.tenny@gmail.com>
2017-12-28 18:32:59 +0800

    added test-all-version.sh script.

---
+++ tests/test-all-version.sh
@@ -0,0 +1,23 @@
+to_php7.sh
+cd ../
+./make.sh
+cd tests/
+./start.sh
+cd ../
+./make.sh
+cd tests/
+to_php72.sh
+cd ../
+./make.sh
+cd tests/
+./start.sh
+to_php56.sh
+cd ../
+./make.sh
+cd tests/
+./start.sh
+to_php55.sh
+cd ../
+./make.sh
+cd tests/
+./start.sh
