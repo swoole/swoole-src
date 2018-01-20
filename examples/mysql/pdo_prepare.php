266cbd9a7964ea4be0648b60e7da755949e09616
matyhtf <mikan.tenny@gmail.com>
2018-01-20 18:31:01 +0800

    update pdo_prepare.php

--- examples/mysql/pdo_prepare.php
+++ examples/mysql/pdo_prepare.php
@@ -1,4 +1,6 @@
 <?php
-$db = new PDO("dbtype:host=127.0.0.1;dbname=test;charset=utf8", "root" ,"root");
-$query=$db->prepare("Select * from table where id=?");
-$query->excute(array($myinsecuredata));
+$db = new PDO("mysql:host=127.0.0.1;dbname=test;charset=utf8", "root" ,"root");
+$query = $db->prepare("select * from userinfo where id=?");
+$rs = $query->execute(array(1));
+var_dump($rs);
+echo count($query->fetchAll());
