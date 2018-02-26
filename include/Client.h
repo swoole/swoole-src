05ac5d8deeab5839d776d914ffb3694b3294d9c7
matyhtf <mikan.tenny@gmail.com>
2018-02-26 18:43:27 +0800

    added cli->close_defer

--- include/Client.h
+++ include/Client.h
@@ -80,6 +80,7 @@ typedef struct _swClient
     uint32_t shutdow_rw :1;
     uint32_t shutdown_read :1;
     uint32_t shutdown_write :1;
+    uint32_t close_defer :1;
 
     /**
      * one package: length check
