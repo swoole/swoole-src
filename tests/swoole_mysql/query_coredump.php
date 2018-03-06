1a87a1f92752d86116c130f5cde578344fd69284
GXhua <393323503@qq.com>
2018-03-06 15:29:48 +0800

    remove no use file

--- tests/swoole_mysql/query_coredump.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . "/../include/swoole.inc";
-require_once __DIR__."/../include/api/swoole_mysql/swoole_mysql_init.php";
-
-swoole_mysql_query("select 1", function($swoole_mysql, $result) {
-    fprintf(STDERR, "SUCCESS\n");
-    $swoole_mysql->close();
-});
-?>
