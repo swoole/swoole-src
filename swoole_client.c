bea6b821adea1f0a4c332c13364e1f277454b5b1
matyhtf <mikan.tenny@gmail.com>
2018-03-12 18:59:28 +0800

    update error message.

--- swoole_client.c
+++ swoole_client.c
@@ -1336,8 +1336,20 @@ static PHP_METHOD(swoole_client, connect)
     //nonblock async
     if (cli->connect(cli, host, port, timeout, sock_flag) < 0)
     {
-        swoole_php_sys_error(E_WARNING, "connect to server[%s:%d] failed.", host, (int )port);
-        zend_update_property_long(swoole_client_class_entry_ptr, getThis(), SW_STRL("errCode")-1, errno TSRMLS_CC);
+        if (errno == 0 )
+        {
+            if (SwooleG.error == SW_ERROR_DNSLOOKUP_RESOLVE_FAILED)
+            {
+                swoole_php_error(E_WARNING, "connect to server[%s:%d] failed. Error: %s[%d]", host, (int )port,
+                        hstrerror(h_errno), h_errno);
+            }
+            zend_update_property_long(swoole_client_class_entry_ptr, getThis(), SW_STRL("errCode")-1, SwooleG.error TSRMLS_CC);
+        }
+        else
+        {
+            swoole_php_sys_error(E_WARNING, "connect to server[%s:%d] failed.", host, (int )port);
+            zend_update_property_long(swoole_client_class_entry_ptr, getThis(), SW_STRL("errCode")-1, errno TSRMLS_CC);
+        }
         RETURN_FALSE;
     }
     RETURN_TRUE;
