a9c87d7295d20b7314f457655cc3312b5b2c477b
yunnian <936321732@qq.com>
2018-03-16 00:12:26 +0800

    metaData 非协程版

--- swoole_postgresql_coro.c
+++ swoole_postgresql_coro.c
@@ -25,6 +25,7 @@ static PHP_METHOD(swoole_postgresql_coro, query);
 static PHP_METHOD(swoole_postgresql_coro, fetchAll);
 static PHP_METHOD(swoole_postgresql_coro, affectedRows);
 static PHP_METHOD(swoole_postgresql_coro, numRows);
+static PHP_METHOD(swoole_postgresql_coro,metaData);
 
 static void _close_pgsql_link(zend_resource *rsrc);
 static void _free_result(zend_resource *rsrc);
@@ -60,15 +61,21 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_pg_num_rows, 0, 0, 0)
     ZEND_ARG_INFO(0, result)
 ZEND_END_ARG_INFO()
 
+ZEND_BEGIN_ARG_INFO_EX(arginfo_pg_meta_data, 0, 0, 2)
+    ZEND_ARG_INFO(0, connection)
+    ZEND_ARG_INFO(0, table_name)
+ZEND_END_ARG_INFO()
+
 static const zend_function_entry swoole_postgresql_coro_methods[] =
 {
     PHP_ME(swoole_postgresql_coro, __construct, arginfo_swoole_void, ZEND_ACC_PUBLIC | ZEND_ACC_CTOR)
-    PHP_ME(swoole_postgresql_coro, connect, arginfo_pg_connect, ZEND_ACC_PUBLIC | ZEND_ACC_CTOR)
-    PHP_ME(swoole_postgresql_coro, query, arginfo_pg_query, ZEND_ACC_PUBLIC | ZEND_ACC_CTOR)
-    PHP_ME(swoole_postgresql_coro, fetchAll, arginfo_pg_fetch_all, ZEND_ACC_PUBLIC | ZEND_ACC_CTOR)
-    PHP_ME(swoole_postgresql_coro, affectedRows, arginfo_pg_fetch_all, ZEND_ACC_PUBLIC | ZEND_ACC_CTOR)
-    PHP_ME(swoole_postgresql_coro, numRows, arginfo_pg_num_rows, ZEND_ACC_PUBLIC | ZEND_ACC_CTOR)
-    PHP_ME(swoole_postgresql_coro, __destruct, arginfo_swoole_void, ZEND_ACC_PUBLIC | ZEND_ACC_CTOR)
+    PHP_ME(swoole_postgresql_coro, connect, arginfo_pg_connect, ZEND_ACC_PUBLIC)
+    PHP_ME(swoole_postgresql_coro, query, arginfo_pg_query, ZEND_ACC_PUBLIC )
+    PHP_ME(swoole_postgresql_coro, fetchAll, arginfo_pg_fetch_all, ZEND_ACC_PUBLIC)
+    PHP_ME(swoole_postgresql_coro, affectedRows, arginfo_pg_fetch_all, ZEND_ACC_PUBLIC)
+    PHP_ME(swoole_postgresql_coro, numRows, arginfo_pg_num_rows, ZEND_ACC_PUBLIC)
+    PHP_ME(swoole_postgresql_coro, metaData, arginfo_pg_meta_data, ZEND_ACC_PUBLIC)
+    PHP_ME(swoole_postgresql_coro, __destruct, arginfo_swoole_void, ZEND_ACC_PUBLIC | ZEND_ACC_DTOR)
     PHP_FE_END
 };
 
@@ -587,6 +594,133 @@ static PHP_METHOD(swoole_postgresql_coro,numRows){
     RETVAL_LONG(PQntuples(pgsql_result));
 }
 
+static PHP_METHOD(swoole_postgresql_coro,metaData)
+{
+
+    zval *pgsql_link;
+    char *table_name;
+    size_t table_name_len;
+    zend_bool extended=0;
+    PGconn *pgsql;
+
+    PGresult *pg_result;
+    char *src, *tmp_name, *tmp_name2 = NULL;
+    char *escaped;
+    smart_str querystr = {0};
+    size_t new_len;
+    int i, num_rows;
+    zval elem;
+
+    ZEND_PARSE_PARAMETERS_START(2,2)
+        Z_PARAM_RESOURCE(pgsql_link)
+        Z_PARAM_STRING(table_name, table_name_len)
+    ZEND_PARSE_PARAMETERS_END();
+
+    pgsql = (PGconn *)zend_fetch_resource(Z_RES_P(pgsql_link), "postgresql connection", le_link);
+
+
+    while ((pg_result = PQgetResult(pgsql))) {
+        PQclear(pg_result);
+    }
+
+    if (!*table_name) {
+        php_error_docref(NULL, E_WARNING, "The table name must be specified");
+        RETURN_FALSE;
+    }
+
+    src = estrdup(table_name);
+    tmp_name = php_strtok_r(src, ".", &tmp_name2);
+    if (!tmp_name) {
+        efree(src);
+        php_error_docref(NULL, E_WARNING, "The table name must be specified");
+        RETURN_FALSE;
+    }
+    if (!tmp_name2 || !*tmp_name2) {
+        /* Default schema */
+        tmp_name2 = tmp_name;
+        tmp_name = "public";
+    }
+
+    if (extended) {
+        smart_str_appends(&querystr,
+                          "SELECT a.attname, a.attnum, t.typname, a.attlen, a.attnotNULL, a.atthasdef, a.attndims, t.typtype, "
+                                  "d.description "
+                                  "FROM pg_class as c "
+                                  " JOIN pg_attribute a ON (a.attrelid = c.oid) "
+                                  " JOIN pg_type t ON (a.atttypid = t.oid) "
+                                  " JOIN pg_namespace n ON (c.relnamespace = n.oid) "
+                                  " LEFT JOIN pg_description d ON (d.objoid=a.attrelid AND d.objsubid=a.attnum AND c.oid=d.objoid) "
+                                  "WHERE a.attnum > 0  AND c.relname = '");
+    } else {
+        smart_str_appends(&querystr,
+                          "SELECT a.attname, a.attnum, t.typname, a.attlen, a.attnotnull, a.atthasdef, a.attndims, t.typtype "
+                                  "FROM pg_class as c "
+                                  " JOIN pg_attribute a ON (a.attrelid = c.oid) "
+                                  " JOIN pg_type t ON (a.atttypid = t.oid) "
+                                  " JOIN pg_namespace n ON (c.relnamespace = n.oid) "
+                                  "WHERE a.attnum > 0 AND c.relname = '");
+    }
+    escaped = (char *)safe_emalloc(strlen(tmp_name2), 2, 1);
+    new_len = PQescapeStringConn(pgsql, escaped, tmp_name2, strlen(tmp_name2), NULL);
+    if (new_len) {
+        smart_str_appendl(&querystr, escaped, new_len);
+    }
+    efree(escaped);
+
+    smart_str_appends(&querystr, "' AND n.nspname = '");
+    escaped = (char *)safe_emalloc(strlen(tmp_name), 2, 1);
+    new_len = PQescapeStringConn(pgsql, escaped, tmp_name, strlen(tmp_name), NULL);
+    if (new_len) {
+        smart_str_appendl(&querystr, escaped, new_len);
+    }
+    efree(escaped);
+
+    smart_str_appends(&querystr, "' ORDER BY a.attnum;");
+    smart_str_0(&querystr);
+    efree(src);
+
+    pg_result = PQexec(pgsql, ZSTR_VAL(querystr.s));
+    if (PQresultStatus(pg_result) != PGRES_TUPLES_OK || (num_rows = PQntuples(pg_result)) == 0) {
+        php_error_docref(NULL, E_WARNING, "Table '%s' doesn't exists", table_name);
+        smart_str_free(&querystr);
+        PQclear(pg_result);
+        RETURN_FALSE;
+    }
+    smart_str_free(&querystr);
+
+    for (i = 0; i < num_rows; i++) {
+        char *name;
+        array_init(&elem);
+        /* pg_attribute.attnum */
+        add_assoc_long_ex(&elem, "num", sizeof("num") - 1, atoi(PQgetvalue(pg_result, i, 1)));
+        /* pg_type.typname */
+        add_assoc_string_ex(&elem, "type", sizeof("type") - 1, PQgetvalue(pg_result, i, 2));
+        /* pg_attribute.attlen */
+        add_assoc_long_ex(&elem, "len", sizeof("len") - 1, atoi(PQgetvalue(pg_result,i,3)));
+        /* pg_attribute.attnonull */
+        add_assoc_bool_ex(&elem, "not null", sizeof("not null") - 1, !strcmp(PQgetvalue(pg_result, i, 4), "t"));
+        /* pg_attribute.atthasdef */
+        add_assoc_bool_ex(&elem, "has default", sizeof("has default") - 1, !strcmp(PQgetvalue(pg_result,i,5), "t"));
+        /* pg_attribute.attndims */
+        add_assoc_long_ex(&elem, "array dims", sizeof("array dims") - 1, atoi(PQgetvalue(pg_result, i, 6)));
+        /* pg_type.typtype */
+        add_assoc_bool_ex(&elem, "is enum", sizeof("is enum") - 1, !strcmp(PQgetvalue(pg_result, i, 7), "e"));
+        if (extended) {
+            /* pg_type.typtype */
+            add_assoc_bool_ex(&elem, "is base", sizeof("is base") - 1, !strcmp(PQgetvalue(pg_result, i, 7), "b"));
+            add_assoc_bool_ex(&elem, "is composite", sizeof("is composite") - 1, !strcmp(PQgetvalue(pg_result, i, 7), "c"));
+            add_assoc_bool_ex(&elem, "is pesudo", sizeof("is pesudo") - 1, !strcmp(PQgetvalue(pg_result, i, 7), "p"));
+            /* pg_description.description */
+            add_assoc_string_ex(&elem, "description", sizeof("description") - 1, PQgetvalue(pg_result, i, 8));
+        }
+        /* pg_attribute.attname */
+        name = PQgetvalue(pg_result,i,0);
+        array_init(return_value);
+        add_assoc_zval(return_value, name, &elem);
+    }
+    PQclear(pg_result);
+
+}
 
 /* {{{ _close_pgsql_link
  */
@@ -647,6 +781,8 @@ static int swoole_pgsql_coro_onError(swReactor *reactor, swEvent *event)
 
     return SW_OK;
 }
+
+
 
 static PHP_METHOD(swoole_postgresql_coro, __destruct)
 {
