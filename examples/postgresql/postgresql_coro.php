ef5652329fb1edfaf0dd2b499a106461520edc1f
yunnian <936321732@qq.com>
2018-03-18 14:48:07 +0800

    update examples of postgresql

--- examples/postgresql/postgresql_coro.php
+++ examples/postgresql/postgresql_coro.php
@@ -6,10 +6,8 @@ go(function () {
     $pg = new Swoole\Coroutine\PostgreSql();
     $conn  = $pg -> connect ("host=127.0.0.1 port=5432 dbname=test user=wuzhenyu password=");
     $result = $pg->query($conn, 'SELECT * FROM test;');
-    $arr = $pg->fetchAll($result);// the same with affectedRows(),fetchObject(),fetchAssoc(),fetchArray(),fetchRow()
+    $arr = $pg->fetchAll($result);// the same with affectedRows(),fetchObject(),fetchAssoc(),fetchArray(),fetchRow(),numRows()
     var_dump($arr);
-    //$numRows = $pg->numRows($result);
-    //var_dump($numRows);
 
 });
 
