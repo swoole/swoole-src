e99037b2046320d068d77fa102f4fb4466ba542e
matyhtf <mikan.tenny@gmail.com>
2018-04-23 19:06:20 +0800

    add test script.

--- tests/include/lib/curl.php
+++ tests/include/lib/curl.php
@@ -5,6 +5,7 @@ function curlGet($url)
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     //在http 请求头加入 gzip压缩
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept-Encoding: gzip'));
     //curl返回的结果，采用gzip解压
