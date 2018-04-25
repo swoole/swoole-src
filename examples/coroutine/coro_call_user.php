996e40d5e40343b19d86d193d1dc9e1f0f7b6860
shiguangqi <shiguangqi2008@gmail.com>
2018-04-25 16:19:26 +0800

    add test

--- examples/coroutine/coro_call_user.php
+++ examples/coroutine/coro_call_user.php
@@ -3,45 +3,15 @@ use Swoole\Coroutine as co;
 co::set(['trace_flags' => 1]);
 
 co::create(function() {
+    echo "co func start\n";
 	call_user_func_array("test",["test\n"]);
-	echo "main func client end \n";
+	echo "co func end \n";
 });
 
-function test($a)
+function test($params)
 {
-	echo "func $a";
-	$client = new Swoole\Coroutine\Client(SWOOLE_SOCK_TCP);
-	$res = $client->connect('127.0.0.1', 9501, 10);
-	echo(__FUNCTION__." connect res :".var_export($res,1)."\n");
-	call_user_func_array("test2",["test2\n"]);
+    echo "func $params";
+    co::sleep(1);
+    echo "func end\n";
 }
-
-function test2($a)
-{
-	call_user_func_array("test3",["test3\n"]);
-	echo "func $a";
-	$client = new Swoole\Coroutine\Client(SWOOLE_SOCK_TCP);
-	$res = $client->connect('127.0.0.1', 9501, 10);
- 	echo(__FUNCTION__." connect res :".var_export($res,1)."\n");
-	call_user_func('test4', "test4\n");
-}
-
-function test3($a)
-{
-	echo "func $a";
-	$client = new Swoole\Coroutine\Client(SWOOLE_SOCK_TCP);
-	$res = $client->connect('127.0.0.1', 9501, 10);
-	sleep(1);
- 	echo(__FUNCTION__." connect res :".var_export($res,1)."\n");
-}
-
-function test4($a)
-{
-	echo "func $a";
-	$client = new Swoole\Coroutine\Client(SWOOLE_SOCK_TCP);
-	$res = $client->connect('127.0.0.1', 9501, 10);
-	sleep(1);
- 	echo(__FUNCTION__." connect res :".var_export($res,1)."\n");
-}
-
 echo "main script last\n";
