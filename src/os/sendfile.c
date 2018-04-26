0188e569774ea9a28ad48a9d1cd915d612812c3a
hantianfeng <mikan.tenny@gmail.com>
2018-04-26 21:59:41 +0800

    warning free

--- src/os/sendfile.c
+++ src/os/sendfile.c
@@ -38,7 +38,7 @@ int swoole_sendfile(int out_fd, int in_fd, off_t *offset, size_t size)
 
     do_sendfile:
 #ifdef __MACH__
-    ret = sendfile(in_fd, out_fd, *offset, &size, &hdtr, 0);
+    ret = sendfile(in_fd, out_fd, *offset, (long long *) &size, &hdtr, 0);
 #else
     ret = sendfile(in_fd, out_fd, *offset, size, 0, &sent_bytes, 0);
 #endif
