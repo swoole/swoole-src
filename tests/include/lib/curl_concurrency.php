0a82d78e2e671e1040eac69a8cb1f0557947c125
scil <scil.zc@gmail.com>
2018-04-29 22:17:03 +0800

    rename $mh to $curlH

--- tests/include/lib/curl_concurrency.php
+++ tests/include/lib/curl_concurrency.php
@@ -6,26 +6,26 @@
    */
 function curl_concurrency($arrayUrls )
 {
-	$mh = curl_multi_init();
+	$curlH = curl_multi_init();
 
 	foreach ($arrayUrls as $i => $url) {
 	    	 $conn[$i]=curl_init($url);
 		curl_setopt($conn[$i],CURLOPT_RETURNTRANSFER,1);
-		curl_multi_add_handle ($mh,$conn[$i]);
+		curl_multi_add_handle ($curlH,$conn[$i]);
 	}
 
 	$active = null;
 
 	do {
-	    $mrc = curl_multi_exec($mh, $active);
+	    $mrc = curl_multi_exec($curlH, $active);
 	} while ($mrc == CURLM_CALL_MULTI_PERFORM);
 
 	while ($active && $mrc == CURLM_OK) {
-	    if (curl_multi_select($mh) != -1) {
+	    if (curl_multi_select($curlH) != -1) {
 			usleep(100);
 		}
         do {
-            $mrc = curl_multi_exec($mh, $active);
+            $mrc = curl_multi_exec($curlH, $active);
         } while ($mrc == CURLM_CALL_MULTI_PERFORM);
 	    
 	}
@@ -33,11 +33,11 @@ function curl_concurrency($arrayUrls )
 
 	foreach ($arrayUrls as $i => $url) {
 	      $res[$i]=curl_multi_getcontent($conn[$i]);
-		curl_multi_remove_handle($mh, $conn[$i]);
+		curl_multi_remove_handle($curlH, $conn[$i]);
 	      curl_close($conn[$i]);
 	}
 
-	curl_multi_close($mh);
+	curl_multi_close($curlH);
 
 	return $res;
 }
