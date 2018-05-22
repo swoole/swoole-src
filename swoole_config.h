e4d23c26aca7d960aca4348f5a16b7bc26f66900
matyhtf <mikan.tenny@gmail.com>
2018-05-22 16:01:02 +0800

    Fixed issue where http2 server could not send more than 16k data

--- swoole_config.h
+++ swoole_config.h
@@ -257,7 +257,7 @@
 //#define SW_HTTP_100_CONTINUE
 #define SW_HTTP2_DATA_BUFFSER_SIZE       8192
 #define SW_HTTP2_MAX_CONCURRENT_STREAMS  128
-#define SW_HTTP2_MAX_FRAME_SIZE          ((1u << 24) - 1)
+#define SW_HTTP2_MAX_FRAME_SIZE          ((1u << 14))
 #define SW_HTTP2_MAX_WINDOW              ((1u << 31) - 1)
 #define SW_HTTP2_DEFAULT_WINDOW          65535
 
