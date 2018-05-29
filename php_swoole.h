942a81bde47e5578ad3ff146aa6aeadaccb2035a
matyhtf <mikan.tenny@gmail.com>
2018-05-29 18:28:15 +0800

    fixed #1454

--- php_swoole.h
+++ php_swoole.h
@@ -52,7 +52,7 @@
 
 BEGIN_EXTERN_C()
 
-#define PHP_SWOOLE_VERSION  "4.0.0-beta"
+#define PHP_SWOOLE_VERSION  "4.0.0-rc1"
 #define PHP_SWOOLE_CHECK_CALLBACK
 #define PHP_SWOOLE_ENABLE_FASTCALL
 #define PHP_SWOOLE_CLIENT_USE_POLL
@@ -126,12 +126,8 @@ extern swoole_object_array swoole_objects;
 #endif
 
 #ifdef SW_SOCKETS
-#if PHP_VERSION_ID >= 50301 && (HAVE_SOCKETS || defined(COMPILE_DL_SOCKETS))
 #include "ext/sockets/php_sockets.h"
 #define SWOOLE_SOCKETS_SUPPORT
-#else
-#error "Enable sockets support, require sockets extension."
-#endif
 #endif
 
 #ifdef SW_USE_HTTP2
