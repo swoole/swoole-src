d8b08c87a8f56ffd665bdc6a8b54ef480a0f440d
Caleb Mazalevskis <maikuolan@gmail.com>
2018-06-25 02:42:42 +0800

    Sync PHP examples code style.
    (Just synchronizes the PHP examples code style. Nothing else changed).

--- examples/php/debug_server.php
+++ examples/php/debug_server.php
@@ -1,5 +1,4 @@
 <?php
-
 class DebugServer
 {
     protected $alloc_point = array();
@@ -12,52 +11,39 @@ class DebugServer
         $tag = trim($tag);
         $vars = explode(',', trim($_vars));
         $data = array();
-        foreach($vars as $str)
-        {
+        foreach($vars as $str) {
             list($k, $v) = explode('=', trim($str));
             $data[$k] = $v;
         }
 
-        if ($tag == 'alloc')
-        {
-            file_put_contents(__DIR__.'/alloc.log', $data['ptr']."\n", FILE_APPEND);
-        }
-        elseif($tag =='memory')
-        {
+        if ($tag == 'alloc') {
+            file_put_contents(__DIR__ . '/alloc.log', $data['ptr'] . "\n", FILE_APPEND);
+        } elseif ($tag == 'memory') {
             var_dump($tag, $data);
-        }
-        elseif ($tag == 'free')
-        {
-            file_put_contents(__DIR__.'/free.log', $data['ptr']."\n", FILE_APPEND);
-        }
-        elseif($tag == 'invalid')
-        {
-            foreach($this->alloc_point as $k => $v)
-            {
+        } elseif ($tag == 'free') {
+            file_put_contents(__DIR__ . '/free.log', $data['ptr'] . "\n", FILE_APPEND);
+        } elseif ($tag == 'invalid') {
+            foreach($this->alloc_point as $k => $v) {
                 echo "$k => $v\n";
             }
-        }
-        else
-        {
-            //var_dump($tag, $data);
+        } else {
+            // var_dump($tag, $data);
         }
     }
 
     function run()
     {
-        unlink(__DIR__.'/alloc.log');
-        unlink(__DIR__.'/free.log');
-        $socket = stream_socket_server("udp://127.0.0.1:9999", $errno, $errstr, STREAM_SERVER_BIND);
-        if (!$socket)
-        {
+        unlink(__DIR__ . '/alloc.log');
+        unlink(__DIR__ . '/free.log');
+        $socket = stream_socket_server('udp://127.0.0.1:9999', $errno, $errstr, STREAM_SERVER_BIND);
+        if (!$socket) {
             die("$errstr ($errno)");
         }
-        while(1)
-        {
+        while (1) {
             $pkt = stream_socket_recvfrom($socket, 65535, 0, $peer);
             $this->package_decode($pkt);
-            //echo "$peer: $pkt\n";
-            //stream_socket_sendto($socket, date("D M j H:i:s Y\r\n"), 0, $peer);
+            // echo "$peer: $pkt\n";
+            // stream_socket_sendto($socket, date("D M j H:i:s Y\r\n"), 0, $peer);
         }
     }
 }
--- examples/php/inotify.php
+++ examples/php/inotify.php
@@ -5,10 +5,8 @@ $fd = inotify_init();
 // Watch __FILE__ for metadata changes (e.g. mtime)
 $watch_descriptor = inotify_add_watch($fd, __DIR__ . '/php', IN_MODIFY | IN_MOVED_FROM | IN_CREATE | IN_DELETE | IN_ISDIR);
 
-while (true)
-{
+while (true) {
     // Read events
     $events = inotify_read($fd);
     print_r($events);
 }
-
