71db82ecf67501863f6eeb994a781f8e042a047c
twosee <twose@qq.com>
2018-07-24 22:56:20 +0800

    Try again.

--- travis/compile.sh
+++ travis/compile.sh
@@ -3,5 +3,5 @@ phpize && \
 ./configure && \
 make clean && \
 make && install_info="`make install`" && \
-echo -e ${install_info} && php_ext_dir="`echo -e ${install_info} | grep -o " /.*extensions\/no-debug.*\/"`" && \
+echo -e "${install_info}" && php_ext_dir="`echo -e "${install_info}" | grep -o " /.*extensions\/no-debug.*\/"`" && \
 echo "\n[swoole]\nextension=${php_ext_dir}swoole.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
