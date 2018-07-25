d9c593d405d0387b7a5ce15c310982752d0d3782
twosee <twose@qq.com>
2018-07-25 17:36:12 +0800

    Fix.

--- tests/init.php
+++ tests/init.php
@@ -8,11 +8,12 @@ try {
         "mysql:host=" . MYSQL_SERVER_HOST . ";dbname=" . MYSQL_SERVER_DB . ";charset=utf8",
         MYSQL_SERVER_USER, MYSQL_SERVER_PWD
     );
-    if ($mysql->exec(file_get_contents(__DIR__ . '/test.sql'))) {
-        echo "[DB-init] Done!\n";
-    } else {
-        echo "[DB-init] Failed! Error#{$mysql->errorCode()}: {$mysql->errorInfo()}\n";
+    $mysql->exec(file_get_contents(__DIR__ . '/test.sql'));
+    if ($mysql->errorCode() > 0) {
+        echo "[DB-init] Failed! Error#{$mysql->errorCode()}: \n" . var_dump_return($mysql->errorInfo()) . "\n";
         exit(1);
+    } else {
+        echo "[DB-init] Done!\n";
     }
 } catch (\Exception $e) {
     echo "[DB-init] Connect failed! Error#{$e->getCode()}: {$e->getMessage()}\n";
