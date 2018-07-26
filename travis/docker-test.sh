77ee9f8b3fc8e17510e85bca6c1f611fcc478435
twosee <twose@qq.com>
2018-07-26 14:31:17 +0800

    Improve test script.

--- travis/docker-test.sh
+++ travis/docker-test.sh
@@ -7,8 +7,24 @@ cd ${__DIR__} && cd ../tests/
 # initialization
 php ./init.php
 # run
-./start.sh --set-timeout 15 --show-diff -w failed.list ./swoole_*
+./start.sh \
+--set-timeout 10 \
+--show-diff \
+-w failed.list \
+./swoole_*
 
 if [ "`cat failed.list | grep "phpt"`" ]; then
-    exit 255
+    # read failed list
+    failed_list="`cat failed.list`"
+
+    # replace \n to space and retry
+    ./start.sh \
+    --set-timeout 20 \
+    --show-diff \
+    -w failed.list \
+    ${failed_list/\n/ }
+
+    if [ "`cat failed.list | grep "phpt"`" ]; then
+        exit 255
+    fi
 fi
