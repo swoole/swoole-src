c2369c55a7ed5f1f91d2a7edb500d9d131a0ae35
twosee <twose@qq.com>
2018-07-26 10:54:48 +0800

    Fix logic.

--- tests/include/skip/skip_if_no_proxy.php
+++ tests/include/skip/skip_if_no_proxy.php
@@ -2,6 +2,6 @@
 require_once __DIR__ . '../skipif.inc';
 require_once __DIR__ . '../config.php';
 
-if (!IS_IN_DOCKER && check_tcp_port(HTTP_PROXY_HOST, HTTP_PROXY_PORT) === 1) {
+if (IS_IN_DOCKER || check_tcp_port(HTTP_PROXY_HOST, HTTP_PROXY_PORT) !== 1) {
     exit('skip by no http proxy available');
 }
