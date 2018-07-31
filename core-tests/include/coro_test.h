27410ad1828051c43b27a45266ddf473cb9bf3d3
matyhtf <mikan.tenny@gmail.com>
2018-07-31 14:36:59 +0800

    update core-tests

--- core-tests/include/coro_test.h
+++ core-tests/include/coro_test.h
@@ -7,16 +7,6 @@
 
 namespace swoole_test
 {
-int coroutine_create_test1();
-void coroutine_socket_connect_refused();
-void coroutine_socket_connect_timeout();
-void coroutine_socket_connect_with_dns();
-void coroutine_socket_recv_fail();
-void coroutine_socket_recv_success();
-void coroutine_socket_bind_success();
-void coroutine_socket_bind_fail();
-void coroutine_socket_listen();
-void coroutine_socket_accept();
 int server_test();
 }
 
