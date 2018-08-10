40cb5e183a12c8f7b4dc1ab33d1f85b9449b79bb
CismonX <qiaocheng@twt.edu.cn>
2018-08-11 00:01:53 +0800

    compatible with solaris

--- include/Server.h
+++ include/Server.h
@@ -21,6 +21,10 @@
 #include "buffer.h"
 #include "Connection.h"
 
+#if defined(__sun) && !defined(s6_addr32)
+#define s6_addr32                  _S6_un._S6_u32
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
