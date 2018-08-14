b7b929f2e48a5cfc1b82de6ed698f0dfb23dd6de
matyhtf <mikan.tenny@gmail.com>
2018-08-14 14:38:42 +0800

    added runtime::enableCoroutine example

---
+++ examples/runtime/coroutine.php
@@ -0,0 +1,24 @@
+<?php
+go(function () {
+    swoole\runtime::enableCoroutine();
+
+    $redis = new redis;
+    $retval = $redis->connect("127.0.0.1", 6379);
+    var_dump($retval, $redis->getLastError());
+
+    var_dump($redis->get("key"));
+
+
+    $db = new mysqli;
+    $db->connect('127.0.0.1', 'root', 'root', 'test');
+
+    $result = $db->query("show databases");
+    var_dump($result->fetch_all());
+
+    $db = new PDO("mysql:host=127.0.0.1;dbname=test;charset=utf8", "root" ,"root");
+    $query = $db->prepare("select * from userinfo where id=?");
+    $rs = $query->execute(array(1));
+    var_dump($rs);
+    echo count($query->fetchAll());
+
+});
