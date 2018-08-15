6aba3c4942e339fdc7cf628e2680e6d427a2cf55
yunnian(zhenyu) <936321732@qq.com>
2018-08-15 16:58:11 +0800

    add time out

--- swoole_config.h
+++ swoole_config.h
@@ -258,6 +258,7 @@
 #define SW_MYSQL_DEFAULT_CHARSET         33  //0x21, utf8_general_ci
 
 #define SW_REDIS_CONNECT_TIMEOUT         1.0
+#define SW_PGSQL_CONNECT_TIMEOUT         3.0
 
 #define SW_TIMER_MAX_VALUE               86400000
 
