567608b8b19ebbcad7459920aab2a93320c7807c
twosee <twose@qq.com>
2018-09-26 18:24:56 +0800

    Add pecl package command.

--- travis/pecl.sh
+++ travis/pecl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 pecl config-show && \
-pecl package && package_file="`ls | grep swoole-*tgz`" && \
+php ./tools/pecl-package.php && package_file="`ls | grep swoole-*tgz`" && \
 echo "\n" | pecl install -f ${package_file} | tee pecl.log && \
 cat pecl.log | grep "successfully" && \
 pecl uninstall swoole && \
