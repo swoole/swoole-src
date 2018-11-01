cde47503a6b9be810ca2775381f09a25d544a6dd
matyhtf <mikan.tenny@gmail.com>
2018-11-01 19:50:12 +0800

    add comments

--- include/file_hook.h
+++ include/file_hook.h
@@ -1,3 +1,22 @@
+/*
+  +----------------------------------------------------------------------+
+  | Swoole                                                               |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 2.0 of the Apache license,    |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+  | If you did not receive a copy of the Apache2.0 license and are unable|
+  | to obtain it through the world-wide-web, please send a note to       |
+  | license@swoole.com so we can mail you a copy immediately.            |
+  +----------------------------------------------------------------------+
+  | Author: Tianfeng Han  <mikan.tenny@gmail.com>                        |
+  +----------------------------------------------------------------------+
+*/
+
+#ifndef SW_FILE_HOOK_H_
+#define SW_FILE_HOOK_H_
+
 #include "coroutine_c_api.h"
 
 #define access(pathname, mode)             swoole_coroutine_access(pathname, mode)
@@ -16,4 +35,6 @@ DIR *swoole_coroutine_opendir(const char *name);
 struct dirent *swoole_coroutine_readdir(DIR *dirp);
 #define opendir(name)                      swoole_coroutine_opendir(name)
 #define readdir(dir)                       swoole_coroutine_readdir(dir)
+#endif
+
 #endif
