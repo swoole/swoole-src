1a050fe1e43de4388be4c8b186947216a80b70a5
twosee <twose@qq.com>
2018-11-19 21:52:36 +0800

    Add SW_ERROR_UNREGISTERED_SIGNAL, fix code-generator.

--- src/os/signal.c
+++ src/os/signal.c
@@ -149,7 +149,7 @@ void swSignal_callback(int signo)
     swSignalHandler callback = signals[signo].handler;
     if (!callback)
     {
-        swWarn("signal[%d] callback is null.", signo);
+        swoole_error_log(SW_LOG_WARNING, SW_ERROR_UNREGISTERED_SIGNAL, "signal[%d] callback is null.", signo);
         return;
     }
     callback(signo);
@@ -292,7 +292,7 @@ static int swSignalfd_onSignal(swReactor *reactor, swEvent *event)
         }
         else
         {
-            swWarn("signal[%d] callback is null.", siginfo.ssi_signo);
+            swoole_error_log(SW_LOG_WARNING, SW_ERROR_UNREGISTERED_SIGNAL, "signal[%d] callback is null.", siginfo.ssi_signo);
         }
     }
 
