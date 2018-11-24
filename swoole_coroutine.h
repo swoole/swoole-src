cd797ac687975638e5263c6a8d17deea68b22f09
twosee <twose@qq.com>
2018-11-24 10:49:46 +0800

    Revert "add coro swap callback"
    This reverts commit 4a8bc3d

--- swoole_coroutine.h
+++ swoole_coroutine.h
@@ -59,10 +59,6 @@ struct coro_global
     uint64_t peak_coro_num;
     uint32_t stack_size;
     coro_task task;
-    zend_fcall_info_cache swapin;
-    zend_fcall_info_cache swapout;
-    zval swapin_cb;
-    zval swapout_cb;
 };
 
 // TODO: remove php context
@@ -104,3 +100,5 @@ void sw_coro_close();
 int sw_coro_resume(php_context *sw_current_context, zval *retval, zval *coro_retval);
 void sw_coro_save(zval *return_value, php_context *sw_php_context);
 void sw_coro_set_stack_size(int stack_size);
+
+
