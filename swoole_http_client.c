13f44d05b14b2bb45c6f7efa945695623b943405
matyhtf <mikan.tenny@gmail.com>
2018-12-13 22:40:33 +0800

    fix bugs

--- swoole_http_client.c
+++ swoole_http_client.c
@@ -1918,7 +1918,7 @@ int http_client_parser_on_headers_complete(swoole_http_parser *parser)
         http->state = HTTP_CLIENT_STATE_WAIT_CLOSE;
         parser->flags |= F_CONNECTION_CLOSE;
     }
-    if (http->method == PHP_HTTP_HEAD || parser->status_code == SW_HTTP_NO_CONTENT)
+    if (http->method == SW_HTTP_HEAD || parser->status_code == SW_HTTP_NO_CONTENT)
     {
         return 1;
     }
