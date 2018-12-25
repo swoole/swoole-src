5adcd3ae607f82bc21f770fb10c4656c1e77c0f2
matyhtf <mikan.tenny@gmail.com>
2018-12-25 17:55:12 +0800

    EG(bailout) swap

--- swoole_coroutine.cc
+++ swoole_coroutine.cc
@@ -63,6 +63,7 @@ static sw_inline void php_coro_save_vm_stack(coro_task *task)
     task->error_handling = EG(error_handling);
     task->exception_class = EG(exception_class);
     task->exception = EG(exception);
+    task->bailout = EG(bailout);
     SW_SAVE_EG_SCOPE(task->scope);
 }
 
@@ -86,6 +87,7 @@ static sw_inline void php_coro_restore_vm_stack(coro_task *task)
     EG(error_handling) = task->error_handling;
     EG(exception_class) = task->exception_class;
     EG(exception) = task->exception;
+    EG(bailout) = task->bailout;
     SW_SET_EG_SCOPE(task->scope);
 }
 
