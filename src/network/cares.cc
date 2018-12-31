c49be961c098e93852514862e78dec9afddd1647
windrunner414 <windrunner414@outlook.com>
2018-12-31 13:06:02 +0800

    code optimization

--- src/network/cares.cc
+++ src/network/cares.cc
@@ -2,14 +2,64 @@
 #include "cares.h"
 #include "coroutine.h"
 #include "ares.h"
+#include "sys/uio.h"
 
 using namespace swoole;
 using namespace std;
+
+static ares_socket_t asocket(int af, int type, int protocol, void *data)
+{
+    int fd = socket(af, type, protocol);
+    swSetNonBlock(fd);
+
+    int ev_type = SW_FD_ARES | SW_EVENT_READ;
+    if (type == SOCK_STREAM)
+    {
+        ev_type |= SW_EVENT_WRITE;
+    }
+
+    if (unlikely(SwooleG.main_reactor->add(SwooleG.main_reactor, fd, ev_type) < 0))
+    {
+        close(fd);
+        return -1;
+    }
+
+    auto sock = swReactor_get(SwooleG.main_reactor, fd);
+    sock->removed = 0;
+    sock->fd = fd;
+    sock->object = data;
+    return fd;
+}
+
+static int aclose(ares_socket_t sock, void *data)
+{
+    swReactor_get(SwooleG.main_reactor, sock)->removed = 1;
+    SwooleG.main_reactor->del(SwooleG.main_reactor, sock);
+
+    return close(sock);
+}
+
+static int aconnect(ares_socket_t sock, const struct sockaddr *sock_addr, ares_socklen_t sock_len, void *data)
+{
+    return connect(sock, sock_addr, sock_len);
+}
+
+static ares_ssize_t arecvfrom(ares_socket_t sock, void *buf, size_t len, int flag,
+        struct sockaddr *from, ares_socklen_t *from_len, void *data)
+{
+    return recvfrom(sock, buf, len, flag, from, from_len);
+}
+
+static ares_ssize_t asendv(ares_socket_t sock, const struct iovec *vector, int count, void *data)
+{
+    return writev(sock, vector, count);
+}
 
 struct _ares_dns_task
 {
     ares_channel channel;
     ares_options options;
+    ares_socket_functions sock_func;
     bool finish;
     string result;
     Coroutine *co;
@@ -19,11 +69,18 @@ struct _ares_dns_task
         finish = false;
         co = nullptr;
         options.flags = ARES_FLAG_STAYOPEN; // don't close socket in end_query.
+        sock_func.asocket = asocket;
+        sock_func.aclose = aclose;
+        sock_func.aconnect = aconnect;
+        sock_func.arecvfrom = arecvfrom;
+        sock_func.asendv = asendv;
 
-        if (ares_init_options(&channel, &options, ARES_OPT_FLAGS) != ARES_SUCCESS)
+        if (unlikely(ares_init_options(&channel, &options, ARES_OPT_FLAGS) != ARES_SUCCESS))
         {
             finish = true;
         }
+
+        ares_set_socket_functions(channel, &sock_func, this);
     }
 
     ~_ares_dns_task()
@@ -76,27 +133,18 @@ static void ares_dns_timeout(swTimer *timer, swTimer_node *tnode)
 static int ares_event_read(swReactor *reactor, swEvent *event)
 {
     auto task = (ares_dns_task *) event->socket->object;
-    auto co = task->co;
 
     ares_process_fd(task->channel, event->fd, ARES_SOCKET_BAD);
-    co->resume();
-
+    if (task->finish) task->co->resume();
     return SW_OK;
 }
 
 static int ares_event_write(swReactor *reactor, swEvent *event)
 {
     auto task = (ares_dns_task *) event->socket->object;
-    auto co = task->co;
 
     ares_process_fd(task->channel, ARES_SOCKET_BAD, event->fd);
-    co->resume();
-
-    return SW_OK;
-}
-
-static int ares_event_error(swReactor *reactor, swEvent *event)
-{
+    if (task->finish) task->co->resume();
     return SW_OK;
 }
 
@@ -106,6 +154,13 @@ string CAres::resolve(const std::string &hostname, int domain, double timeout)
     if (unlikely(!reactor))
     {
         return "";
+    }
+
+    if (unlikely(!swReactor_handle_isset(reactor, SW_FD_ARES)))
+    {
+        reactor->setHandle(reactor, SW_FD_ARES | SW_EVENT_READ, ares_event_read);
+        reactor->setHandle(reactor, SW_FD_ARES | SW_EVENT_WRITE, ares_event_write);
+        reactor->setHandle(reactor, SW_FD_ARES | SW_EVENT_ERROR, ares_event_read);
     }
 
     ares_dns_task task;
@@ -129,86 +184,7 @@ string CAres::resolve(const std::string &hostname, int domain, double timeout)
     }
 
     ares_gethostbyname(channel, hostname.c_str(), domain, ares_dns_callback, &task);
-    int bitmap;
-    ares_socket_t sock[ARES_GETSOCK_MAXNUM];
-
-    if (unlikely(!swReactor_handle_isset(reactor, SW_FD_ARES)))
-    {
-        reactor->setHandle(reactor, SW_FD_ARES | SW_EVENT_READ, ares_event_read);
-        reactor->setHandle(reactor, SW_FD_ARES | SW_EVENT_WRITE, ares_event_write);
-        reactor->setHandle(reactor, SW_FD_ARES | SW_EVENT_ERROR, ares_event_error);
-    }
-
-    ares_socket_t active_sock[ARES_GETSOCK_MAXNUM];
-    for (int i = 0; i < ARES_GETSOCK_MAXNUM; ++i)
-    {
-        active_sock[i] = ARES_SOCKET_BAD;
-    }
-
-    for (;;)
-    {
-        bitmap = ares_getsock(channel, sock, ARES_GETSOCK_MAXNUM);
-        if (bitmap == 0)
-        {
-            break;
-        }
-
-        for (int i = 0; i < ARES_GETSOCK_MAXNUM; ++i)
-        {
-            if (ARES_GETSOCK_WRITABLE(bitmap, i))
-            {
-                // if it's writeable, it must be readable too.
-                if (unlikely(reactor->add(reactor, sock[i], SW_FD_ARES | SW_EVENT_READ | SW_EVENT_WRITE) < 0))
-                {
-                    return "";
-                }
-
-                active_sock[i] = sock[i];
-                auto sw_conn = swReactor_get(reactor, sock[i]);
-                sw_conn->object = &task;
-                sw_conn->removed = 0;
-            }
-            else if (ARES_GETSOCK_READABLE(bitmap, i))
-            {
-                // only readable
-                if (unlikely(reactor->add(reactor, sock[i], SW_FD_ARES | SW_EVENT_READ) < 0))
-                {
-                    return "";
-                }
-
-                active_sock[i] = sock[i];
-                auto sw_conn = swReactor_get(reactor, sock[i]);
-                sw_conn->object = &task;
-                sw_conn->removed = 0;
-            }
-            else
-            {
-                // don't have more socket
-                break;
-            }
-        }
-
-        co->yield();
-
-        for (int i = 0; i < ARES_GETSOCK_MAXNUM; ++i)
-        {
-            if (active_sock[i] != ARES_SOCKET_BAD)
-            {
-                swReactor_get(reactor, active_sock[i])->removed = 1;
-                reactor->del(reactor, active_sock[i]);
-                active_sock[i] = ARES_SOCKET_BAD;
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        if (task.finish)
-        {
-            break;
-        }
-    }
+    co->yield();
 
     if (timer)
     {
