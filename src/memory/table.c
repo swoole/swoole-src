872e99d771727806fb8f4ef140db067813ec142f
twosee <twose@qq.com>
2019-01-04 11:39:44 +0800

    Improve code.

--- src/memory/table.c
+++ src/memory/table.c
@@ -462,10 +462,8 @@ int swTableRow_del(swTable *table, char *key, int keylen)
         {
             tmp = tmp->next;
             row->next = tmp->next;
-            size_t tmp_keylen = strlen(tmp->key);
-            memcpy(row->key, tmp->key, tmp_keylen);
+            memcpy(row->key, tmp->key, strlen(tmp->key) + 1);
             memcpy(row->data, tmp->data, table->item_size);
-            row->key[tmp_keylen] = '\0';
         }
         if (prev)
         {
