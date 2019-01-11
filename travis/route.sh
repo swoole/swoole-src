1e6d3b680ee3edb34804871c042f24ef1436295b
twosee <twose@qq.com>
2019-01-11 17:17:41 +0800

    Merge env to script (due to sh).

--- travis/route.sh
+++ travis/route.sh
@@ -1,6 +1,19 @@
-#!/bin/sh
+#!/bin/sh -e
 __CURRENT__=`pwd`
 __DIR__=$(cd "$(dirname "$0")";pwd)
+
+[ -z "${TRAVIS_BRANCH}" ] && export TRAVIS_BRANCH="master"
+[ -z "${TRAVIS_BUILD_DIR}" ] && export TRAVIS_BUILD_DIR=$(cd "$(dirname "$0")";cd ../;pwd)
+export DOCKER_COMPOSE_VERSION="1.21.0"
+export PHP_VERSION_ID=`php -r "echo PHP_VERSION_ID;"`
+if [ ${PHP_VERSION_ID} -lt 70300 ]; then
+    export PHP_VERSION="`php -r "echo PHP_MAJOR_VERSION;"`.`php -r "echo PHP_MINOR_VERSION;"`"
+else
+    export PHP_VERSION="rc"
+fi
+if [ "${TRAVIS_BRANCH}" = "alpine" ]; then
+    export PHP_VERSION="${PHP_VERSION}-alpine"
+fi
 
 check_docker_dependency(){
     if [ "`docker -v 2>&1 | grep "version"`"x = ""x ]; then
@@ -58,11 +71,6 @@ remove_tests_resources(){
     remove_docker_containers
     remove_data_files
 }
-
-set -e
-
-cd ${__DIR__}
-source env.sh
 
 install_docker_compose
 
