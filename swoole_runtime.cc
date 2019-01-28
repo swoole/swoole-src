7b1e78643c40b0c8ace1bca8c7b10e3064cce488
shiguangqi <shiguangqi2008@gmail.com>
2019-01-28 15:28:26 +0800

    optimize code

--- swoole_runtime.cc
+++ swoole_runtime.cc
@@ -918,6 +918,7 @@ static php_stream *socket_create(
     abstract->stream.timeout.tv_sec = FG(default_socket_timeout);
     abstract->stream.socket = sock->get_fd();
 
+    persistent_id = nullptr;//prevent stream api in user level using pconnect to persist the socket
     stream = php_stream_alloc_rel(&socket_ops, abstract, persistent_id, "r+");
 
     if (stream == NULL)
