d2beda050e43f5664a40472d4bc7d8618f781a90
twosee <twose@qq.com>
2019-02-02 11:20:00 +0800

    Fix typo error in deaf763.

--- swoole_server_port.cc
+++ swoole_server_port.cc
@@ -353,10 +353,10 @@ static PHP_METHOD(swoole_server_port, set)
             }
             efree(func_name);
             port->protocol.get_package_length = php_swoole_length_func;
-            if (cli->protocol.private_data)
+            if (port->protocol.private_data)
             {
-                zval_ptr_dtor((zval *)cli->protocol.private_data);
-                efree(cli->protocol.private_data);
+                zval_ptr_dtor((zval *)port->protocol.private_data);
+                efree(port->protocol.private_data);
             }
             Z_TRY_ADDREF_P(v);
             port->protocol.private_data = sw_zval_dup(v);
