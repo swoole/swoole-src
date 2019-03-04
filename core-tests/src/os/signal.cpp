a7de9ec8a778ac730ac43fbd03a9d560ddeb1d8f
junwei <337560754@qq.com>
2019-03-05 05:10:26 +0800

    add unit test

---
+++ core-tests/src/os/signal.cpp
@@ -0,0 +1,35 @@
+#include "tests.h"
+#ifdef HAVE_SIGNALFD
+static void sig_usr1(int signo){
+
+}
+TEST(os_signal, swSignalfd_set)
+{
+    int ret;
+    sigset_t curset;
+
+    SwooleG.use_signalfd = 1;
+
+    sigemptyset(&curset);
+    sigprocmask(SIG_BLOCK, NULL, &curset);
+    ret = sigismember(&curset,SIGUSR1);
+    ASSERT_EQ(ret, 0);
+
+    swSignalfd_init();
+    swSignal_add(SIGUSR1,sig_usr1);
+    swSignalfd_setup(SwooleG.main_reactor);
+
+    sigemptyset(&curset);
+    sigprocmask(SIG_BLOCK, NULL, &curset);
+    ret = sigismember(&curset,SIGUSR1);
+    ASSERT_EQ(ret, 1);
+
+    swSignal_add(SIGUSR1,NULL);
+    swSignal_clear();
+
+    sigemptyset(&curset);
+    sigprocmask(SIG_BLOCK, NULL, &curset);
+    ret = sigismember(&curset,SIGUSR1);
+    ASSERT_EQ(ret, 0);
+}
+#endif
