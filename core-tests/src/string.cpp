b854409339333d8a43bede32ea29ddf4bd48bf9b
matyhtf <mikan.tenny@gmail.com>
2019-03-08 13:52:00 +0800

    added test

---
+++ core-tests/src/string.cpp
@@ -0,0 +1,14 @@
+#include "tests.h"
+
+TEST(string, rtrim)
+{
+    char buf[1024];
+    strcpy(buf, "hello world  ");
+    swoole_rtrim(buf, strlen(buf));
+    ASSERT_EQ(strcmp("hello world", buf), 0);
+    ASSERT_NE(strcmp("hello world  ", buf), 0);
+
+    strcpy(buf, "  ");
+    swoole_rtrim(buf, strlen(buf));
+    ASSERT_EQ(strlen(buf), 0);
+}
