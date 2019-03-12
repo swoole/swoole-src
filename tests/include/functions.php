c7f9f51c86e359aacc808658b9d9778bf2319947
twosee <twose@qq.com>
2019-03-12 18:10:15 +0800

    Use PHP socket again.

--- tests/include/functions.php
+++ tests/include/functions.php
@@ -57,6 +57,22 @@ function is_alpine_linux(): bool
 
 function get_one_free_port()
 {
+    $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+    if (!socket_bind($socket, "0.0.0.0", 0)) {
+        return false;
+    }
+    if (!socket_listen($socket)) {
+        return false;
+    }
+    if (!socket_getsockname($socket, $addr, $port)) {
+        return false;
+    }
+    socket_close($socket);
+    return $port;
+}
+
+function get_one_free_port_coro()
+{
     $socket = new Co\Socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
     $socket->bind('0.0.0.0');
     $socket->listen();
