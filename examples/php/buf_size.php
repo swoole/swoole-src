df6e2d1d9e684effa037ab3a9d56a00a2494c0b4
matyhtf <mikan.tenny@gmail.com>
2019-04-02 12:56:17 +0800

    update example

--- examples/php/buf_size.php
+++ examples/php/buf_size.php
@@ -6,9 +6,17 @@ socket_create_pair(AF_UNIX, SOCK_DGRAM, 0, $fds);
 $socket = $fds[0];
 
 socket_set_option($socket, SOL_SOCKET, SO_SNDBUF, 8 * 1024 * 1024);
-$retval = socket_get_option($socket, SOL_SOCKET, SO_SNDBUF);
-var_dump($retval);
+$send_buf_size = socket_get_option($socket, SOL_SOCKET, SO_SNDBUF);
+echo "send_buf_size=$send_buf_size\n";
 
-$n = $retval - 32;
+
+socket_set_option($socket, SOL_SOCKET, SO_RCVBUF, 8 * 1024);
+$recv_buf_size = socket_get_option($socket, SOL_SOCKET, SO_RCVBUF);
+echo "recv_buf_size=$recv_buf_size\n";
+
+$n = $send_buf_size - 32;
 $ret_n = socket_write($socket, str_repeat('A', $n), $n);
+var_dump($ret_n);
+
+$ret_n = socket_read($fds[1], $n);
 var_dump($ret_n);
