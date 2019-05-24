49084b793160c6b3d1a783c4cc7755176f4bfd8c
windrunner <windrunner414@outlook.com>
2019-05-24 11:23:54 +0800

    Rename and optimize remake script (#2594)

--- remake_library.sh
+++ remake_library.sh
@@ -1,7 +1,8 @@
+#!/bin/sh
 echo "rm swoole_runtime.lo"
 rm swoole_runtime.lo
 echo "rm library/*.h"
 rm library/*.h
-php tools/build-library.php
+/usr/bin/env php tools/build-library.php
 echo "done"
-make
+make $*
