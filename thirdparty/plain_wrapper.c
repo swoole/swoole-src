d86114d24c8d6419392f6ac8cbb97af543e71913
matyhtf <mikan.tenny@gmail.com>
2019-06-06 15:53:50 +0800

    format

--- thirdparty/plain_wrapper.c
+++ thirdparty/plain_wrapper.c
@@ -213,143 +213,93 @@ static php_stream *_sw_php_stream_fopen_from_fd_int(int fd, const char *mode, co
     self->is_process_pipe = 0;
     self->temp_name = NULL;
     self->fd = fd;
-#ifdef PHP_WIN32
-    self->is_pipe_blocking = 0;
-#endif
-
     return php_stream_alloc_rel(&sw_php_stream_stdio_ops, self, persistent_id, mode);
 }
 
 static size_t php_stdiop_write(php_stream *stream, const char *buf, size_t count)
 {
-	php_stdio_stream_data *data = (php_stdio_stream_data*)stream->abstract;
+    php_stdio_stream_data *data = (php_stdio_stream_data*) stream->abstract;
 
-	assert(data != NULL);
+    assert(data != NULL);
 
-	if (data->fd >= 0) {
-#ifdef PHP_WIN32
-		int bytes_written;
-		if (ZEND_SIZE_T_UINT_OVFL(count)) {
-			count = UINT_MAX;
-		}
-		bytes_written = _write(data->fd, buf, (unsigned int)count);
-#else
-		int bytes_written = write(data->fd, buf, count);
-#endif
-		if (bytes_written < 0) return 0;
-		return (size_t) bytes_written;
-	} else {
-
-#if HAVE_FLUSHIO
-		if (!data->is_pipe && data->last_op == 'r') {
-			zend_fseek(data->file, 0, SEEK_CUR);
-		}
-		data->last_op = 'w';
-#endif
-
-		return fwrite(buf, 1, count, data->file);
-	}
+    if (data->fd >= 0)
+    {
+        int bytes_written = write(data->fd, buf, count);
+        if (bytes_written < 0)
+        {
+            return 0;
+        }
+        return (size_t) bytes_written;
+    }
+    else
+    {
+        return fwrite(buf, 1, count, data->file);
+    }
 }
 
 static size_t php_stdiop_read(php_stream *stream, char *buf, size_t count)
 {
-	php_stdio_stream_data *data = (php_stdio_stream_data*)stream->abstract;
-	size_t ret;
+    php_stdio_stream_data *data = (php_stdio_stream_data*) stream->abstract;
+    size_t ret;
 
-	assert(data != NULL);
+    assert(data != NULL);
 
-	if (data->fd >= 0) {
-#ifdef PHP_WIN32
-		php_stdio_stream_data *self = (php_stdio_stream_data*)stream->abstract;
+    if (data->fd >= 0)
+    {
+        ret = read(data->fd, buf, PLAIN_WRAP_BUF_SIZE(count));
 
-		if ((self->is_pipe || self->is_process_pipe) && !self->is_pipe_blocking) {
-			HANDLE ph = (HANDLE)_get_osfhandle(data->fd);
-			int retry = 0;
-			DWORD avail_read = 0;
+        if (ret == (size_t) -1 && errno == EINTR)
+        {
+            /* Read was interrupted, retry once,
+             If read still fails, giveup with feof==0
+             so script can retry if desired */
+            ret = read(data->fd, buf, PLAIN_WRAP_BUF_SIZE(count));
+        }
 
-			do {
-				/* Look ahead to get the available data amount to read. Do the same
-					as read() does, however not blocking forever. In case it failed,
-					no data will be read (better than block). */
-				if (!PeekNamedPipe(ph, NULL, 0, NULL, &avail_read, NULL)) {
-					break;
-				}
-				/* If there's nothing to read, wait in 10ms periods. */
-				if (0 == avail_read) {
-					usleep(10);
-				}
-			} while (0 == avail_read && retry++ < 3200000);
-
-			/* Reduce the required data amount to what is available, otherwise read()
-				will block.*/
-			if (avail_read < count) {
-				count = avail_read;
-			}
-		}
-#endif
-		ret = read(data->fd, buf,  PLAIN_WRAP_BUF_SIZE(count));
-
-		if (ret == (size_t)-1 && errno == EINTR) {
-			/* Read was interrupted, retry once,
-			   If read still fails, giveup with feof==0
-			   so script can retry if desired */
-			ret = read(data->fd, buf,  PLAIN_WRAP_BUF_SIZE(count));
-		}
-
-		stream->eof = (ret == 0 || (ret == (size_t)-1 && errno != EWOULDBLOCK && errno != EINTR && errno != EBADF));
-
-	} else {
-#if HAVE_FLUSHIO
-		if (!data->is_pipe && data->last_op == 'w')
-			zend_fseek(data->file, 0, SEEK_CUR);
-		data->last_op = 'r';
-#endif
-
-		ret = fread(buf, 1, count, data->file);
-
-		stream->eof = feof(data->file);
-	}
-	return ret;
+        stream->eof = (ret == 0 || (ret == (size_t) -1 && errno != EWOULDBLOCK && errno != EINTR && errno != EBADF));
+    }
+    else
+    {
+        ret = fread(buf, 1, count, data->file);
+        stream->eof = feof(data->file);
+    }
+    return ret;
 }
 
 static int sw_php_stdiop_close(php_stream *stream, int close_handle)
 {
-	int ret;
-	php_stdio_stream_data *data = (php_stdio_stream_data*)stream->abstract;
+    int ret;
+    php_stdio_stream_data *data = (php_stdio_stream_data*) stream->abstract;
 
-	assert(data != NULL);
+    assert(data != NULL);
 
-#if HAVE_MMAP
-	if (data->last_mapped_addr) {
-		munmap(data->last_mapped_addr, data->last_mapped_len);
-		data->last_mapped_addr = NULL;
-	}
-#elif defined(PHP_WIN32)
-	if (data->last_mapped_addr) {
-		UnmapViewOfFile(data->last_mapped_addr);
-		data->last_mapped_addr = NULL;
-	}
-	if (data->file_mapping) {
-		CloseHandle(data->file_mapping);
-		data->file_mapping = NULL;
-	}
-#endif
+    if (data->last_mapped_addr)
+    {
+        munmap(data->last_mapped_addr, data->last_mapped_len);
+        data->last_mapped_addr = NULL;
+    }
 
-	if (close_handle) {
-		if (data->file) {
-			if (data->is_process_pipe) {
-				errno = 0;
-				ret = pclose(data->file);
+    if (close_handle)
+    {
+        if (data->file)
+        {
+            if (data->is_process_pipe)
+            {
+                errno = 0;
+                ret = pclose(data->file);
 
 #if HAVE_SYS_WAIT_H
-				if (WIFEXITED(ret)) {
-					ret = WEXITSTATUS(ret);
-				}
+                if (WIFEXITED(ret))
+                {
+                    ret = WEXITSTATUS(ret);
+                }
 #endif
-			} else {
-				ret = fclose(data->file);
-				data->file = NULL;
-			}
+            }
+            else
+            {
+                ret = fclose(data->file);
+                data->file = NULL;
+            }
         }
         else if (data->fd != -1)
         {
@@ -364,71 +314,75 @@ static int sw_php_stdiop_close(php_stream *stream, int close_handle)
         {
             return 0; /* everything should be closed already -> success */
         }
-		if (data->temp_name) {
-#ifdef PHP_WIN32
-			php_win32_ioutil_unlink(ZSTR_VAL(data->temp_name));
-#else
-			unlink(ZSTR_VAL(data->temp_name));
-#endif
-			/* temporary streams are never persistent */
-			zend_string_release(data->temp_name);
-			data->temp_name = NULL;
-		}
-	} else {
-		ret = 0;
-		data->file = NULL;
-		data->fd = -1;
-	}
+        if (data->temp_name)
+        {
+            unlink(ZSTR_VAL(data->temp_name));
+            /* temporary streams are never persistent */
+            zend_string_release(data->temp_name);
+            data->temp_name = NULL;
+        }
+    }
+    else
+    {
+        ret = 0;
+        data->file = NULL;
+        data->fd = -1;
+    }
 
-	pefree(data, stream->is_persistent);
+    pefree(data, stream->is_persistent);
 
-	return ret;
+    return ret;
 }
 
 static int php_stdiop_flush(php_stream *stream)
 {
-	php_stdio_stream_data *data = (php_stdio_stream_data*)stream->abstract;
+    php_stdio_stream_data *data = (php_stdio_stream_data*) stream->abstract;
 
-	assert(data != NULL);
+    assert(data != NULL);
 
-	/*
-	 * stdio buffers data in user land. By calling fflush(3), this
-	 * data is send to the kernel using write(2). fsync'ing is
-	 * something completely different.
-	 */
-	if (data->file) {
-		return fflush(data->file);
-	}
-	return 0;
+    /*
+     * stdio buffers data in user land. By calling fflush(3), this
+     * data is send to the kernel using write(2). fsync'ing is
+     * something completely different.
+     */
+    if (data->file)
+    {
+        return fflush(data->file);
+    }
+    return 0;
 }
 
 static int php_stdiop_seek(php_stream *stream, zend_off_t offset, int whence, zend_off_t *newoffset)
 {
-	php_stdio_stream_data *data = (php_stdio_stream_data*)stream->abstract;
-	int ret;
+    php_stdio_stream_data *data = (php_stdio_stream_data*) stream->abstract;
+    int ret;
 
-	assert(data != NULL);
+    assert(data != NULL);
 
-	if (data->is_pipe) {
-		php_error_docref(NULL, E_WARNING, "cannot seek on a pipe");
-		return -1;
-	}
+    if (data->is_pipe)
+    {
+        php_error_docref(NULL, E_WARNING, "cannot seek on a pipe");
+        return -1;
+    }
 
-	if (data->fd >= 0) {
-		zend_off_t result;
+    if (data->fd >= 0)
+    {
+        zend_off_t result;
 
-		result = lseek(data->fd, offset, whence);
-		if (result == (zend_off_t)-1)
-			return -1;
+        result = lseek(data->fd, offset, whence);
+        if (result == (zend_off_t) -1)
+            return -1;
 
-		*newoffset = result;
-		return 0;
+        *newoffset = result;
+        return 0;
 
-	} else {
-		ret = zend_fseek(data->file, offset, whence);
-		*newoffset = zend_ftell(data->file);
-		return ret;
-	}
+    }
+    else
+    {
+        ret = zend_fseek(data->file, offset, whence);
+        *newoffset = zend_ftell(data->file);
+        return ret;
+    }
 }
 
 static int php_stdiop_cast(php_stream *stream, int castas, void **ret)
@@ -1319,30 +1273,26 @@ static php_stream *_sw_php_stream_fopen_from_fd(int fd, const char *mode, const 
 {
     php_stream *stream = sw_php_stream_fopen_from_fd_int_rel(fd, mode, persistent_id);
 
-    if (stream) {
-        php_stdio_stream_data *self = (php_stdio_stream_data*)stream->abstract;
+    if (stream)
+    {
+        php_stdio_stream_data *self = (php_stdio_stream_data*) stream->abstract;
 
-#ifdef S_ISFIFO
         /* detect if this is a pipe */
-        if (self->fd >= 0) {
+        if (self->fd >= 0)
+        {
             self->is_pipe = (do_fstat(self, 0) == 0 && S_ISFIFO(self->sb.st_mode)) ? 1 : 0;
         }
-#elif defined(PHP_WIN32)
+
+        if (self->is_pipe)
         {
-            zend_uintptr_t handle = _get_osfhandle(self->fd);
-
-            if (handle != (zend_uintptr_t)INVALID_HANDLE_VALUE) {
-                self->is_pipe = GetFileType((HANDLE)handle) == FILE_TYPE_PIPE;
-            }
-        }
-#endif
-
-        if (self->is_pipe) {
             stream->flags |= PHP_STREAM_FLAG_NO_SEEK;
-        } else {
+        }
+        else
+        {
             stream->position = zend_lseek(self->fd, 0, SEEK_CUR);
 #ifdef ESPIPE
-            if (stream->position == (zend_off_t)-1 && errno == ESPIPE) {
+            if (stream->position == (zend_off_t) -1 && errno == ESPIPE)
+            {
                 stream->position = 0;
                 stream->flags |= PHP_STREAM_FLAG_NO_SEEK;
                 self->is_pipe = 1;
