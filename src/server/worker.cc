db6819b38c870d7eada3008ccd262957f2aa2c11
matyhtf <mikan.tenny@gmail.com>
2019-07-10 15:28:14 +0800

    fixed #2675

--- src/server/worker.cc
+++ src/server/worker.cc
@@ -534,6 +534,9 @@ void swWorker_stop(swWorker *worker)
         swReactor_remove_read_event(reactor, worker->pipe_worker);
     }
 
+    //ignore signal
+    reactor->signal_listener_num = 0;
+
     if (serv->factory_mode == SW_MODE_BASE)
     {
         swListenPort *port;
