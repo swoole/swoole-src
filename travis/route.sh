a394b6570ca9bb28190c671a9f29ef5167fa7c26
twosee <twose@qq.com>
2019-07-11 11:44:23 +0800

    Update travis script and doc

--- travis/route.sh
+++ travis/route.sh
@@ -1,12 +1,12 @@
-#!/bin/sh -e
+#!/bin/sh
 __CURRENT__=`pwd`
 __DIR__=$(cd "$(dirname "$0")";pwd)
 
+export DOCKER_COMPOSE_VERSION="1.21.0"
 [ -z "${TRAVIS_BRANCH}" ] && export TRAVIS_BRANCH="master"
 [ -z "${TRAVIS_BUILD_DIR}" ] && export TRAVIS_BUILD_DIR=$(cd "$(dirname "$0")";cd ../;pwd)
-export DOCKER_COMPOSE_VERSION="1.21.0"
-export PHP_VERSION_ID=`php -r "echo PHP_VERSION_ID;"`
-if [ ${PHP_VERSION_ID} -lt 70300 ]; then
+[ -z "${PHP_VERSION_ID}" ] && export PHP_VERSION_ID=`php -r "echo PHP_VERSION_ID;"`
+if [ ${PHP_VERSION_ID} -lt 70400 ]; then
     export PHP_VERSION="`php -r "echo PHP_MAJOR_VERSION;"`.`php -r "echo PHP_MINOR_VERSION;"`"
 else
     export PHP_VERSION="rc"
