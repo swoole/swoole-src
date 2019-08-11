c91a0d5b8df7be6d84364c512c1374fbac582538
Bruce Dou <doubaokun@gmail.com>
2019-08-11 21:29:00 +0100

    Fixed example in sub.php
    The previous example is broken since v4.4.0

--- examples/coroutine/redis/sub.php
+++ examples/coroutine/redis/sub.php
@@ -1,13 +1,20 @@
 <?php
 go(function () {
-
-
-$redis = new Swoole\Coroutine\Redis();
-$redis->connect('127.0.0.1', 6379);
-//while (true) 
-{
+	$redis = new Swoole\Coroutine\Redis();
+	$redis->connect('127.0.0.1', 6379);
 	$msg = $redis->subscribe(array("msg_1"));
-	var_dump($msg);
-}
+	while ($msg = $redis->recv()) 
+	{
+		var_dump($msg);
+	}
+});
 
+go(function () {
+	$redis = new Swoole\Coroutine\Redis();
+	$redis->connect('127.0.0.1', 6379);
+	$msg = $redis->subscribe(array("msg_2"));
+	while ($msg = $redis->recv()) 
+	{
+		var_dump($msg);
+	}
 });
