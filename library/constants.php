24d0621917188c463fb4ce219b2dc34d29fa71af
twosee <twose@qq.com>
2019-11-04 09:28:29 +0800

    Optimization

--- library/constants.php
+++ library/constants.php
@@ -3,8 +3,4 @@ define('SWOOLE_LIBRARY', true);
 
 $useShortname = ini_get_all('swoole')['swoole.use_shortname']['local_value'];
 $useShortname = strtolower(trim(str_replace('0', '', $useShortname)));
-if (! in_array($useShortname, ['', 'off', 'false'], true)) {
-    define('SWOOLE_USE_SHORTNAME', true);
-} else {
-    define('SWOOLE_USE_SHORTNAME', false);
-}
+define('SWOOLE_USE_SHORTNAME', !in_array($useShortname, ['', 'off', 'false'], true));
