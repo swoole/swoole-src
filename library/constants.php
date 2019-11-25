279c7693d722cd766d5311450e615339b546efbe
twosee <twose@qq.com>
2019-11-25 15:31:14 +0800

    Do not pollute the global symbol table

--- library/constants.php
+++ library/constants.php
@@ -1,6 +1,11 @@
 <?php
 define('SWOOLE_LIBRARY', true);
-
-$useShortname = ini_get_all('swoole')['swoole.use_shortname']['local_value'];
-$useShortname = strtolower(trim(str_replace('0', '', $useShortname)));
-define('SWOOLE_USE_SHORTNAME', !in_array($useShortname, ['', 'off', 'false'], true));
+define('SWOOLE_USE_SHORTNAME',
+    !in_array(
+        strtolower(trim(str_replace('0', '',
+            ini_get_all('swoole')['swoole.use_shortname']['local_value']
+        ))),
+        ['', 'off', 'false'],
+        true
+    )
+);
