dd9fbf2f03fb297766e8c69748605e40b582cf87
matyhtf <mikan.tenny@gmail.com>
2020-01-09 12:17:41 +0800

    disable_accept when have too many connections
    (cherry picked from commit f1ad96a332e8e8aec7582fcaa4649f5a39bcff01)

--- src/server/master.cc
+++ src/server/master.cc
@@ -124,6 +124,7 @@ int swServer_master_onAccept(swReactor *reactor, swEvent *event)
         {
             swoole_error_log(SW_LOG_WARNING, SW_ERROR_SERVER_TOO_MANY_SOCKET, "Too many connections [now: %d]", new_fd);
             close(new_fd);
+            swServer_disable_accept(serv);
             return SW_OK;
         }
 
