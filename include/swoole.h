4fbd4723ef54caf7fced87d86f04e1866eaddff6
twosee <twose@qq.com>
2020-01-16 12:01:59 +0800

    Update version to 4.4.16-alpha

--- include/swoole.h
+++ include/swoole.h
@@ -102,10 +102,10 @@ int clock_gettime(clock_id_t which_clock, struct timespec *t);
 
 #define SWOOLE_MAJOR_VERSION      4
 #define SWOOLE_MINOR_VERSION      4
-#define SWOOLE_RELEASE_VERSION    15
-#define SWOOLE_EXTRA_VERSION      ""
-#define SWOOLE_VERSION            "4.4.15"
-#define SWOOLE_VERSION_ID         40415
+#define SWOOLE_RELEASE_VERSION    16
+#define SWOOLE_EXTRA_VERSION      "alpha"
+#define SWOOLE_VERSION            "4.4.16-alpha"
+#define SWOOLE_VERSION_ID         40416
 #define SWOOLE_BUG_REPORT \
     "A bug occurred in Swoole-v" SWOOLE_VERSION ", please report it.\n"\
     "The Swoole developers probably don't know about it,\n"\
