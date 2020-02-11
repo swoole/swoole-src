a98589aa94b96352540a689434c698fe0510777b
GXhua <393323503@qq.com>
2020-02-11 22:32:53 +0800

    fix double eval

--- php_swoole_cxx.cc
+++ php_swoole_cxx.cc
@@ -53,9 +53,14 @@ zend_op_array* zend::swoole_compile_string(zval *source_string, ZEND_STR_CONST c
     return opa;
 }
 
+
 bool zend::eval(std::string code, std::string filename)
 {
-    old_compile_string = zend_compile_string;
+    static zend_bool init = 0;
+    if (!init) {
+        init = 1;
+        old_compile_string = zend_compile_string;
+    }
     //overwrite
     zend_compile_string = swoole_compile_string;
     int ret = (zend_eval_stringl((char*) code.c_str(), code.length(), nullptr, (char *) filename.c_str()) == SUCCESS);
