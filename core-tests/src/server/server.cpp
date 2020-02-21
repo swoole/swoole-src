ef7a1f345eb8928096850b4d1320604fb04727ca
codinghuang <2812240764@qq.com>
2020-02-21 02:52:12 +0000

    test: delete test

--- core-tests/src/server/server.cpp
+++ core-tests/src/server/server.cpp
@@ -3,19 +3,6 @@
 
 using namespace std;
 
-TEST(server, set_ipc_max_size)
-{
-    int ret;
-    swServer serv;
-
-    create_test_server(&serv);
-
-    ret = swFactoryProcess_create_pipes(&serv.factory);
-    ASSERT_EQ(0, ret);
-    swServer_set_ipc_max_size(&serv);
-    ASSERT_GT(serv.ipc_max_size, 0);
-}
-
 TEST(server, create_pipe_buffers)
 {
     int ret;
