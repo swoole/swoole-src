b4e64eef3012da8298ec47b14cc3924a9ed369da
codinghuang <2812240764@qq.com>
2020-02-21 08:31:22 +0000

    redefinition
    swManager_spawn_worker(swServer *serv, int worker_id) to swManager_spawn_worker(swServer *serv, swWorker *worker)

--- src/server/manager.cc
+++ src/server/manager.cc
@@ -43,7 +43,7 @@ typedef std::unordered_map<uint32_t, pid_t> reload_list_t;
 
 static int swManager_loop(swServer *serv);
 static void swManager_signal_handler(int sig);
-static pid_t swManager_spawn_worker(swServer *serv, int worker_id);
+static pid_t swManager_spawn_worker(swServer *serv, swWorker *worker);
 static void swManager_check_exit_status(swServer *serv, int worker_id, pid_t pid, int status);
 
 static swManagerProcess ManagerProcess;
@@ -187,7 +187,7 @@ int swManager_start(swServer *serv)
          */
         for (i = 0; i < serv->worker_num; i++)
         {
-            pid = swManager_spawn_worker(serv, i);
+            pid = swManager_spawn_worker(serv, &serv->workers[i]);
             if (pid < 0)
             {
                 swError("fork() failed");
@@ -318,7 +318,7 @@ static int swManager_loop(swServer *serv)
                 }
                 else
                 {
-                    pid_t new_pid = swManager_spawn_worker(serv, msg.worker_id);
+                    pid_t new_pid = swManager_spawn_worker(serv, &serv->workers[msg.worker_id]);
                     if (new_pid > 0)
                     {
                         serv->workers[msg.worker_id].pid = new_pid;
@@ -427,7 +427,7 @@ static int swManager_loop(swServer *serv)
 
                 while (1)
                 {
-                    new_pid = swManager_spawn_worker(serv, i);
+                    new_pid = swManager_spawn_worker(serv, &serv->workers[i]);
                     if (new_pid < 0)
                     {
                         SW_START_SLEEP;
@@ -563,7 +563,7 @@ static int swManager_loop(swServer *serv)
     return SW_OK;
 }
 
-static pid_t swManager_spawn_worker(swServer *serv, int worker_id)
+static pid_t swManager_spawn_worker(swServer *serv, swWorker *worker)
 {
     pid_t pid;
     int ret;
@@ -579,7 +579,7 @@ static pid_t swManager_spawn_worker(swServer *serv, int worker_id)
     //worker child processor
     else if (pid == 0)
     {
-        ret = swWorker_loop(serv, worker_id);
+        ret = swWorker_loop(serv, worker->id);
         exit(ret);
     }
     //parent,add to writer
