b71e6d6bd1a1921d0fc83c7e452ce36e6de62295
matyhtf <mikan.tenny@gmail.com>
2020-03-12 23:52:55 +0800

    update http_autoindex style

--- examples/http/static_handler.php
+++ examples/http/static_handler.php
@@ -3,6 +3,7 @@ $http = new Swoole\Http\Server("0.0.0.0", 9501, SWOOLE_BASE);
 //$http = new swoole_http_server("0.0.0.0", 9501);
 $http->set([
     'enable_static_handler' => true,
+    'http_autoindex' => true,
     'document_root' => __DIR__,
 ]);
 
