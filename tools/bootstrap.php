3a9ca6368ec7bd52528611f322023fe95ee8b0d8
twosee <twose@qq.com>
2020-03-13 10:31:14 +0800

    Add comments

--- tools/bootstrap.php
+++ tools/bootstrap.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of Swoole, for internal use only
+ *
+ * @link     https://www.swoole.com
+ * @contact  team@swoole.com
+ * @license  https://github.com/swoole/library/blob/master/LICENSE
+ */
+
 define('ROOT_DIR', dirname(__DIR__));
 define('LIBRARY_DIR', ROOT_DIR . '/library');
 define('LIBRARY_SRC_DIR', LIBRARY_DIR . '/src');
