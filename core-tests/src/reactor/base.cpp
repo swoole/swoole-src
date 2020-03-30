444fe067780258ba18da8f0de41a4ec290262229
codinghuang <2812240764@qq.com>
2020-03-30 02:07:21 +0000

    test: add TEST(reactor, swReactor_create)

---
+++ core-tests/src/reactor/base.cpp
@@ -0,0 +1,38 @@
+#include "tests.h"
+
+TEST(reactor, swReactor_create)
+{
+    swReactor reactor;
+    swReactor_create(&reactor, SW_REACTOR_MAXEVENTS);
+
+    ASSERT_EQ(reactor.running, 1);
+    ASSERT_NE(reactor.onFinish, nullptr);
+    ASSERT_NE(reactor.onTimeout, nullptr);
+    ASSERT_NE(reactor.is_empty, nullptr);
+    ASSERT_EQ(reactor.can_exit, nullptr); // set in PHP_METHOD(swoole_coroutine_scheduler, set)
+    ASSERT_NE(reactor.write, nullptr);
+    ASSERT_NE(reactor.close, nullptr);
+    ASSERT_NE(reactor.defer, nullptr);
+    ASSERT_EQ(reactor.defer_tasks, nullptr);
+    ASSERT_NE(reactor.default_write_handler, nullptr);
+
+    /**
+     * coroutine socket reactor
+     */
+    ASSERT_NE(reactor.read_handler[swReactor_fdtype(SW_FD_CORO_SOCKET | SW_EVENT_READ)], nullptr);
+    ASSERT_NE(reactor.write_handler[swReactor_fdtype(SW_FD_CORO_SOCKET | SW_EVENT_WRITE)], nullptr);
+    ASSERT_NE(reactor.error_handler[swReactor_fdtype(SW_FD_CORO_SOCKET | SW_EVENT_ERROR)], nullptr);
+
+    /**
+     * system reactor
+     */
+    ASSERT_NE(reactor.read_handler[swReactor_fdtype(SW_FD_CORO_POLL | SW_EVENT_READ)], nullptr);
+    ASSERT_NE(reactor.write_handler[swReactor_fdtype(SW_FD_CORO_POLL | SW_EVENT_WRITE)], nullptr);
+    ASSERT_NE(reactor.error_handler[swReactor_fdtype(SW_FD_CORO_POLL | SW_EVENT_ERROR)], nullptr);
+
+    ASSERT_NE(reactor.read_handler[swReactor_fdtype(SW_FD_CORO_EVENT | SW_EVENT_READ)], nullptr);
+    ASSERT_NE(reactor.write_handler[swReactor_fdtype(SW_FD_CORO_EVENT | SW_EVENT_WRITE)], nullptr);
+    ASSERT_NE(reactor.error_handler[swReactor_fdtype(SW_FD_CORO_EVENT | SW_EVENT_ERROR)], nullptr);
+
+    ASSERT_NE(reactor.read_handler[swReactor_fdtype(SW_FD_AIO | SW_EVENT_READ)], nullptr);
+}
