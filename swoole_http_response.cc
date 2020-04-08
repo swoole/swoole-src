3154110522c1031804b04410f84735f6da485a70
matyhtf <mikan.tenny@gmail.com>
2020-04-08 16:05:18 +0800

    update arginfo

--- swoole_http_response.cc
+++ swoole_http_response.cc
@@ -247,6 +247,7 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_http_response_redirect, 0, 0, 1)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_http_response_create, 0, 0, 1)
+    ZEND_ARG_INFO(0, server)
     ZEND_ARG_INFO(0, fd)
 ZEND_END_ARG_INFO()
 
