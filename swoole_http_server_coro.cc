04a47ca910f66098e5009426f17d7d1f88962ef3
twosee <twose@qq.com>
2020-04-23 19:22:02 +0800

    Fix shutdown during onAccept (#3267)
    * Fix shutdown during handshake
    
    * Fix HTTP2 receivers
    
    * Remove duplicated code

--- swoole_http_server_coro.cc
+++ swoole_http_server_coro.cc
@@ -569,30 +569,29 @@ static PHP_METHOD(swoole_http_server_coro, onAccept)
     ZEND_PARSE_PARAMETERS_END_EX(RETURN_FALSE);
 
     Socket *sock = php_swoole_get_socket(zconn);
+    swString* buffer = sock->get_read_buffer();
+    size_t total_bytes = 0;
+    http_context *ctx = nullptr;
+
+    hs->receivers.push_front(sock);
+    auto receiver = hs->receivers.begin();
 
 #ifdef SW_USE_OPENSSL
     if (sock->open_ssl)
     {
         if (!sock->ssl_handshake())
         {
-            return;
+            goto _handshake_failed;
         }
     }
 #endif
-
-    swString* buffer = sock->get_read_buffer();
-    size_t total_bytes = 0;
-    http_context *ctx = nullptr;
 
     while (true)
     {
         ssize_t retval;
         if (ctx != nullptr || total_bytes == 0)
         {
-            hs->receivers.push_front(sock);
-            auto receiver = hs->receivers.begin();
             retval = sock->recv(buffer->str + total_bytes, buffer->size - total_bytes);
-            hs->receivers.erase(receiver);
 
             if (sw_unlikely(retval <= 0))
             {
@@ -653,7 +652,9 @@ static PHP_METHOD(swoole_http_server_coro, onAccept)
             buffer->length = total_bytes - (sizeof(SW_HTTP2_PRI_STRING) - 1);
             buffer->offset = buffer->length == 0 ? 0 : (sizeof(SW_HTTP2_PRI_STRING) - 1);
             hs->recv_http2_frame(ctx);
-            return;
+            /* ownership of ctx has been transferred */
+            ctx = nullptr;
+            break;
         }
 #endif
 
@@ -711,6 +712,11 @@ static PHP_METHOD(swoole_http_server_coro, onAccept)
         zval_dtor(ctx->request.zobject);
         zval_dtor(ctx->response.zobject);
     }
+
+#ifdef SW_USE_OPENSSL
+    _handshake_failed:
+#endif
+    hs->receivers.erase(receiver);
 }
 
 static PHP_METHOD(swoole_http_server_coro, shutdown)
