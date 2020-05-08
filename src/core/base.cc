7e035b72d4bbd9e2696c92748d5a296c601de9a0
韩天峰-Rango <mikan.tenny@gmail.com>
2020-05-08 11:04:40 +0800

    remove swoole_error_log[php func], add core tests (#3300)
    * remove swoole_error_log[php func], add core tests
    
    * fix tests
    
    * fix tests[2]
    
    * fix tests[3]
    
    Co-authored-by: twosee <twose@qq.com>

--- src/core/base.cc
+++ src/core/base.cc
@@ -726,6 +726,9 @@ swString* swoole_file_get_contents(const char *filename)
     }
     close(fd);
     content->length = readn;
+    swString_append_ptr(content, "\0", 1);
+    content->length--;
+
     return content;
 }
 
