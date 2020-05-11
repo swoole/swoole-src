3074d50b8d426b8731fa9b417257495fc79d17a1
twosee <twose@qq.com>
2020-05-11 14:07:32 +0800

    Update version for Swoole 4.5.1

--- include/swoole_version.h
+++ include/swoole_version.h
@@ -21,8 +21,8 @@
 #define SWOOLE_MAJOR_VERSION      4
 #define SWOOLE_MINOR_VERSION      5
 #define SWOOLE_RELEASE_VERSION    1
-#define SWOOLE_EXTRA_VERSION      "alpha"
-#define SWOOLE_VERSION            "4.5.1-alpha"
+#define SWOOLE_EXTRA_VERSION      ""
+#define SWOOLE_VERSION            "4.5.1"
 #define SWOOLE_VERSION_ID         40501
 #define SWOOLE_API_VERSION_ID     0x202003a
 
