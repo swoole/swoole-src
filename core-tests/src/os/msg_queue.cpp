334fbac45597740b31661fb159d505c4e735744c
matyhtf <mikan.tenny@gmail.com>
2020-06-30 23:36:52 +0800

    add msg_queue test, fix tests

---
+++ core-tests/src/os/msg_queue.cpp
@@ -0,0 +1,44 @@
+/*
+  +----------------------------------------------------------------------+
+  | Swoole                                                               |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 2.0 of the Apache license,    |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+  | If you did not receive a copy of the Apache2.0 license and are unable|
+  | to obtain it through the world-wide-web, please send a note to       |
+  | license@swoole.com so we can mail you a copy immediately.            |
+  +----------------------------------------------------------------------+
+  | @link     https://www.swoole.com/                                    |
+  | @contact  team@swoole.com                                            |
+  | @license  https://github.com/swoole/swoole-src/blob/master/LICENSE   |
+  | @author   Tianfeng Han  <mikan.tenny@gmail.com>                      |
+  +----------------------------------------------------------------------+
+*/
+
+#include "test_coroutine.h"
+
+TEST(msg_queue, rbac)
+{
+    swMsgQueue q;
+    ASSERT_EQ(swMsgQueue_create(&q, 0, 0, 0), SW_OK);
+    swQueue_data in;
+    in.mtype = 999;
+    strcpy(in.mdata, "hello world");
+
+    ASSERT_EQ(swMsgQueue_push(&q, &in, strlen(in.mdata)), SW_OK);
+
+    int queue_num, queue_bytes;
+    ASSERT_EQ(swMsgQueue_stat(&q, &queue_num, &queue_bytes), SW_OK);
+    ASSERT_EQ(queue_num, 1);
+    ASSERT_GT(queue_bytes, 10);
+
+    swQueue_data out = {};
+    ASSERT_GT(swMsgQueue_pop(&q, &out, sizeof(out)), 1);
+
+    ASSERT_EQ(out.mtype, in.mtype);
+    ASSERT_STREQ(out.mdata, in.mdata);
+}
+
+
