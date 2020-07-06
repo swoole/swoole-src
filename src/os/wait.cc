e8d01a8e42fdf0b11de9656bfb176c14fd2bc804
codinghuang <2812240764@qq.com>
2020-07-06 12:54:39 +0800

    fix: fix #3444 (#3446)
    * fix: fix #3444
    
    We must set the signal before fork, or we may lose the signal
    
    * fix: register SIGCHLD signal before try waitpid
    
    * improvement: add comment

--- src/os/wait.cc
+++ src/os/wait.cc
@@ -134,15 +134,14 @@ pid_t System::waitpid(pid_t __pid, int *__stat_loc, int __options, double timeou
         return ::waitpid(__pid, __stat_loc, __options);
     }
 
-    /* try once if failed we init the task */
+    /* try once if failed we init the task, and we must register SIGCHLD before try waitpid, or we may lose the SIGCHLD */
     wait_task task;
+    signal_init();
     task.pid = ::waitpid(__pid, __stat_loc, __options | WNOHANG);
     if (task.pid > 0)
     {
         return task.pid;
     }
-
-    signal_init();
 
     task.pid = -1;
     task.status = 0;
