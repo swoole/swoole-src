85bb2a3eac231ed92b592671f72efec188f6cae3
twosee <twose@qq.com>
2020-07-31 15:32:28 +0800

    Fix compilation (#3522)

--- src/os/base.cc
+++ src/os/base.cc
@@ -294,14 +294,16 @@ void handler_gethostbyname(Event *event) {
     int ret;
 
 #ifndef HAVE_GETHOSTBYNAME2_R
-    g_gethostbyname2_lock.lock();
+    static std::mutex lock;
+    lock.lock();
 #endif
 
     ret = swoole::network::gethostbyname(event->flags, (char *) event->buf, addr);
 
     sw_memset_zero(event->buf, event->nbytes);
+
 #ifndef HAVE_GETHOSTBYNAME2_R
-    g_gethostbyname2_lock.unlock();
+    lock.unlock();
 #endif
 
     if (ret < 0) {
