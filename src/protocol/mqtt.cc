29b14bfcbf6e259f07450fdca95ee7f96a1019ff
matyhtf <mikan.tenny@gmail.com>
2020-08-02 21:27:45 +0800

    code format

--- src/protocol/mqtt.cc
+++ src/protocol/mqtt.cc
@@ -23,24 +23,21 @@ void swMqtt_print_package(swMqtt_packet *pkg) {
     printf("type=%d, length=%d\n", pkg->type, pkg->length);
 }
 
-void swMqtt_set_protocol(swProtocol *protocol)
-{
+void swMqtt_set_protocol(swProtocol *protocol) {
     protocol->package_length_size = SW_MQTT_MAX_LENGTH_SIZE;
     protocol->package_length_offset = 1;
     protocol->package_body_offset = 0;
     protocol->get_package_length = swMqtt_get_package_length;
 }
 
-//recv variable_header packet twice may cause that the '*data' contain the payload data,
-//but there's no chance to read the next mqtt request ,because MQTT client will recv ACK blocking
-#define MQTT_RECV_LEN_AGAIN    0
+// recv variable_header packet twice may cause that the '*data' contain the payload data,
+// but there's no chance to read the next mqtt request ,because MQTT client will recv ACK blocking
+#define MQTT_RECV_LEN_AGAIN 0
 
-ssize_t swMqtt_get_package_length(swProtocol *protocol, swSocket *conn,const char *data, uint32_t size)
-{
+ssize_t swMqtt_get_package_length(swProtocol *protocol, swSocket *conn, const char *data, uint32_t size) {
     //-1 cause the arg 'size' contain length_offset(1 byte len)
     uint32_t recv_variable_header_size = (size - 1);
-    if (recv_variable_header_size < SW_MQTT_MIN_LENGTH_SIZE)
-    {//recv continue
+    if (recv_variable_header_size < SW_MQTT_MIN_LENGTH_SIZE) {  // recv continue
         return MQTT_RECV_LEN_AGAIN;
     }
 
@@ -48,26 +45,25 @@ ssize_t swMqtt_get_package_length(swProtocol *protocol, swSocket *conn,const cha
     int mul = 1;
     ssize_t length = 0;
     ssize_t variable_header_byte_count = 0;
-    while (1)
-    {
+    while (1) {
         variable_header_byte_count++;
         byte = data[variable_header_byte_count];
         length += (byte & 127) * mul;
         mul *= 128;
-        if ((byte & 128) == 0)
-        {//done! there is no surplus length byte
+        if ((byte & 128) == 0) {  // done! there is no surplus length byte
             break;
         }
-        if (variable_header_byte_count >= SW_MQTT_MAX_LENGTH_SIZE)
-        {
-            swoole_error_log(SW_LOG_WARNING, SW_ERROR_PACKAGE_LENGTH_TOO_LARGE, "bad request, The variable header size uper than %d", SW_MQTT_MAX_LENGTH_SIZE);
+        if (variable_header_byte_count >= SW_MQTT_MAX_LENGTH_SIZE) {
+            swoole_error_log(SW_LOG_WARNING,
+                             SW_ERROR_PACKAGE_LENGTH_TOO_LARGE,
+                             "bad request, The variable header size uper than %d",
+                             SW_MQTT_MAX_LENGTH_SIZE);
             return SW_ERR;
         }
-        if (variable_header_byte_count >= recv_variable_header_size)
-        {//length not enough
+        if (variable_header_byte_count >= recv_variable_header_size) {  // length not enough
             return MQTT_RECV_LEN_AGAIN;
         }
     }
-    //payload_length + variable_header_byte_count + length_offset(1)
+    // payload_length + variable_header_byte_count + length_offset(1)
     return length + variable_header_byte_count + 1;
 }
