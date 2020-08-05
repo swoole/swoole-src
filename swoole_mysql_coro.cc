swoole/swoole-src

commit 62e5be1f1dbc8216d36a105289bb8538bf560775

Code specification (#3463)
* format , logger point

* classname toupper

* fix tests

* fix tests [2]

swoole_mysql_coro.cc
@@ -59,7 +59,7 @@ using swoole::coroutine::Socket;
 namespace swoole {
 class mysql_statement;
 class mysql_client {
-   public:
+  public:
     /* session related {{{ */
     Socket *socket = nullptr;
     Socket::timeout_controller *tc = nullptr;
@@ -295,7 +295,7 @@ class mysql_client {
         close();
     }
 
-   private:
+  private:
     int error_code = 0;
     std::string error_msg = "";
 
@@ -311,7 +311,7 @@ class mysql_client {
 };
 
 class mysql_statement {
-   public:
+  public:
     std::string statement;
     mysql::statement info;
     mysql::result_info result;
@@ -389,7 +389,7 @@ class mysql_statement {
     void fetch_all(zval *return_value);
     void next_result(zval *return_value);
 
-   private:
+  private:
     mysql_client *client = nullptr;
     int error_code = 0;
     std::string error_msg;
@@ -472,7 +472,7 @@ ZEND_END_ARG_INFO()
 
 #ifdef SW_USE_MYSQLND
 ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_mysql_coro_escape, 0, 0, 1)
-    ZEND_ARG_INFO(0, string)
+    ZEND_ARG_INFO(0, String)
     ZEND_ARG_INFO(0, flags)
 ZEND_END_ARG_INFO()
 #endif
@@ -1219,7 +1219,7 @@ void mysql_statement::send_execute_request(zval *return_value, zval *params) {
                 *((buffer->str + null_start_offset) + (index / 8)) |= (1UL << (index % 8));
                 sw_mysql_int2store((buffer->str + type_start_offset) + (index * 2), SW_MYSQL_TYPE_NULL);
             } else {
-                zend::string str_value(value);
+                zend::String str_value(value);
                 uint8_t lcb_size = mysql::write_lcb(stack_buffer, str_value.len());
                 sw_mysql_int2store((buffer->str + type_start_offset) + (index * 2), SW_MYSQL_TYPE_VAR_STRING);
                 if (swString_append_ptr(buffer, stack_buffer, lcb_size) < 0) {
@@ -1715,7 +1715,7 @@ static PHP_METHOD(swoole_mysql_coro, connect) {
         zval *ztmp;
 
         if (php_swoole_array_get_value(ht, "host", ztmp)) {
-            mc->host = std::string(zend::string(ztmp).val());
+            mc->host = std::string(zend::String(ztmp).val());
         } else {
             zend_throw_exception(swoole_mysql_coro_exception_ce, "Parameter [host] is required", EINVAL);
             RETURN_FALSE;
@@ -1736,19 +1736,19 @@ static PHP_METHOD(swoole_mysql_coro, connect) {
 #endif
         }
         if (php_swoole_array_get_value(ht, "user", ztmp)) {
-            mc->user = std::string(zend::string(ztmp).val());
+            mc->user = std::string(zend::String(ztmp).val());
         } else {
             zend_throw_exception(swoole_mysql_coro_exception_ce, "Parameter [user] is required", EINVAL);
             RETURN_FALSE;
         }
         if (php_swoole_array_get_value(ht, "password", ztmp)) {
-            mc->password = std::string(zend::string(ztmp).val());
+            mc->password = std::string(zend::String(ztmp).val());
         } else {
             zend_throw_exception(swoole_mysql_coro_exception_ce, "Parameter [password] is required", EINVAL);
             RETURN_FALSE;
         }
         if (php_swoole_array_get_value(ht, "database", ztmp)) {
-            mc->database = std::string(zend::string(ztmp).val());
+            mc->database = std::string(zend::String(ztmp).val());
         } else {
             zend_throw_exception(swoole_mysql_coro_exception_ce, "Parameter [database] is required", EINVAL);
             RETURN_FALSE;
@@ -1757,7 +1757,7 @@ static PHP_METHOD(swoole_mysql_coro, connect) {
             mc->connect_timeout = zval_get_double(ztmp);
         }
         if (php_swoole_array_get_value(ht, "charset", ztmp)) {
-            zend::string zstr_charset(ztmp);
+            zend::String zstr_charset(ztmp);
             char charset = mysql::get_charset(zstr_charset.val());
             if (UNEXPECTED(charset < 0)) {
                 zend_throw_exception_ex(

.clang-format
@@ -1,7 +1,7 @@
 ---
 Language:        Cpp
 # BasedOnStyle:  Google
-AccessModifierOffset: -1
+AccessModifierOffset: -2
 AlignAfterOpenBracket: Align
 AlignConsecutiveAssignments: false
 AlignConsecutiveDeclarations: false
@@ -11,7 +11,7 @@ AlignTrailingComments: true
 AllowAllParametersOfDeclarationOnNextLine: true
 AllowShortBlocksOnASingleLine: false
 AllowShortCaseLabelsOnASingleLine: false
-AllowShortFunctionsOnASingleLine: Inline
+AllowShortFunctionsOnASingleLine: Empty
 AllowShortIfStatementsOnASingleLine: true
 AllowShortLoopsOnASingleLine: true
 AlwaysBreakAfterDefinitionReturnType: None

.editorconfig
@@ -1,0 +1,31 @@
+# https://editorconfig.org/
+
+root = true
+
+[*]
+trim_trailing_whitespace = true
+insert_final_newline     = true
+end_of_line              = lf
+charset                  = utf-8
+indent_style             = space
+tab_width                = 4
+
+[{*.{awk,sh},Makefile*}]
+indent_size              = 4
+indent_style             = table
+max_line_length          = 80
+
+[*.{c,cc,cpp,h,html,inc,php,phpt}]
+indent_size              = 4
+indent_style             = space
+max_line_length          = 120
+
+[*.md]
+indent_style             = space
+max_line_length          = 120
+
+[COMMIT_EDITMSG]
+indent_size              = 4
+indent_style             = space
+max_line_length          = 80
+

core-tests/src/core/log.cpp
@@ -7,17 +7,17 @@ using namespace swoole;
 const char *file = "/tmp/swoole_log_test.log";
 
 TEST(log, level) {
-    sw_logger().reset();
-    sw_logger().set_level(SW_LOG_NOTICE);
-    sw_logger().open(file);
+    sw_logger()->reset();
+    sw_logger()->set_level(SW_LOG_NOTICE);
+    sw_logger()->open(file);
 
-    sw_logger().put(SW_LOG_INFO, SW_STRL("hello info"));
-    sw_logger().put(SW_LOG_NOTICE, SW_STRL("hello notice"));
-    sw_logger().put(SW_LOG_WARNING, SW_STRL("hello warning"));
+    sw_logger()->put(SW_LOG_INFO, SW_STRL("hello info"));
+    sw_logger()->put(SW_LOG_NOTICE, SW_STRL("hello notice"));
+    sw_logger()->put(SW_LOG_WARNING, SW_STRL("hello warning"));
 
     swoole::String content(swoole_file_get_contents(file));
 
-    sw_logger().close();
+    sw_logger()->close();
     unlink(file);
 
     ASSERT_FALSE(swString_contains(content.get(), SW_STRL("hello info")));
@@ -26,14 +26,14 @@ TEST(log, level) {
 }
 
 TEST(log, date_format) {
-    sw_logger().reset();
-    sw_logger().set_date_format("day %d of %B in the year %Y. Time: %I:%S %p");
-    sw_logger().open(file);
+    sw_logger()->reset();
+    sw_logger()->set_date_format("day %d of %B in the year %Y. Time: %I:%S %p");
+    sw_logger()->open(file);
 
-    sw_logger().put(SW_LOG_WARNING, SW_STRL("hello world"));
+    sw_logger()->put(SW_LOG_WARNING, SW_STRL("hello world"));
     swoole::String content(swoole_file_get_contents(file));
 
-    sw_logger().close();
+    sw_logger()->close();
     unlink(file);
 
     int data[16];
@@ -55,29 +55,29 @@ TEST(log, date_format) {
 }
 
 TEST(log, date_format_long_string) {
-    sw_logger().reset();
-    sw_logger().set_level(SW_LOG_ERROR);
+    sw_logger()->reset();
+    sw_logger()->set_level(SW_LOG_ERROR);
     swoole::String content(swString_new(256));
     auto str = content.get();
 
     swString_repeat(str, "x", 1, 120);
     swString_append_ptr(str, SW_STRL("day %d of %B in the year %Y. Time: %I:%S %p"));
 
-    int retval = sw_logger().set_date_format(str->str);
+    int retval = sw_logger()->set_date_format(str->str);
 
     ASSERT_EQ(retval, SW_ERR);
     ASSERT_EQ(swoole_get_last_error(), SW_ERROR_INVALID_PARAMS);
 }
 
 TEST(log, date_with_microseconds) {
-    sw_logger().reset();
-    sw_logger().set_date_with_microseconds(true);
-    sw_logger().open(file);
+    sw_logger()->reset();
+    sw_logger()->set_date_with_microseconds(true);
+    sw_logger()->open(file);
 
-    sw_logger().put(SW_LOG_WARNING, SW_STRL("hello world"));
+    sw_logger()->put(SW_LOG_WARNING, SW_STRL("hello world"));
     swoole::String content(swoole_file_get_contents(file));
 
-    sw_logger().close();
+    sw_logger()->close();
     unlink(file);
 
     std::regex e("\\[\\S+\\s\\d{2}:\\d{2}:\\d{2}\\<\\.(\\d+)\\>\\s@\\d+\\.\\d+\\]\tWARNING\thello world");
@@ -85,18 +85,18 @@ TEST(log, date_with_microseconds) {
 }
 
 TEST(log, rotation) {
-    sw_logger().reset();
-    sw_logger().set_rotation(SW_LOG_ROTATION_DAILY);
-    sw_logger().open(file);
+    sw_logger()->reset();
+    sw_logger()->set_rotation(SW_LOG_ROTATION_DAILY);
+    sw_logger()->open(file);
 
-    sw_logger().put(SW_LOG_WARNING, SW_STRL("hello world"));
+    sw_logger()->put(SW_LOG_WARNING, SW_STRL("hello world"));
 
-    ASSERT_EQ(access(sw_logger().get_file(), R_OK), -1);
+    ASSERT_EQ(access(sw_logger()->get_file(), R_OK), -1);
     ASSERT_EQ(errno, ENOENT);
-    ASSERT_EQ(access(sw_logger().get_real_file(), R_OK), 0);
+    ASSERT_EQ(access(sw_logger()->get_real_file(), R_OK), 0);
 
-    sw_logger().close();
-    unlink(sw_logger().get_real_file());
+    sw_logger()->close();
+    unlink(sw_logger()->get_real_file());
 }
 
 TEST(log, redirect) {
@@ -106,20 +106,20 @@ TEST(log, redirect) {
         return;
     }
 
-    sw_logger().reset();
-    retval = sw_logger().open(file);
+    sw_logger()->reset();
+    retval = sw_logger()->open(file);
     ASSERT_EQ(retval, SW_OK);
 
-    retval = sw_logger().redirect_stdout_and_stderr(1);
+    retval = sw_logger()->redirect_stdout_and_stderr(1);
     ASSERT_EQ(retval, SW_OK);
     printf("hello world\n");
     swoole::String content(swoole_file_get_contents(file));
     ASSERT_NE(content.get(), nullptr);
 
-    sw_logger().close();
-    retval = sw_logger().redirect_stdout_and_stderr(0);
+    sw_logger()->close();
+    retval = sw_logger()->redirect_stdout_and_stderr(0);
     ASSERT_EQ(retval, SW_OK);
-    unlink(sw_logger().get_real_file());
+    unlink(sw_logger()->get_real_file());
 
     ASSERT_TRUE(swString_contains(content.get(), SW_STRL("hello world\n")));
 }

core-tests/src/memory/lru_cache.cpp
@@ -13,7 +13,7 @@ LRUCache cache(2);
 
 int dtor_num = 0;
 class lru_cache_test_class {
-   public:
+  public:
     lru_cache_test_class() {}
 
     ~lru_cache_test_class() { ++dtor_num; }

core-tests/src/memory/table.cpp
@@ -39,18 +39,18 @@ struct row_t {
 };
 
 class table_t {
-   private:
+  private:
     swTableColumn *column_id;
     swTableColumn *column_name;
     swTableColumn *column_score;
 
     swTable *table;
 
-   public:
+  public:
     table_t(uint32_t rows_size, float conflict_proportion = 0.2) {
         table = swTable_new(rows_size, conflict_proportion);
         if (swTable_create(table), 0) {
-            throw exception_t("alloc failed", SwooleG.error);
+            throw exception_t("alloc failed", swoole_get_last_error());
         }
 
         swTableColumn_add(table, "id", SW_TABLE_INT, 0);
@@ -58,7 +58,7 @@ class table_t {
         swTableColumn_add(table, "score", SW_TABLE_FLOAT, 0);
 
         if (swTable_create(table) < 0) {
-            throw exception_t("create failed", SwooleG.error);
+            throw exception_t("create failed", swoole_get_last_error());
         }
         column_id = swTableColumn_get(table, std::string("id"));
         column_name = swTableColumn_get(table, std::string("name"));

core-tests/src/network/stream.cpp
@@ -27,8 +27,8 @@ TEST(stream, send) {
     swServer serv;
     serv.worker_num = 1;
     serv.factory_mode = SW_MODE_BASE;
-    int ori_log_level = sw_logger().get_level();
-    sw_logger().set_level(SW_LOG_ERROR);
+    int ori_log_level = sw_logger()->get_level();
+    sw_logger()->set_level(SW_LOG_ERROR);
 
     swListenPort *port = serv.add_port(SW_SOCK_TCP, TEST_HOST, TEST_PORT);
     if (!port) {
@@ -104,5 +104,5 @@ TEST(stream, send) {
     serv.start();
     t1.join();
 
-    sw_logger().set_level(ori_log_level);
+    sw_logger()->set_level(ori_log_level);
 }

core-tests/src/os/timer.cpp
@@ -31,18 +31,20 @@ TEST(timer, sys) {
 
     uint64_t ms1 = swoole::time<std::chrono::milliseconds>();
 
-    swoole_timer_add(20, 0, [](swTimer *, swTimer_node *) { timer1_count++; }, nullptr);
+    swoole_timer_add(
+        20, 0, [](swTimer *, swTimer_node *) { timer1_count++; }, nullptr);
 
-    swoole_timer_add(100,
-                     1,
-                     [](swTimer *, swTimer_node *tnode) {
-                         timer2_count++;
-                         if (timer2_count == 5) {
-                             swoole_timer_del(tnode);
-                             timer_running = false;
-                         }
-                     },
-                     nullptr);
+    swoole_timer_add(
+        100,
+        1,
+        [](swTimer *, swTimer_node *tnode) {
+            timer2_count++;
+            if (timer2_count == 5) {
+                swoole_timer_del(tnode);
+                timer_running = false;
+            }
+        },
+        nullptr);
 
     while (1) {
         sleep(10);

commit 77c4d81b0791ec36663a3c9439fe17ad53f4b5be

fix the typo introduced in pull request #3463

swoole_mysql_coro.cc
@@ -479,7 +479,7 @@ ZEND_END_ARG_INFO()
 
 #ifdef SW_USE_MYSQLND
 ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_mysql_coro_escape, 0, 0, 1)
-    ZEND_ARG_INFO(0, String)
+    ZEND_ARG_INFO(0, string)
     ZEND_ARG_INFO(0, flags)
 ZEND_END_ARG_INFO()
 #endif