b765dd0130d84624c6bb05cb7d8b497cee821711
twosee <twose@qq.com>
2020-08-27 19:14:16 +0800

    Fix missing sched_yield()

--- include/swoole.h
+++ include/swoole.h
@@ -46,19 +46,16 @@
 #include <limits.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sched.h>  /* sched_yield() */
 
 #include <sys/utsname.h>
 #include <sys/time.h>
 
 #if defined(HAVE_CPU_AFFINITY)
-#ifdef __FreeBSD__
 #include <sys/types.h>
 #include <sys/cpuset.h>
 #include <pthread_np.h>
 typedef cpuset_t cpu_set_t;
-#else
-#include <sched.h>
-#endif
 #endif
 
 #include <memory>
