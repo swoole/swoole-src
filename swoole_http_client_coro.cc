6fcd88fa8f1335b31d41b67994a0e61e027b533f
韩天峰-Rango <mikan.tenny@gmail.com>
2020-09-07 18:18:28 +0800

    Fix #3533 (#3632)
    * fix #3533
    
    * update error message
    
    * update error code
    # Conflicts:
    #	swoole_http_client_coro.cc

--- swoole_http_client_coro.cc
+++ swoole_http_client_coro.cc
@@ -1491,6 +1491,7 @@ bool http_client::recv_http_response(double timeout)
     ssize_t retval = 0;
     size_t total_bytes = 0, parsed_n = 0;
     swString *buffer = socket->get_read_buffer();
+    bool header_completed = false;
 
     // re-init http response parser
     swoole_http_parser_init(&parser, PHP_HTTP_RESPONSE);
@@ -1507,7 +1508,7 @@ bool http_client::recv_http_response(double timeout)
         {
             return false;
         }
-        retval = socket->recv(buffer->str, buffer->size);
+        retval = socket->recv(buffer->str + buffer->length, buffer->size - buffer->length);
         if (sw_unlikely(retval <= 0))
         {
             if (retval == 0)
@@ -1521,6 +1522,24 @@ bool http_client::recv_http_response(double timeout)
             }
             return false;
         }
+
+        if (!header_completed) {
+            buffer->length += retval;
+            if (swoole_strnpos(buffer->str, buffer->length, ZEND_STRL("\r\n\r\n")) < 0) {
+                if (buffer->length == buffer->size) {
+                    swoole_error_log(SW_LOG_TRACE, SW_ERROR_HTTP_INVALID_PROTOCOL, "Http header too large");
+                    socket->set_err(SW_ERROR_HTTP_INVALID_PROTOCOL);
+                    return false;
+                }
+                buffer->offset = (ssize_t) buffer->length - 4 <= 0 ? 0 : buffer->length - 4;
+                continue;
+            } else {
+                header_completed = true;
+                retval = buffer->length;
+                swString_clear(buffer);
+            }
+        }
+
         total_bytes += retval;
         parsed_n = swoole_http_parser_execute(&parser, &http_parser_settings, buffer->str, retval);
         swTraceLog(SW_TRACE_HTTP_CLIENT, "parsed_n=%ld, retval=%ld, total_bytes=%ld, completed=%d", parsed_n, retval, total_bytes, parser.state == s_start_res);
@@ -1535,7 +1554,7 @@ bool http_client::recv_http_response(double timeout)
         }
         if (sw_unlikely(parser.state == s_dead))
         {
-            socket->set_err(EPROTO);
+            socket->set_err(SW_ERROR_HTTP_INVALID_PROTOCOL);
             return false;
         }
     }
