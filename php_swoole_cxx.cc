f063c6c2f1cf11528107054f8906e833c48e0b53
matyhtf <mikan.tenny@gmail.com>
2020-10-10 08:50:04 +0800

    Improve function::call

--- php_swoole_cxx.cc
+++ php_swoole_cxx.cc
@@ -20,7 +20,17 @@ typedef zend_string zend_source_string_t;
 
 static zend_op_array *swoole_compile_string(zend_source_string_t *source_string, ZEND_STR_CONST char *filename);
 
-bool zend::include(std::string file) {
+// for compatibly with dis_eval
+static zend_op_array *(*old_compile_string)(zend_source_string_t *source_string, ZEND_STR_CONST char *filename);
+
+static zend_op_array *swoole_compile_string(zend_source_string_t *source_string, ZEND_STR_CONST char *filename) {
+    zend_op_array *opa = old_compile_string(source_string, filename);
+    opa->type = ZEND_USER_FUNCTION;
+    return opa;
+}
+
+namespace zend {
+bool include(const std::string &file) {
     zend_file_handle file_handle;
     int ret = php_stream_open_for_zend_ex(file.c_str(), &file_handle, USE_PATH | STREAM_OPEN_FOR_INCLUDE);
     if (ret != SUCCESS) {
@@ -56,17 +66,7 @@ bool zend::include(std::string file) {
     return Z_TYPE(retval) == IS_TRUE;
 }
 
-// for compatibly with dis_eval
-
-static zend_op_array *(*old_compile_string)(zend_source_string_t *source_string, ZEND_STR_CONST char *filename);
-
-static zend_op_array *swoole_compile_string(zend_source_string_t *source_string, ZEND_STR_CONST char *filename) {
-    zend_op_array *opa = old_compile_string(source_string, filename);
-    opa->type = ZEND_USER_FUNCTION;
-    return opa;
-}
-
-bool zend::eval(std::string code, std::string filename) {
+bool eval(const std::string &code, std::string const &filename) {
     if (!old_compile_string) {
         old_compile_string = zend_compile_string;
     }
@@ -78,7 +78,7 @@ bool zend::eval(std::string code, std::string filename) {
     return ret;
 }
 
-void zend::known_strings_init(void) {
+void known_strings_init(void) {
     zend_string *str;
     sw_zend_known_strings = nullptr;
 
@@ -91,7 +91,45 @@ void zend::known_strings_init(void) {
     }
 }
 
-void zend::known_strings_dtor(void) {
+void known_strings_dtor(void) {
     pefree(sw_zend_known_strings, 1);
     sw_zend_known_strings = nullptr;
 }
+
+namespace function {
+
+bool call(zend_fcall_info_cache *fci_cache, uint32_t argc, zval *argv, zval *retval, const bool enable_coroutine) {
+    bool success;
+    if (enable_coroutine) {
+        if (retval) {
+            /* the coroutine has no return value */
+            ZVAL_NULL(retval);
+        }
+        success = swoole::PHPCoroutine::create(fci_cache, argc, argv) >= 0;
+    } else {
+        success = sw_zend_call_function_ex(nullptr, fci_cache, argc, argv, retval) == SUCCESS;
+    }
+    /* we have no chance to return to ZendVM to check the exception  */
+    if (UNEXPECTED(EG(exception))) {
+        zend_exception_error(EG(exception), E_ERROR);
+    }
+    return success;
+}
+
+ReturnValue call(const std::string &func_name, int argc, zval *argv) {
+    zval function_name;
+    ZVAL_STRINGL(&function_name, func_name.c_str(), func_name.length());
+    ReturnValue retval;
+    if (call_user_function(EG(function_table), NULL, &function_name, &retval.value, argc, argv) != SUCCESS) {
+        ZVAL_NULL(&retval.value);
+    }
+    zval_dtor(&function_name);
+    /* we have no chance to return to ZendVM to check the exception  */
+    if (UNEXPECTED(EG(exception))) {
+        zend_exception_error(EG(exception), E_ERROR);
+    }
+    return retval;
+}
+
+}  // namespace function
+}  // namespace zend
