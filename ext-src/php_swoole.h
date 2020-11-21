af74412b0bca02e898488afa1044e6d083577325
twosee <twose@qq.com>
2020-11-21 23:43:56 +0800

    Require PHP 8.0.1

--- ext-src/php_swoole.h
+++ ext-src/php_swoole.h
@@ -912,7 +912,7 @@ static sw_inline zend_bool sw_zend_is_callable_at_frame(zval *zcallable, zval *z
 {
     zend_string *name;
     zend_bool ret;
-#if PHP_VERSION_ID < 80000
+#if PHP_VERSION_ID < 80001
     ret = zend_is_callable_ex(zcallable, zobject ? Z_OBJ_P(zobject) : NULL, check_flags, &name, fci_cache, error);
 #else
     ret = zend_is_callable_at_frame(zcallable, zobject ? Z_OBJ_P(zobject) : NULL, frame, check_flags, fci_cache, error);
