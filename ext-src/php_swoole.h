acab513b0442c119cfdcc09cfe986e2410cb67ef
twosee <twose@qq.com>
2020-11-21 23:40:26 +0800

    Fix sw_zend_is_callable_at_frame()

--- ext-src/php_swoole.h
+++ ext-src/php_swoole.h
@@ -915,7 +915,8 @@ static sw_inline zend_bool sw_zend_is_callable_at_frame(zval *zcallable, zval *z
 #if PHP_VERSION_ID < 80000
     ret = zend_is_callable_ex(zcallable, zobject ? Z_OBJ_P(zobject) : NULL, check_flags, &name, fci_cache, error);
 #else
-    ret = zend_is_callable_at_frame(zcallable, zobject ? Z_OBJ_P(zobject) : NULL, frame, check_flags, &name, fci_cache, error);
+    ret = zend_is_callable_at_frame(zcallable, zobject ? Z_OBJ_P(zobject) : NULL, frame, check_flags, fci_cache, error);
+    name = zend_get_callable_name_ex(zcallable, zobject);
 #endif
     if (callable_name)
     {
