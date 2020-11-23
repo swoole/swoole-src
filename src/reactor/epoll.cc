4fb19bdb52a791a173b8ebe732252e428345bce2
matyhtf <mikan.tenny@gmail.com>
2020-11-23 14:10:59 +0800

    debug info

--- src/reactor/epoll.cc
+++ src/reactor/epoll.cc
@@ -136,6 +136,7 @@ int ReactorEpoll::del(Socket *_socket) {
         return SW_ERR;
     }
     if (epoll_ctl(epfd_, EPOLL_CTL_DEL, _socket->fd, nullptr) < 0) {
+        swoole_print_backtrace();
         swSysWarn("failed to delete events[fd=%d#%d, type=%d, events=%d]",
                   _socket->fd,
                   reactor_->id,
