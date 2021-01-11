846d93ff9626bcb05df30b00eb28915853b12b73
yumancang <mancangluo@tencent.com>
2021-01-11 10:10:14 +0800

    Add swoole_http_server ->getMethod()

--- ext-src/swoole_http_request.cc
+++ ext-src/swoole_http_request.cc
@@ -474,7 +474,7 @@ static int http_request_on_headers_complete(swoole_http_parser *parser) {
 
     ctx->keepalive = swoole_http_should_keep_alive(parser);
 
-    add_assoc_string(zserver, "request_method", (char *) http_get_method_name(parser->method));
+    add_assoc_string(zserver, "request_method", http_get_method_name(parser->method));
     add_assoc_stringl_ex(zserver, ZEND_STRL("request_uri"), ctx->request.path, ctx->request.path_len);
     // path_info should be decoded
     zend_string *zstr_path = zend_string_init(ctx->request.path, ctx->request.path_len, 0);
@@ -988,7 +988,7 @@ static PHP_METHOD(swoole_http_request, getMethod) {
     if (UNEXPECTED(!ctx)) {
         RETURN_FALSE;
     }
-    const char *method = (char *) http_get_method_name((ctx->parser).method);
+    const char *method = http_get_method_name((ctx->parser).method);
     RETURN_STRING(method);
 }
 
