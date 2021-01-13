9102098c3793199fcd0ea6f7225175476cc30288
codinghuang <2812240764@qq.com>
2021-01-13 12:04:30 +0800

    Fix coroutine::defer coredump in php8
    when not return value

--- ext-src/swoole_coroutine.cc
+++ ext-src/swoole_coroutine.cc
@@ -737,8 +737,11 @@ void PHPCoroutine::main_func(void *arg) {
             while (!tasks->empty()) {
                 zend::Function *defer_fci = tasks->top();
                 tasks->pop();
-                defer_fci->fci.param_count = 1;
-                defer_fci->fci.params = retval;
+                if (Z_TYPE_P(retval) != IS_UNDEF) {
+                    defer_fci->fci.param_count = 1;
+                    defer_fci->fci.params = retval;
+                }
+
                 if (UNEXPECTED(sw_zend_call_function_anyway(&defer_fci->fci, &defer_fci->fci_cache) != SUCCESS)) {
                     php_swoole_fatal_error(E_WARNING, "defer callback handler error");
                 }
