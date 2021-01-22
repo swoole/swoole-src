b74d5d4c1e78ec9c89ad728f5b2cd9dd89ef90b2
沈唁 <52o@qq52o.cn>
2021-01-22 16:44:06 +0800

    Optimize make.sh (#4009)
    * Optimize make.sh
    
    * Add CPU_COUNT and whoami
    
    * remove sync-module

--- make.sh
+++ make.sh
@@ -1,14 +1,23 @@
 #!/bin/sh -e
 __DIR__=$(cd "$(dirname "$0")";pwd)
-__EXT_DIR__=$(php-config --extension-dir)
+COMPILE_PARAMS="--enable-openssl --enable-sockets --enable-mysqlnd --enable-http2 --enable-swoole-json --enable-swoole-curl"
+
+if [ "$(uname | grep -i darwin)"x != ""x ]; then
+  CPU_COUNT="$(sysctl -n machdep.cpu.core_count)"
+else
+  CPU_COUNT="$(/usr/bin/nproc)"
+fi
+if [ -z ${CPU_COUNT} ]; then
+  CPU_COUNT=4
+fi
 
 cd "${__DIR__}"
 
 if [ "$1" = "cmake" ] ;then
   phpize
-  ./configure --enable-openssl --enable-swoole-json --enable-swoole-curl --enable-sockets --enable-mysqlnd --enable-http2
+  ./configure ${COMPILE_PARAMS}
   cmake .
-  make -j 8
+  make -j ${CPU_COUNT}
   exit 0
 fi
 
@@ -20,15 +29,9 @@ fi
 
 if [ "$1" = "install-module" ] ;then
   make ext-swoole
+  __EXT_DIR__=$(php-config --extension-dir)
   cp lib/swoole.so "${__EXT_DIR__}"
   echo "cp lib/swoole.so ${__EXT_DIR__}"
-  exit 0
-fi
-
-if [ "$1" = "sync-module" ] ;then
-  # thirtparty
-  git submodule init
-  git submodule update
   exit 0
 fi
 
@@ -45,7 +48,7 @@ if [ "$1" = "library" ] ;then
   if [ "$2" = "dev" ] ;then
     /usr/bin/env php tools/build-library.php dev
   else
-     /usr/bin/env php tools/build-library.php
+    /usr/bin/env php tools/build-library.php
   fi
 
   echo "remake..."
@@ -57,20 +60,24 @@ fi
 if [ "$1" = "help" ] ;then
   echo "./make.sh cmake"
   echo "./make.sh install-module"
-  echo "./make.sh sync-module"
   echo "./make.sh clean"
   echo "./make.sh debug"
+  echo "./make.sh library [dev]"
   echo "./make.sh"
-   echo "./make.sh library"
   exit 0
 fi
 
 phpize
 if [ "$1" = "debug" ] ;then
-  ./configure --enable-openssl --enable-swoole-json --enable-swoole-curl --enable-sockets --enable-mysqlnd --enable-http2 --enable-debug-log
+  ./configure ${COMPILE_PARAMS} --enable-debug-log
 else
-  ./configure --enable-openssl --enable-swoole-json --enable-swoole-curl --enable-sockets --enable-mysqlnd --enable-http2
+  ./configure ${COMPILE_PARAMS}
 fi
 make clean
-make -j 8
-make install
+make -j ${CPU_COUNT}
+
+if [ "$(whoami)" == "root" ];then
+    make install
+else
+    sudo make install
+fi
