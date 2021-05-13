366b7d5d07341229df3f56e076a196bb30ed5033
韩天峰-Rango <mikan.tenny@gmail.com>
2021-05-13 22:55:08 +0800

    Fix #4200 (#4208)
    * fix curl bug
    
    * fix tests

--- thirdparty/php/curl/interface.cc
+++ thirdparty/php/curl/interface.cc
@@ -134,6 +134,24 @@ php_curl *_php_curl_get_handle(zval *zid, bool exclusive, bool required) {
     return ch;
 }
 
+static long php_curl_easy_setopt_str(php_curl *ch, CURLoption option, const char *str) {
+    if (option == CURLOPT_PRIVATE) {
+        ch->private_data = str;
+        return CURLE_OK;
+    } else {
+        return curl_easy_setopt(ch->cp, option, str);
+    }
+}
+
+static long php_curl_easy_getinfo_str(php_curl *ch, CURLINFO option, char **value) {
+    if (option == CURLINFO_PRIVATE) {
+        *value = (char *) ch->private_data;
+        return CURLE_OK;
+    } else {
+        return curl_easy_getinfo(ch->cp, option, value);
+    }
+}
+
 static int php_curl_option_str(php_curl *ch, zend_long option, const char *str, const size_t len, zend_bool make_copy) {
     long error = CURLE_OK;
 
@@ -153,11 +171,11 @@ static int php_curl_option_str(php_curl *ch, zend_long option, const char *str, 
 
         /* Strings passed to libcurl as 'char *' arguments, are copied by the library since 7.17.0 */
         copystr = estrndup(str, len);
-        error = curl_easy_setopt(ch->cp, (CURLoption) option, copystr);
+        php_curl_easy_setopt_str(ch, (CURLoption) option, copystr);
         zend_llist_add_element(&ch->to_free->str, &copystr);
 #if LIBCURL_VERSION_NUM >= 0x071100
     } else {
-        error = curl_easy_setopt(ch->cp, (CURLoption) option, str);
+        error = php_curl_easy_setopt_str(ch, (CURLoption) option, str);
     }
 #endif
 
@@ -2186,7 +2204,7 @@ PHP_FUNCTION(swoole_native_curl_setopt) {
     Z_PARAM_ZVAL(zvalue)
     ZEND_PARSE_PARAMETERS_END();
 
-    if ((ch = _php_curl_get_handle(zid)) == NULL) {
+    if ((ch = _php_curl_get_handle(zid, false)) == NULL) {
         RETURN_FALSE;
     }
 
@@ -2219,7 +2237,7 @@ PHP_FUNCTION(swoole_native_curl_setopt_array) {
     Z_PARAM_ARRAY(arr)
     ZEND_PARSE_PARAMETERS_END();
 
-    if ((ch = _php_curl_get_handle(zid)) == NULL) {
+    if ((ch = _php_curl_get_handle(zid, false)) == NULL) {
         RETURN_FALSE;
     }
 
@@ -2536,7 +2554,7 @@ PHP_FUNCTION(swoole_native_curl_getinfo) {
             case CURLINFO_STRING: {
                 char *s_code = NULL;
 
-                if (curl_easy_getinfo(ch->cp, (CURLINFO) option, &s_code) == CURLE_OK && s_code) {
+                if (php_curl_easy_getinfo_str(ch, (CURLINFO) option, &s_code) == CURLE_OK && s_code) {
                     RETURN_STRING(s_code);
                 } else {
                     RETURN_FALSE;
