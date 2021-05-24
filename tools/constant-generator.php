d0d2223d844d93c95bb6968795f3d38483fae034
sy-records <52o@qq52o.cn>
2021-05-24 14:34:05 +0800

    Update constant to add event

--- tools/constant-generator.php
+++ tools/constant-generator.php
@@ -36,7 +36,39 @@ $constant_php_content = preg_replace(
 );
 
 if (!$replaced || !file_put_contents($constant_php, $constant_php_content)) {
-    swoole_error('Update Constant failed ');
+    swoole_error('Update constant option failed');
+}
+
+$server_events = [];
+$server_event_file = "{$root_dir}/ext-src/swoole_server.cc";
+if (file_exists($server_event_file)) {
+    $server_event_content = file_get_contents($server_event_file);
+    preg_match_all('/ServerEvent\(SW_SERVER_CB_on(.+?),/', $server_event_content, $server_event);
+    $server_events = array_merge($server_events, array_unique($server_event[1]));
+}
+$server_port_event_file = "{$root_dir}/ext-src/swoole_server_port.cc";
+if (file_exists($server_port_event_file)) {
+    $server_port_event_content = file_get_contents($server_port_event_file);
+    preg_match_all('/server_port_event\(SW_SERVER_CB_on(.+?),/', $server_port_event_content, $server_event);
+    $server_events = array_merge($server_events, array_unique($server_event[1]));
+}
+if (!empty($server_events)) {
+    $result = '';
+    foreach ($server_events as $event) {
+        $result .= space(4) . sprintf("public const EVENT_%s = '%s';\n\n", strtoupper($event), lcfirst($event));
+    }
+
+    $constant_php_content = file_get_contents($constant_php);
+    $constant_php_content = preg_replace(
+        '/(\/\* \{\{\{ EVENT \*\/\n)([\s\S]*)(\/\* \}\}\} EVENT \*\/)/',
+        '${1}' . $result . space(4) . '${3}',
+        $constant_php_content,
+        1,
+        $replaced
+    );
+    if (!$replaced || !file_put_contents($constant_php, $constant_php_content)) {
+        swoole_error('Update constant event failed');
+    }
 }
 
 swoole_success('Constant generator successfully done!');
