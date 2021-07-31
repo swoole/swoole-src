d518c3db4394edee7199fc6084dea6ddb86a013a
NathanFreeman <1056159381@qq.com>
2021-08-01 00:54:56 +0800

    unit test

--- core-tests/src/network/dns.cpp
+++ core-tests/src/network/dns.cpp
@@ -99,8 +99,25 @@ TEST(dns, load_resolv_conf) {
 }
 
 TEST(dns, gethosts) {
+    /**
+     * the contents of /etc/hosts are as follow
+     * 127.0.0.1 localhost www.baidu.com
+     *                  127.0.0.1 aaa.com   bbb.com  #ccc.com
+     * # 127.0.0.1 ddd.com
+     */
+
     std::string ip = swoole::coroutine::get_ip_by_hosts("localhost");
     ASSERT_EQ(ip, "127.0.0.1");
+
+    std::string ip = swoole::coroutine::get_ip_by_hosts("www.baidu.com");
+    ASSERT_EQ(ip, "127.0.0.1");
+
+
+    std::string ip = swoole::coroutine::get_ip_by_hosts("ccc.com");
+    ASSERT_EQ(ip, "");
+
+    std::string ip = swoole::coroutine::get_ip_by_hosts("ddd.com");
+    ASSERT_EQ(ip, "");
 
     ip = swoole::coroutine::get_ip_by_hosts("non.exist.com");
     ASSERT_EQ(ip, "");
