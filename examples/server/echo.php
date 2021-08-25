c3338461c17c299e58f0adab767bfdd1eafa96a2
matyhtf <mikan.tenny@gmail.com>
2021-08-25 18:13:38 +0800

    Update example

--- examples/server/echo.php
+++ examples/server/echo.php
@@ -1,26 +1,29 @@
 <?php
-// $serv = new swoole_server("0.0.0.0", 9501, SWOOLE_BASE);
-$serv = new swoole_server("0.0.0.0", 9501);
+$serv = new swoole_server("0.0.0.0", 9501, SWOOLE_BASE);
+// $serv = new swoole_server("0.0.0.0", 9501);
 
-//$serv->on('connect', function ($serv, $fd, $reactor_id){
-//	echo "[#".posix_getpid()."]\tClient@[$fd:$reactor_id]: Connect.\n";
-//});
+$serv->set([
+    'worker_num' =>1,
+]);
+
+$serv->on('connect', function ($serv, $fd, $reactor_id){
+	echo "[#".posix_getpid()."]\tClient@[$fd:$reactor_id]: Connect.\n";
+});
+
 $serv->set(array(
     'worker_num' => 1,
-
 ));
 
 $serv->on('receive', function (swoole_server $serv, $fd, $reactor_id, $data) {
 	echo "[#".$serv->worker_id."]\tClient[$fd] receive data: $data\n";
-    if ($serv->send($fd, "hello {$data}\n") == false)
-    {
+    if ($serv->send($fd, "hello {$data}\n") == false) {
         echo "error\n";
     }
 
 });
 
-//$serv->on('close', function ($serv, $fd, $reactor_id) {
-//	echo "[#".posix_getpid()."]\tClient@[$fd:$reactor_id]: Close.\n";
-//});
+$serv->on('close', function ($serv, $fd, $reactor_id) {
+	echo "[#".posix_getpid()."]\tClient@[$fd:$reactor_id]: Close.\n";
+});
 
 $serv->start();
