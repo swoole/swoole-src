625ff61375f4145e3f207e3925143080d5667d82
NathanFreeman <1056159381@qq.com>
2021-11-14 16:25:37 +0800

    remove default value

--- stubs/php_swoole_event.stub.php
+++ stubs/php_swoole_event.stub.php
@@ -1,7 +1,7 @@
 <?php
-function swoole_event_add(mixed $fd, ?callable $read_callback = null, ?callable $write_callback = null, ?int $events = null): bool {}
+function swoole_event_add(mixed $fd, ?callable $read_callback, ?callable $write_callback, int $events): bool {}
 
-function swoole_event_set(mixed $fd, ?callable $read_callback = null, ?callable $write_callback = null, ?int $events = null): bool {}
+function swoole_event_set(mixed $fd, ?callable $read_callback, ?callable $write_callback, int $events): bool {}
 
 function swoole_event_del(mixed $fd): bool {}
 
@@ -15,8 +15,8 @@ function swoole_event_exit(): void {}
 
 function swoole_event_defer(callable $callback): bool {}
 
-function swoole_event_cycle(?callable $callback = null, bool $before = false): bool {}
+function swoole_event_cycle(?callable $callback, bool $before = false): bool {}
 
 function swoole_event_dispatch(): bool {}
 
-function swoole_event_isset(mixed $fd, ?int $events = null): bool {}
+function swoole_event_isset(mixed $fd, int $events): bool {}
