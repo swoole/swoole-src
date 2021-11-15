a7ba3e86740dc5f5d479dbbc9a7ce032a5c86bc5
MARiA so cute <33935209+NathanFreeman@users.noreply.github.com>
2021-11-15 16:40:42 +0800

    We need to be more careful.

--- ext-src/swoole_runtime.cc
+++ ext-src/swoole_runtime.cc
@@ -120,9 +120,9 @@ static std::vector<std::string> unsafe_functions {
 
 static const zend_function_entry swoole_runtime_methods[] =
 {
-    PHP_ME(swoole_runtime, enableCoroutine, arginfo_class_Swoole_swoole_runtime_enableCoroutine, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
-    PHP_ME(swoole_runtime, getHookFlags, arginfo_class_Swoole_swoole_runtime_getHookFlags, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
-    PHP_ME(swoole_runtime, setHookFlags, arginfo_class_Swoole_swoole_runtime_setHookFlags, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
+    PHP_ME(swoole_runtime, enableCoroutine, arginfo_class_Swoole_Runtime_enableCoroutine, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
+    PHP_ME(swoole_runtime, getHookFlags, arginfo_class_Swoole_Runtime_getHookFlags, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
+    PHP_ME(swoole_runtime, setHookFlags, arginfo_class_Swoole_Runtime_setHookFlags, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
     PHP_FE_END
 };
 // clang-format on
