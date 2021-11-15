1b5a6da24f7ee8798366bc217dc67731cb55a54c
MARiA so cute <33935209+NathanFreeman@users.noreply.github.com>
2021-11-15 11:31:02 +0800

    swoole_runtime arginfo.

---
+++ stubs/php_swoole_runtime_arginfo.h
@@ -0,0 +1,20 @@
+/* This is a generated file, edit the .stub.php file instead.
+ * Stub hash: 44748fb2ad008f46ab4447d31004b2840aa46d61 */
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_swoole_runtime_enableCoroutine, 0, 0, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, enable, _IS_BOOL, 0, "true")
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, flags, IS_LONG, 0, "SWOOLE_HOOK_ALL")
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_swoole_runtime_getHookFlags, 0, 0, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_swoole_runtime_setHookFlags, 0, 0, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, flags, IS_LONG, 0, "SWOOLE_HOOK_ALL")
+ZEND_END_ARG_INFO()
+
+#define arginfo_class_Swoole_swoole_runtime_enableCoroutine arginfo_class_Swoole_swoole_runtime_enableCoroutine
+
+#define arginfo_class_Swoole_swoole_runtime_getHookFlags arginfo_class_Swoole_swoole_runtime_getHookFlags
+
+#define arginfo_class_Swoole_swoole_runtime_setHookFlags arginfo_class_Swoole_swoole_runtime_setHookFlags
