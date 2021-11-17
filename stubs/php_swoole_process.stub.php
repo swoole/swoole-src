5669aab918c8a98befd0029926969c94f5aaa736
NathanFreeman <1056159381@qq.com>
2021-11-17 20:43:15 +0800

    swoole_process arginfo.

---
+++ stubs/php_swoole_process.stub.php
@@ -0,0 +1,31 @@
+<?php
+namespace Swoole {
+    class Process {
+        public function __construct(callable $callback, bool $redirect_stdin_and_stdout = false, int $pipe_type = SOCK_DGRAM, bool $enable_coroutine = false) {}
+        public function __destruct() {}
+        public function useQueue(int $key = 0, int $mode = 2, int $capacity = -1): bool {}
+        public function statQueue(): array|bool {}
+        public function freeQueue(): bool {}
+        public function pop(int $size = 65536): bool|string {}
+        public function push(string $data): bool {}
+        public function kill(int $pid, int $signal_no = SIGTERM): bool {}
+        public function signal(int $signal_no, ?callable $callback = null): bool {}
+        public function alarm(int $usec, int $type = 0): bool {}
+        public function wait(bool $blocking = true): array|bool {}
+        public function daemon(bool $nochdir = true, bool $noclose = true, array $pipes = []): bool {}
+        public function setAffinity(array $cpu_settings): bool {}
+        public function set(array $settings): void|bool {}
+        public function setTimeout(float $seconds): bool {}
+        public function setBlocking(bool $blocking): void|bool {}
+        public function setPriority(int $which, int $priority):bool {}
+        public function getPriority(int $which): bool|int {}
+        public function start(): bool|int {}
+        public function write(string $data): bool|int {}
+        public function read(int $size = 8192): bool|string {}
+        public function close(int $which = 0): bool {}
+        public function exit(int $exit_code = 0): bool {}
+        public function exec(string $exec_file, array $args): bool {}
+        public function exportSocket(): \Swoole\Coroutine\Socket|bool {}
+        public function name(string $process_name): bool {}
+    }
+}
---
+++ stubs/php_swoole_process_legacy_arginfo.h
@@ -0,0 +1,131 @@
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_construct, 0, 0, 1)
+    ZEND_ARG_CALLABLE_INFO(0, callback, 0)
+    ZEND_ARG_INFO(0, redirect_stdin_and_stdout)
+    ZEND_ARG_INFO(0, pipe_type)
+    ZEND_ARG_INFO(0, enable_coroutine)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_void, 0, 0, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_wait, 0, 0, 0)
+    ZEND_ARG_INFO(0, blocking)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_signal, 0, 0, 2)
+    ZEND_ARG_INFO(0, signal_no)
+    ZEND_ARG_INFO(0, callback)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_alarm, 0, 0, 1)
+    ZEND_ARG_INFO(0, usec)
+    ZEND_ARG_INFO(0, type)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_kill, 0, 0, 1)
+    ZEND_ARG_INFO(0, pid)
+    ZEND_ARG_INFO(0, signal_no)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_daemon, 0, 0, 0)
+    ZEND_ARG_INFO(0, nochdir)
+    ZEND_ARG_INFO(0, noclose)
+    ZEND_ARG_INFO(0, pipes)
+ZEND_END_ARG_INFO()
+
+#ifdef HAVE_CPU_AFFINITY
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_setAffinity, 0, 0, 1)
+    ZEND_ARG_ARRAY_INFO(0, cpu_settings, 0)
+ZEND_END_ARG_INFO()
+#endif
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_setPriority, 0, 0, 2)
+    ZEND_ARG_INFO(0, which)
+    ZEND_ARG_INFO(0, priority)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_getPriority, 0, 0, 1)
+    ZEND_ARG_INFO(0, which)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_set, 0, 0, 1)
+    ZEND_ARG_ARRAY_INFO(0, settings, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_setTimeout, 0, 0, 1)
+    ZEND_ARG_INFO(0, seconds)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_setBlocking, 0, 0, 1)
+    ZEND_ARG_INFO(0, blocking)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_useQueue, 0, 0, 0)
+    ZEND_ARG_INFO(0, key)
+    ZEND_ARG_INFO(0, mode)
+    ZEND_ARG_INFO(0, capacity)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_write, 0, 0, 1)
+    ZEND_ARG_INFO(0, data)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_read, 0, 0, 0)
+    ZEND_ARG_INFO(0, size)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_push, 0, 0, 1)
+    ZEND_ARG_INFO(0, data)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_pop, 0, 0, 0)
+    ZEND_ARG_INFO(0, size)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_exit, 0, 0, 0)
+    ZEND_ARG_INFO(0, exit_code)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_exec, 0, 0, 2)
+    ZEND_ARG_INFO(0, exec_file)
+    ZEND_ARG_INFO(0, args)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_name, 0, 0, 1)
+    ZEND_ARG_INFO(0, process_name)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_swoole_process_close, 0, 0, 1)
+    ZEND_ARG_INFO(0, which)
+ZEND_END_ARG_INFO()
+
+#define arginfo_class_Swoole_Process___construct arginfo_swoole_process_construct
+#define arginfo_class_Swoole_Process___destruct arginfo_swoole_process_void
+#define arginfo_class_Swoole_Process_wait arginfo_swoole_process_wait
+#define arginfo_class_Swoole_Process_signal arginfo_swoole_process_signal
+#define arginfo_class_Swoole_Process_alarm arginfo_swoole_process_alarm
+#define arginfo_class_Swoole_Process_kill arginfo_swoole_process_kill
+#define arginfo_class_Swoole_Process_daemon arginfo_swoole_process_daemon
+
+#ifdef HAVE_CPU_AFFINITY
+#define arginfo_class_Swoole_Process_setAffinity arginfo_swoole_process_setAffinity
+#endif
+
+#define arginfo_class_Swoole_Process_setPriority arginfo_swoole_process_setPriority
+#define arginfo_class_Swoole_Process_getPriority arginfo_swoole_process_setPriority
+#define arginfo_class_Swoole_Process_set arginfo_swoole_process_setPriority
+#define arginfo_class_Swoole_Process_setTimeout arginfo_swoole_process_setPriority
+#define arginfo_class_Swoole_Process_setBlocking arginfo_swoole_process_setPriority
+#define arginfo_class_Swoole_Process_useQueue arginfo_swoole_process_setPriority
+#define arginfo_class_Swoole_Process_statQueue arginfo_swoole_process_setPriority
+#define arginfo_class_Swoole_Process_freeQueue arginfo_swoole_process_setPriority
+#define arginfo_class_Swoole_Process_start arginfo_swoole_process_setPriority
+#define arginfo_class_Swoole_Process_write arginfo_swoole_process_setPriority
+#define arginfo_class_Swoole_Process_close arginfo_swoole_process_close
+#define arginfo_class_Swoole_Process_read arginfo_swoole_process_setPriority
+#define arginfo_class_Swoole_Process_push arginfo_swoole_process_setPriority
+#define arginfo_class_Swoole_Process_pop arginfo_swoole_process_setPriority
+#define arginfo_class_Swoole_Process_exit arginfo_swoole_process_setPriority
+#define arginfo_class_Swoole_Process_exec arginfo_swoole_process_exec
+#define arginfo_class_Swoole_Process_exportSocket arginfo_swoole_process_setPriority
+#define arginfo_class_Swoole_Process_name arginfo_swoole_process_name
