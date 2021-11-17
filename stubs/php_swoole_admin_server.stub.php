d487a29819e85720346c5b3181bb6a262cc6674b
MARiA so cute <33935209+NathanFreeman@users.noreply.github.com>
2021-11-17 13:08:28 +0800

    php_swoole arginfo and swoole_admin_server arginfo.

---
+++ stubs/php_swoole_admin_server.stub.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @generate-legacy-arginfo
+ * @generate-function-entries
+ */
+function swoole_get_objects(): bool|array
+{
+}
+
+function swoole_get_vm_status(): array
+{
+}
+
+function swoole_get_object_by_handle(int $handle): bool|object
+{
+}
