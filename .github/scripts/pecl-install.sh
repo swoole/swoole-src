71f71652fb1177ad7fb1c513362336fda8487479
tianfenghan <rango@swoole.com>
2022-08-05 16:17:30 +0800

    fix

--- .github/scripts/pecl-install.sh
+++ .github/scripts/pecl-install.sh
@@ -4,7 +4,7 @@ __DIR__=$(cd "$(dirname "$0")";pwd)
 
 cd ${__DIR__} && cd ../ && \
 pecl config-show && \
-php ./tools/pecl-package.php && package_file="`ls | grep swoole-*tgz`" && \
+php ../tools/pecl-package.php && package_file="`ls | grep swoole-*tgz`" && \
 echo "\n" | pecl install -f ${package_file} | tee pecl.log && \
 cat pecl.log | grep "successfully" && \
 pecl uninstall swoole && \
