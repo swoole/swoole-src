e07b33dc9c00a5f79701e829eda10ac9cca1db41
NathanFreeman <1056159381@qq.com>
2022-09-21 21:57:58 +0800

    fix compile error for low openssl

--- src/protocol/dtls.cc
+++ src/protocol/dtls.cc
@@ -125,7 +125,11 @@ BIO_METHOD *BIO_get_methods(void) {
 #ifdef OPENSSL_IS_BORINGSSL
     BIO_meth_set_ctrl(_bio_methods, (long (*)(BIO *, int, long, void *)) BIO_callback_ctrl);
 #else
+#if defined(__MACH__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
     BIO_meth_set_callback_ctrl(_bio_methods, (long (*)(BIO *, int, BIO_info_cb *)) BIO_callback_ctrl);
+#else
+    BIO_meth_set_callback_ctrl(_bio_methods, BIO_callback_ctrl);
+#endif
 #endif
 
     return _bio_methods;
@@ -180,10 +184,10 @@ bool Session::listen() {
     } else if (retval < 0) {
         int reason = ERR_GET_REASON(ERR_peek_error());
         swoole_warning("DTLSv1_listen() failed, client[%s:%d], reason=%d, error_string=%s",
-               socket->info.get_ip(),
-               socket->info.get_port(),
-               reason,
-               swoole_ssl_get_error());
+                       socket->info.get_ip(),
+                       socket->info.get_port(),
+                       reason,
+                       swoole_ssl_get_error());
         return false;
     } else {
         listened = true;
