8b4479c94772de9f065099bc6a86b88b0ea0a7ab
MARiA so cute <33935209+NathanFreeman@users.noreply.github.com>
2022-09-26 12:41:20 +0800

    Fix bug #4838 (#4842)
    * fix bug #4838
    
    * fix help

--- src/server/master.cc
+++ src/server/master.cc
@@ -452,7 +452,11 @@ int Server::start_master_thread() {
     if (!single_thread) {
         pthread_barrier_wait(&reactor_thread_barrier);
     }
+#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
+    SW_START_SLEEP;
+#else
     pthread_barrier_wait(&gs->manager_barrier);
+#endif
 #else
     SW_START_SLEEP;
 #endif
@@ -856,8 +860,10 @@ int Server::create() {
 #ifdef HAVE_PTHREAD_BARRIER
     if (is_process_mode()) {
         pthread_barrier_init(&reactor_thread_barrier, nullptr, reactor_num + 1);
+#if !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__NetBSD__)
         pthread_barrierattr_setpshared(&gs->manager_barrier_attr, PTHREAD_PROCESS_SHARED);
         pthread_barrier_init(&gs->manager_barrier, &gs->manager_barrier_attr, 2);
+#endif
     }
 #endif
 
@@ -997,8 +1003,10 @@ void Server::destroy() {
 #ifdef HAVE_PTHREAD_BARRIER
     if (is_process_mode()) {
         pthread_barrier_destroy(&reactor_thread_barrier);
+#if !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__NetBSD__)
         pthread_barrier_destroy(&gs->manager_barrier);
         pthread_barrierattr_destroy(&gs->manager_barrier_attr);
+#endif
     }
 #endif
     sw_shm_free(session_list);
