a6ccf7baa8e1a2fe8036a51f7459fc0e0103a180
Quarkay <MrQuarkay@gmail.com>
2022-10-13 17:02:59 +0800

    Robust or not is set in constructor
    [EINVAL]
        The mutex object referenced by mutex is not robust.

--- src/lock/mutex.cc
+++ src/lock/mutex.cc
@@ -25,6 +25,8 @@ struct MutexImpl {
 };
 
 Mutex::Mutex(int flags) : Lock() {
+    flags_ = flags;
+
     if (flags & PROCESS_SHARED) {
         impl = (MutexImpl *) sw_mem_pool()->alloc(sizeof(*impl));
         if (impl == nullptr) {
@@ -62,11 +64,13 @@ Mutex::Mutex(int flags) : Lock() {
 
 int Mutex::lock() {
     int retval = pthread_mutex_lock(&impl->lock_);
+
 #ifdef HAVE_PTHREAD_MUTEX_CONSISTENT
-    if (retval == EOWNERDEAD) {
+    if (retval == EOWNERDEAD && (flags_ & ROBUST)) {
         retval = pthread_mutex_consistent(&impl->lock_);
     }
 #endif
+
     return retval;
 }
 
