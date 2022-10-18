fb48c2e2edbc49bd8a9c5683d6a1d549db8ca6a1
tianfenghan <rango@swoole.com>
2022-10-18 15:04:08 +0800

    Update version code

--- include/swoole_version.h
+++ include/swoole_version.h
@@ -20,10 +20,10 @@
 
 #define SWOOLE_MAJOR_VERSION 5
 #define SWOOLE_MINOR_VERSION 0
-#define SWOOLE_RELEASE_VERSION 0
-#define SWOOLE_EXTRA_VERSION ""
-#define SWOOLE_VERSION "5.0.0"
-#define SWOOLE_VERSION_ID 50000
+#define SWOOLE_RELEASE_VERSION 1
+#define SWOOLE_EXTRA_VERSION "dev"
+#define SWOOLE_VERSION "5.0.1-dev"
+#define SWOOLE_VERSION_ID 50001
 #define SWOOLE_API_VERSION_ID 0x202208a
 
 #define SWOOLE_BUG_REPORT                                                                                              \
