4b6a91079e2b99234c513de3db212211426cfaba
NathanFreeman <1056159381@qq.com>
2022-11-10 20:45:11 +0800

    TEST(server, udp_packet)

--- core-tests/src/server/server.cpp
+++ core-tests/src/server/server.cpp
@@ -1055,7 +1055,7 @@ TEST(server, udp_packet) {
     server->create();
     server->onPacket = [](Server *serv, RecvData *req) {
         swoole::DgramPacket *recv_data = (swoole::DgramPacket *) req->data;
-        EXPECT_STREQ(string(recv_data->data, recv_data->length), string(packet));
+        EXPECT_EQ(string(recv_data->data, recv_data->length), string(packet));
         network::Socket *server_socket = serv->get_server_socket(req->info.server_fd);
         string resp = string(packet);
         server_socket->sendto(recv_data->socket_addr, resp.c_str(), resp.length(), 0);
