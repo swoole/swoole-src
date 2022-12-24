fa70d8abffc0e40b0a482f8995f927a1ea8b3147
tianfenghan <rango@swoole.com>
2022-12-24 11:57:54 +0800

    fix 5

--- core-tests/src/protocol/http2.cpp
+++ core-tests/src/protocol/http2.cpp
@@ -20,7 +20,7 @@
 #include "test_core.h"
 #include "test_coroutine.h"
 #include "redis_client.h"
-#include "swoole_redis.h"
+#include "swoole_http2.h"
 
 using namespace swoole;
 using namespace std;
@@ -28,61 +28,36 @@ using namespace std;
 const std::string REDIS_TEST_KEY = "key-swoole";
 const std::string REDIS_TEST_VALUE = "value-swoole";
 
-TEST(redis, get) {
-    test::coroutine::run([](void *arg) {
-        RedisClient redis;
-        ASSERT_TRUE(redis.Connect("127.0.0.1", 6379));
-        ASSERT_TRUE(redis.Set(REDIS_TEST_KEY, REDIS_TEST_VALUE));
-        ASSERT_EQ(redis.Get(REDIS_TEST_KEY), REDIS_TEST_VALUE);
-    });
-}
+TEST(http2, default_settings) {
+    ASSERT_EQ(http2::get_default_setting(SW_HTTP2_SETTING_HEADER_TABLE_SIZE), SW_HTTP2_DEFAULT_HEADER_TABLE_SIZE);
+    ASSERT_EQ(http2::get_default_setting(SW_HTTP2_SETTINGS_ENABLE_PUSH), SW_HTTP2_DEFAULT_ENABLE_PUSH);
+    ASSERT_EQ(http2::get_default_setting(SW_HTTP2_SETTINGS_MAX_CONCURRENT_STREAMS),
+              SW_HTTP2_DEFAULT_MAX_CONCURRENT_STREAMS);
+    ASSERT_EQ(http2::get_default_setting(SW_HTTP2_SETTINGS_INIT_WINDOW_SIZE), SW_HTTP2_DEFAULT_INIT_WINDOW_SIZE);
+    ASSERT_EQ(http2::get_default_setting(SW_HTTP2_SETTINGS_MAX_FRAME_SIZE), SW_HTTP2_DEFAULT_MAX_FRAME_SIZE);
+    ASSERT_EQ(http2::get_default_setting(SW_HTTP2_SETTINGS_MAX_HEADER_LIST_SIZE),
+              SW_HTTP2_DEFAULT_MAX_HEADER_LIST_SIZE);
 
-TEST(redis, server) {
-    Server serv(Server::MODE_BASE);
-    serv.worker_num = 1;
-    serv.enable_static_handler = true;
-
-    sw_logger()->set_level(SW_LOG_WARNING);
-
-    ListenPort *port = serv.add_port(SW_SOCK_TCP, TEST_HOST, 0);
-    ASSERT_TRUE(port);
-    port->open_redis_protocol = true;
-
-    serv.create();
-
-    serv.onWorkerStart = [&](swServer *serv, int worker_id) {
-        if (worker_id != 0) {
-            return;
-        }
-        swoole::Coroutine::create(
-            [](void *arg) {
-                Server *serv = reinterpret_cast<Server *>(arg);
-                RedisClient redis;
-                ASSERT_TRUE(redis.Connect("127.0.0.1", serv->get_primary_port()->port));
-                ASSERT_TRUE(redis.Set(REDIS_TEST_KEY, REDIS_TEST_VALUE));
-                ASSERT_EQ(redis.Get(REDIS_TEST_KEY), REDIS_TEST_VALUE);
-                kill(serv->gs->master_pid, SIGTERM);
-            },
-            serv);
+    http2::Settings _settings = {
+        (uint32_t) swoole_rand(1, 100000),
+        (uint32_t) swoole_rand(1, 100000),
+        (uint32_t) swoole_rand(1, 100000),
+        (uint32_t) swoole_rand(1, 100000),
+        (uint32_t) swoole_rand(1, 100000),
+        (uint32_t) swoole_rand(1, 100000),
     };
 
-    serv.onReceive = [](swServer *serv, swRecvData *req) -> int {
-        int session_id = req->info.fd;
-        auto list = redis::parse(req->data, req->info.len);
+    http2::put_default_setting(SW_HTTP2_SETTING_HEADER_TABLE_SIZE, _settings.header_table_size);
+    http2::put_default_setting(SW_HTTP2_SETTINGS_ENABLE_PUSH, _settings.enable_push);
+    http2::put_default_setting(SW_HTTP2_SETTINGS_MAX_CONCURRENT_STREAMS, _settings.max_concurrent_streams);
+    http2::put_default_setting(SW_HTTP2_SETTINGS_INIT_WINDOW_SIZE, _settings.init_window_size);
+    http2::put_default_setting(SW_HTTP2_SETTINGS_MAX_FRAME_SIZE, _settings.max_frame_size);
+    http2::put_default_setting(SW_HTTP2_SETTINGS_MAX_HEADER_LIST_SIZE, _settings.max_header_list_size);
 
-        String *buffer = sw_tg_buffer();
-        buffer->clear();
-
-        if (strcasecmp(list[0].c_str(), "GET") == 0) {
-            redis::format(buffer, redis::REPLY_STRING, REDIS_TEST_VALUE);
-            serv->send(session_id, buffer->str, buffer->length);
-        } else if (strcasecmp(list[0].c_str(), "SET") == 0) {
-            redis::format(buffer, redis::REPLY_STATUS, "OK");
-            serv->send(session_id, buffer->str, buffer->length);
-        }
-
-        return SW_OK;
-    };
-
-    serv.start();
+    ASSERT_EQ(http2::get_default_setting(SW_HTTP2_SETTING_HEADER_TABLE_SIZE), _settings.header_table_size);
+    ASSERT_EQ(http2::get_default_setting(SW_HTTP2_SETTINGS_ENABLE_PUSH), _settings.enable_push);
+    ASSERT_EQ(http2::get_default_setting(SW_HTTP2_SETTINGS_MAX_CONCURRENT_STREAMS), _settings.max_concurrent_streams);
+    ASSERT_EQ(http2::get_default_setting(SW_HTTP2_SETTINGS_INIT_WINDOW_SIZE), _settings.init_window_size);
+    ASSERT_EQ(http2::get_default_setting(SW_HTTP2_SETTINGS_MAX_FRAME_SIZE), _settings.max_frame_size);
+    ASSERT_EQ(http2::get_default_setting(SW_HTTP2_SETTINGS_MAX_HEADER_LIST_SIZE), _settings.max_header_list_size);
 }
