e5909cdd1df76a79d51049d7efd48891f4d1dfd3
MARiA so cute <33935209+NathanFreeman@users.noreply.github.com>
2024-07-24 11:32:53 +0800

    [5.1]Fix bug #5407 (#5414)
    * fix bug #5407
    
    * optimize code

--- src/server/manager.cc
+++ src/server/manager.cc
@@ -608,6 +608,12 @@ pid_t Server::spawn_event_worker(Worker *worker) {
         return pid;
     }
 
+    // see https://github.com/swoole/swoole-src/issues/5407
+    if (worker->concurrency > 0 && worker_num > 1) {
+        sw_atomic_sub_fetch(&gs->concurrency, worker->concurrency);
+        worker->concurrency = 0;
+    }
+
     if (is_base_mode()) {
         gs->connection_nums[worker->id] = 0;
         gs->event_workers.main_loop(&gs->event_workers, worker);
