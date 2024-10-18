1335e52d95e5729f6f5eeea06f94722f881c09af
NathanFreeman <1056159381@qq.com>
2024-10-19 01:11:34 +0800

    Fix error

---
+++ src/lock/coroutine_lock.cc
@@ -0,0 +1,79 @@
+/*
+  +----------------------------------------------------------------------+
+  | Swoole                                                               |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 2.0 of the Apache license,    |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+  | If you did not receive a copy of the Apache2.0 license and are unable|
+  | to obtain it through the world-wide-web, please send a note to       |
+  | license@swoole.com so we can mail you a copy immediately.            |
+  +----------------------------------------------------------------------+
+  | Author: NathanFreeman  <mariasocute@163.com>                         |
+  +----------------------------------------------------------------------+
+ */
+
+#include "swoole.h"
+#include "swoole_lock.h"
+
+#ifdef HAVE_IOURING_FUTEX
+
+using swoole::AsyncIouring;
+using swoole::Coroutine;
+using swoole::coroutine::futex;
+
+namespace swoole {
+
+CoroutineLock::CoroutineLock() : Lock() {
+    type_ = COROUTINE_LOCK;
+    value = (sw_atomic_t *) sw_shm_malloc(sizeof(sw_atomic_t));
+    *value = 0;
+}
+
+CoroutineLock::~CoroutineLock() {
+    sw_shm_free((void *) value);
+    value = nullptr;
+}
+
+int CoroutineLock::lock() {
+    Coroutine *coroutine = Coroutine::get_current();
+    if (current_coroutine == coroutine) {
+        return 1;
+    }
+
+    if (!sw_atomic_cmp_set(value, 0, 1)) {
+        int result = futex(
+            AsyncIouring::SW_IORING_OP_FUTEX_WAIT, (uint32_t *) value, 1, FUTEX_BITSET_MATCH_ANY, FUTEX2_SIZE_U32, 0);
+        *value = 1;
+        return result > 0 ? 1 : 0;
+    }
+
+    current_coroutine = coroutine;
+    return 1;
+}
+
+int CoroutineLock::unlock() {
+    if (*value == 0) {
+        return 0;
+    }
+
+    *value = 0;
+    int result =
+        futex(AsyncIouring::SW_IORING_OP_FUTEX_WAKE, (uint32_t *) value, 1, FUTEX_BITSET_MATCH_ANY, FUTEX2_SIZE_U32, 0);
+    return result > 0 ? 1 : 0;
+}
+
+int CoroutineLock::lock_rd() {
+    return 0;
+}
+
+int CoroutineLock::trylock_rd() {
+    return 0;
+}
+
+int CoroutineLock::trylock() {
+    return 0;
+}
+}  // namespace swoole
+#endif
