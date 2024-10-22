53248eddee4726b61a9beffcaf38f5815a350047
NathanFreeman <1056159381@qq.com>
2024-10-22 23:46:19 +0800

    fix shell

--- scripts/library.sh
+++ scripts/library.sh
@@ -14,5 +14,5 @@ echo '/usr/local/instantclient' >/etc/ld.so.conf.d/oracle-instantclient.conf
 ldconfig
 
 wget https://github.com/axboe/liburing/archive/refs/tags/liburing-2.7.tar.gz
-tar zxf liburing-2.6.tar.gz
-cd liburing-liburing-2.6 && ./configure && make -j$(cat /proc/cpuinfo | grep processor | wc -l) && make install
+tar zxf liburing-2.7.tar.gz
+cd liburing-liburing-2.7 && ./configure && make -j$(cat /proc/cpuinfo | grep processor | wc -l) && make install
