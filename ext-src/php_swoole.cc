e205d985f331618840cb83666211f86f6cb42840
matyhtf <rango@swoole.com>
2024-10-22 19:07:49 +0800

    Fix PHP execution limit timer error.

--- ext-src/php_swoole.cc
+++ ext-src/php_swoole.cc
@@ -18,13 +18,13 @@
 #include "php_swoole_process.h"
 #include "php_swoole_thread.h"
 
-#if (HAVE_PCRE || HAVE_BUNDLED_PCRE) && !defined(COMPILE_DL_PCRE)
-#include "ext/pcre/php_pcre.h"
-#endif
+BEGIN_EXTERN_C()
 #include "zend_exceptions.h"
 #include "zend_extensions.h"
 
-BEGIN_EXTERN_C()
+#if (HAVE_PCRE || HAVE_BUNDLED_PCRE) && !defined(COMPILE_DL_PCRE)
+#include "ext/pcre/php_pcre.h"
+#endif
 #include "ext/json/php_json.h"
 
 #include "stubs/php_swoole_arginfo.h"
@@ -1040,6 +1040,10 @@ void sw_php_exit(int status) {
 #endif
 }
 
+static void sw_after_fork(void *args) {
+    zend_max_execution_timer_init();
+}
+
 PHP_RINIT_FUNCTION(swoole) {
     if (!SWOOLE_G(cli)) {
         return SUCCESS;
@@ -1079,6 +1083,8 @@ PHP_RINIT_FUNCTION(swoole) {
     /* Disable warning even in ZEND_DEBUG because we may register our own signal handlers  */
     SIGG(check) = 0;
 #endif
+
+    swoole_add_hook(SW_GLOBAL_HOOK_AFTER_FORK, sw_after_fork, 0);
 
     php_swoole_http_server_rinit();
     php_swoole_websocket_server_rinit();
