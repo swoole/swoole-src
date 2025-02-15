518a2337a0fc3efae97426f82ff1f03eb62f56f8
MARiA so cute <33935209+NathanFreeman@users.noreply.github.com>
2025-02-16 07:52:50 +0800

    Release v5.1.7 (#5688)

--- include/swoole_version.h
+++ include/swoole_version.h
@@ -20,10 +20,10 @@
 
 #define SWOOLE_MAJOR_VERSION 5
 #define SWOOLE_MINOR_VERSION 1
-#define SWOOLE_RELEASE_VERSION 6
+#define SWOOLE_RELEASE_VERSION 7
 #define SWOOLE_EXTRA_VERSION ""
-#define SWOOLE_VERSION "5.1.6"
-#define SWOOLE_VERSION_ID 50106
+#define SWOOLE_VERSION "5.1.7"
+#define SWOOLE_VERSION_ID 50107
 #define SWOOLE_API_VERSION_ID 0x202208a
 
 #define SWOOLE_BUG_REPORT                                                                                              \
