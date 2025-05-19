217c27f7b863de20ca7766590e5359b7fdbd0c08
matyhtf <rango@swoole.com>
2025-05-19 09:20:28 +0800

    Optimize tests [13] --filter=[core] --verbose

--- core-tests/src/core/hash.cpp
+++ core-tests/src/core/hash.cpp
@@ -20,6 +20,8 @@
 #include "test_core.h"
 #include "swoole_hash.h"
 
+static const int hash_count = 8192;
+static const int str_max_len = 1024;
 static const char *data = "hello world, PHP  the best.";
 
 TEST(hash, crc32) {
@@ -27,22 +29,35 @@ TEST(hash, crc32) {
 }
 
 static void test_hash_func(uint64_t (*hash_fn)(const char *key, size_t len), int n) {
+    std::vector<uint64_t> hashes;
+    std::vector<std::string> data;
+    hashes.resize(n);
+    data.resize(n);
+
     SW_LOOP_N(n) {
-        size_t len = 1 + swoole_random_int() % 256;
-        char buf[256];
+        size_t len = 1 + swoole_random_int() % str_max_len;
+        char buf[str_max_len];
         ASSERT_EQ(swoole_random_bytes(buf, len), len);
-        ASSERT_GT(hash_fn(buf, len), 0);
+        hashes[i] = hash_fn(buf, len);
+        data[i] = std::string(buf, len);
+    }
+
+    usleep(100);
+
+    SW_LOOP_N(n) {
+        auto &s = data.at(i);
+        ASSERT_EQ(hashes[i], hash_fn(s.c_str(), s.length()));
     }
 }
 
 TEST(hash, php) {
-    test_hash_func(swoole_hash_jenkins, 2048);
+    test_hash_func(swoole_hash_jenkins, hash_count);
 }
 
 TEST(hash, jenkins) {
-    test_hash_func(swoole_hash_jenkins, 2048);
+    test_hash_func(swoole_hash_jenkins, hash_count);
 }
 
 TEST(hash, austin) {
-    test_hash_func(swoole_hash_austin, 2048);
+    test_hash_func(swoole_hash_austin, hash_count);
 }
