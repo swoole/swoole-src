5189e5ddebbe219024c080a798b6bff79f1096f6
matyhtf <rango@swoole.com>
2025-05-24 17:06:36 +0800

    Fix tests [29] --filter=[core] --asan --verbose

--- core-tests/src/coroutine/gethostbyname.cpp
+++ core-tests/src/coroutine/gethostbyname.cpp
@@ -5,23 +5,20 @@ using swoole::Timer;
 using swoole::coroutine::System;
 using swoole::test::coroutine;
 
-const char *domain_baidu = "www.baidu.com";
-const char *domain_tencent = "www.tencent.com";
-
 TEST(coroutine_gethostbyname, resolve_cache) {
     coroutine::run([](void *arg) {
         System::set_dns_cache_capacity(10);
-        std::string addr1 = System::gethostbyname(domain_baidu, AF_INET);
+        std::string addr1 = System::gethostbyname(TEST_DOMAIN_BAIDU, AF_INET);
         ASSERT_NE(addr1, "");
         for (int i = 0; i < 100; ++i) {
-            std::string addr2 = System::gethostbyname(domain_baidu, AF_INET);
+            std::string addr2 = System::gethostbyname(TEST_DOMAIN_BAIDU, AF_INET);
             ASSERT_EQ(addr1, addr2);
         }
         ASSERT_GT(System::get_dns_cache_hit_ratio(), 0.99);
 
         System::set_dns_cache_capacity(0);
         for (int i = 0; i < 5; ++i) {
-            std::string addr2 = System::gethostbyname(domain_baidu, AF_INET);
+            std::string addr2 = System::gethostbyname(TEST_DOMAIN_BAIDU, AF_INET);
             ASSERT_NE(addr2, "");
         }
         ASSERT_LT(System::get_dns_cache_hit_ratio(), 0.01);
@@ -30,7 +27,7 @@ TEST(coroutine_gethostbyname, resolve_cache) {
 
 TEST(coroutine_gethostbyname, impl_async) {
     coroutine::run([](void *arg) {
-        auto result = swoole::coroutine::gethostbyname_impl_with_async(domain_baidu, AF_INET);
+        auto result = swoole::coroutine::gethostbyname_impl_with_async(TEST_DOMAIN_BAIDU, AF_INET);
         ASSERT_EQ(result.empty(), false);
     });
 }
@@ -64,6 +61,8 @@ TEST(coroutine_gethostbyname, resolve_cache_inet4_and_inet6) {
 TEST(coroutine_gethostbyname, dns_expire) {
     coroutine::run([](void *arg) {
         System::set_dns_cache_expire(1);
+        System::clear_dns_cache();
+
         System::gethostbyname(TEST_HTTP_DOMAIN, AF_INET);
         System::gethostbyname(TEST_HTTP_DOMAIN, AF_INET);
         ASSERT_GE(System::get_dns_cache_hit_ratio(), 0.5);
@@ -73,5 +72,6 @@ TEST(coroutine_gethostbyname, dns_expire) {
         ASSERT_LT(System::get_dns_cache_hit_ratio(), 0.35);
 
         System::clear_dns_cache();
+        System::set_dns_cache_expire(60);
     });
 }
