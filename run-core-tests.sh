03fa9937501de810127ac4c270b2eb2d5340096e
matyhtf <rango@swoole.com>
2025-05-29 10:36:25 +0800

    Fix tests [3] --filter=[core]

--- run-core-tests.sh
+++ run-core-tests.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 __DIR__=$(cd "$(dirname "$0")" || exit;pwd)
 
-cmake . ${CMAKE_ARGS} || exit 1
-make VERBOSE=1 -j8  || exit 1
 ipcs -q
 
 cd "${__DIR__}"/core-tests/js || exit 1
@@ -12,9 +10,6 @@ cd "${__DIR__}" || exit 1
 tasks=$(./bin/core-tests --gtest_list_tests | awk '/\./') || exit 255
 for task in $tasks; do
     execute_command="./bin/core-tests"
-
-    echo "run tests for $task"
-
     if [ "$task" = "log." ]; then
         $execute_command --gtest_filter="$task"*
     else
