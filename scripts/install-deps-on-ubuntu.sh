11fe7b48f3f3b134fd89c7c19832bc07c15077ff
tianfenghan <rango@swoole.com>
2025-11-18 16:13:12 +0800

    add  libssh2-1-dev

--- scripts/install-deps-on-ubuntu.sh
+++ scripts/install-deps-on-ubuntu.sh
@@ -2,4 +2,5 @@ sudo apt install libcurl4-openssl-dev \
     libc-ares-dev libssl-dev libbrotli-dev \
     libpq-dev libsqlite3-dev unixodbc-dev \
     libzstd-dev \
-    liburing-dev
+    liburing-dev \
+    libssh2-1-dev
