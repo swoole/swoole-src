36344fd7530cd5680ec0a7c74ee3adad1cf33312
tianfenghan <rango@swoole.com>
2025-11-19 18:43:44 +0800

    update --filter=[unit]

--- scripts/install-deps-on-ubuntu.sh
+++ scripts/install-deps-on-ubuntu.sh
@@ -1,4 +1,9 @@
-sudo apt install libcurl4-openssl-dev \
+sudo apt install cmake make gcc \
+    libssl-dev \
+    libmariadb-dev \
+    libaio-dev libaio1  \
+    zlib1g-dev \
+    libcurl4-openssl-dev \
     libc-ares-dev libssl-dev libbrotli-dev \
     libpq-dev libsqlite3-dev unixodbc-dev \
     libzstd-dev \
