7841ee204b4f19a90228ed990f611c841d23cd10
tianfenghan <rango@swoole.com>
2025-11-19 19:11:01 +0800

    update --filter=[unit]

--- scripts/install-deps-on-ubuntu.sh
+++ scripts/install-deps-on-ubuntu.sh
@@ -1,11 +1,14 @@
-sudo apt install cmake make gcc sqlite3 \
+sudo apt install -y cmake make gcc \
+    libc-ares-dev \
     libssl-dev \
+    libcurl4-openssl-dev \
     libmariadb-dev \
     libaio-dev libaio1  \
     zlib1g-dev \
-    libcurl4-openssl-dev \
-    libc-ares-dev libssl-dev libbrotli-dev \
-    libpq-dev libsqlite3-dev unixodbc-dev \
+    sqlite3 libsqlite3-dev \
+    libbrotli-dev \
+    libpq-dev \
+    unixodbc-dev \
     libzstd-dev \
     liburing-dev \
     libssh2-1-dev
