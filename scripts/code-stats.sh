6d32ce8034aed0dccd01c57a6fec1d6fb5ae43ff
tianfenghan <rango@swoole.com>
2025-11-20 11:28:30 +0800

    Optimize code-stats.sh

--- scripts/code-stats.sh
+++ scripts/code-stats.sh
@@ -3,5 +3,9 @@ __CURRENT__=$(pwd)
 __DIR__=$(cd "$(dirname "$0")";pwd)
 
 # enter the dir
-cd "${__DIR__}"
-cloc ../ --exclude-dir=thirdparty,Debug,CMakeFiles,build,CMakeFiles,.git
+cd "${__DIR__}/.."
+cloc . --exclude-dir=thirdparty,Debug,CMakeFiles,build,.git \
+  --fullpath \
+  --not-match-d='tools/vendor' \
+  --not-match-d='tests/include/lib/vendor' \
+  --not-match-f='ext-src/php_swoole_library\.h$'
