32a425f9f1f853eb2ee085791c18b140a9424b93
tianfenghan <rango@swoole.com>
2025-12-19 19:00:29 +0800

    fix test --filter=[core]

--- core-tests/src/coroutine/uring_socket.cpp
+++ core-tests/src/coroutine/uring_socket.cpp
@@ -28,19 +28,19 @@ using swoole::coroutine::UringSocket;
 using swoole::test::coroutine;
 using swoole::test::create_socket_pair;
 
-const std::string host = "www.baidu.com";
-
-TEST(uring_socket, connect_with_dns) {
+TEST(uring_socket, connect) {
     coroutine::run([](void *arg) {
         UringSocket sock(SW_SOCK_TCP);
-        bool retval = sock.connect(host, 80);
+        bool retval = sock.connect(TEST_HTTP_DOMAIN, 80);
         ASSERT_EQ(retval, true);
         ASSERT_EQ(sock.errCode, 0);
 
         ssize_t rv;
 
-        rv = sock.send(TEST_REQUEST_BAIDU, strlen(TEST_REQUEST_BAIDU));
-        ASSERT_EQ(rv, strlen(TEST_REQUEST_BAIDU));
+        auto req = swoole::test::http_get_request(TEST_HTTP_DOMAIN, "/");
+
+        rv = sock.send(req.c_str(), req.length());
+        ASSERT_EQ(rv, req.length());
 
         char buf[4096];
 
@@ -48,7 +48,7 @@ TEST(uring_socket, connect_with_dns) {
         ASSERT_GT(rv, 100);
 
         std::string s{buf};
-        ASSERT_TRUE(s.find("Location: https://www.baidu.com/") != s.npos);
+        ASSERT_TRUE(s.find(TEST_HTTP_EXPECT) != s.npos);
     });
 }
 
@@ -56,14 +56,17 @@ TEST(uring_socket, ssl_connect) {
     coroutine::run([](void *arg) {
         UringSocket sock(SW_SOCK_TCP);
         sock.enable_ssl_encrypt();
-        sock.set_tls_host_name(TEST_DOMAIN_BAIDU);
+        sock.set_tls_host_name(TEST_HTTP_DOMAIN);
         sock.set_ssl_verify_peer(true);
-        bool retval = sock.connect(host, 443);
+
+        auto req = swoole::test::http_get_request(TEST_HTTP_DOMAIN, "/");
+
+        bool retval = sock.connect(TEST_HTTP_DOMAIN, 443);
         ASSERT_EQ(retval, true);
         ASSERT_EQ(sock.errCode, 0);
 
-        auto rv = sock.send(TEST_REQUEST_BAIDU, strlen(TEST_REQUEST_BAIDU));
-        ASSERT_EQ(rv, strlen(TEST_REQUEST_BAIDU));
+        auto rv = sock.send(req.c_str(), req.length());
+        ASSERT_EQ(rv, req.length());
 
         ASSERT_TRUE(sock.check_liveness());
 
@@ -76,7 +79,7 @@ TEST(uring_socket, ssl_connect) {
             }
             buf.append(rbuf, nr);
         }
-        ASSERT_TRUE(buf.contains("www.baidu.com"));
+        ASSERT_TRUE(buf.contains(TEST_HTTPS_EXPECT));
     });
 }
 
