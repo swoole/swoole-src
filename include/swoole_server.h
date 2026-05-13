41533c66057e7c218f777b0e4acb7c1863ca73a6
MARiA so cute <33935209+NathanFreeman@users.noreply.github.com>
2026-05-13 23:08:54 +0800

    [6.2]merge swoole 6.1 into master and fix empry path test (#6058)
    * merge swoole 6.1 into master
    
    * fix test

--- include/swoole_server.h
+++ include/swoole_server.h
@@ -99,8 +99,6 @@ struct Connection {
     uint8_t close_force;
     ReactorId reactor_id;
     uint16_t close_errno;
-    int server_fd;
-    sw_atomic_t closed;
     sw_atomic_t recv_queued_bytes;
     uint32_t send_queued_bytes;
     uint16_t waiting_time;
@@ -142,6 +140,8 @@ struct Connection {
     String *ssl_client_cert;
     pid_t ssl_client_cert_pid;
     sw_atomic_t lock;
+    sw_atomic_t closed;
+    int server_fd;
 };
 
 //------------------------------------ReactorThread-------------------------------------------
