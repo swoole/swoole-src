5d1708d7b6c01fe7567cb2a572a4d386ed5b3e2f
Emre Çalışkan <bilgi@emrecaliskan.com.tr>
2026-08-20 20:34:07 +0300

    Add PHP 8.6 compatibility

--- ext-src/php_swoole_odbc.h
+++ ext-src/php_swoole_odbc.h
@@ -30,8 +30,10 @@ BEGIN_EXTERN_C()
 #include "thirdparty/php83/pdo_odbc/php_pdo_odbc_int.h"
 #elif PHP_VERSION_ID >= 80400 && PHP_VERSION_ID < 80500
 #include "thirdparty/php84/pdo_odbc/php_pdo_odbc_int.h"
-#else
+#elif PHP_VERSION_ID >= 80500 && PHP_VERSION_ID < 80600
 #include "thirdparty/php85/pdo_odbc/php_pdo_odbc_int.h"
+#else
+#include "thirdparty/php86/pdo_odbc/php_pdo_odbc_int.h"
 #endif
 
 extern const pdo_driver_t swoole_pdo_odbc_driver;
